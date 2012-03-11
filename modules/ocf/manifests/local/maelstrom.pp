c9cff1b0228ac9296e97c1af3a62fe2f8a1d5477
Dara Adib <daradib@ocf.berkeley.edu>
2012-03-11 13:22:47 -0700

    maelstrom: add MySQL config

--- modules/ocf/manifests/local/maelstrom.pp
+++ modules/ocf/manifests/local/maelstrom.pp
@@ -1,5 +1,26 @@
 class ocf::local::maelstrom {
-  package {
-    [ 'mysql-server' ]:
+
+  # install mysql and set root password
+  package { 'mysql-server-5.1':
+    responsefile => '/opt/share/puppet/mysql-server-5.1.preseed'
   }
+  file { '/opt/share/puppet/mysql-server-5.1.preseed':
+    mode         => '0600',
+    source       => 'puppet:///private/mysql-server-5.1.preseed'
+  }
+
+  # provide mysql server and client config
+  file {
+    '/etc/mysql/my.cnf':
+      source => 'puppet:///modules/ocf/local/maelstrom/my.cnf';
+    '/root/.my.cnf':
+      mode   => '0600',
+      source => 'puppet:///private/root-my.cnf'
+  }
+
+  service { 'mysql':
+    subscribe => File['/etc/mysql/my.cnf'],
+    require   => Package['mysql-server-5.1']
+  }
+
 }
