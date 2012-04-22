0483f180dd3232759a5e908e3849e8c2cfe05e55
Dara Adib <daradib@ocf.berkeley.edu>
2012-04-22 00:43:00 -0700

    coupdetat: add pam_mkhomedir and sshfs

--- modules/ocf/manifests/local/coupdetat.pp
+++ modules/ocf/manifests/local/coupdetat.pp
@@ -43,4 +43,33 @@ class ocf::local::coupdetat {
   # install pssh for parallel execution
   package { 'pssh': }
 
+  # install sshfs
+  package { 'sshfs':
+  }
+  # change fuse group to match ocf group gid
+  exec { 'fuse':
+    command => 'sed -i "s/^fuse:.*/fuse:x:20:/g" /etc/group',
+    unless  => 'grep "fuse:x:20:" /etc/group',
+    require => Package['sshfs']
+  }
+  file {
+    '/usr/bin/fusermount':
+      mode    => '4754',
+      group   => fuse,
+      require => [ Package['sshfs'], Exec['fuse'] ];
+    '/etc/fuse.conf':
+      group   => fuse,
+      require => [ Package['sshfs'], Exec['fuse'] ]
+  }
+  service { 'fuse':
+      subscribe => [ Exec['fuse'], File[ '/usr/bin/fusermount', '/etc/fuse.conf' ] ],
+      require   => [ Package['sshfs'],  ]
+  }
+
+  # create pam_mkhomedir profile
+  file { '/usr/share/pam-configs/mkhomedir':
+    source  => 'puppet:///modules/ocf/desktop/pam/mkhomedir',
+    notify  => Exec['pam-auth-update']
+  }
+
 }
