544d00beb90c2845577b1060aae0da698481f98a
Kenny Do <kedo@ocf.berkeley.edu>
2012-06-12 23:26:33 -0700

    fixed apache2 logrotate permissions, cosmetic changes

--- modules/ocf/manifests/local/death.pp
+++ modules/ocf/manifests/local/death.pp
@@ -3,7 +3,7 @@ class ocf::local::death {
   package {
     [ 'apache2', 'php5', 'php5-mysql', 'libapache-mod-security', 'libapache2-mod-suphp', 'python-django', 'python-ldap', 'python-mysqldb', 'python-flup', 'python-cracklib', 'apache2-threaded-dev', 'libapache2-mod-fcgid' ]:
       ensure  => installed;
-    'nfs-kernel-server':
+    [ 'nfs-kernel-server', 'logrotate' ]:
       ensure  => installed;
     # for general development
     'git':
@@ -26,7 +26,7 @@ class ocf::local::death {
     mode      => '0644',
     source    => 'puppet:///modules/ocf/local/death/apache/httpd.conf',
     require   => [ Package['apache2'] ],
-    notify      => Service['apache2'],
+    notify    => Service['apache2'],
   }
 
   file { '/etc/apache2/sites-available':
@@ -76,7 +76,7 @@ class ocf::local::death {
   file { '/usr/lib/apache2':
     ensure   => directory,
     require  => Package['apache2'],
-  } 
+  }
 
   file { '/usr/lib/apache2/suexec':
     ensure    => file,
@@ -91,83 +91,83 @@ class ocf::local::death {
   exec { '/usr/bin/apxs2 -i -c -a -n ocfdir /etc/apache2/mods-available/mod_ocfdir.c':
     require     => [ Package['apache2-threaded-dev'], File['/etc/apache2/mods-available/mod_ocfdir.c'] ],
     notify      => Service['apache2'],
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/ocfdir.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/ocfdir.load',
   }
 
   # enable apache modules
   exec { '/usr/sbin/a2enmod rewrite':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/rewrite.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/rewrite.load',
     notify      => Service['apache2'],
     require     => Package['apache2'],
   }
   exec { '/usr/sbin/a2enmod fcgid':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/fcgid.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/fcgid.load',
     notify      => Service['apache2'],
     require     => [Package['apache2'], Package['libapache2-mod-fcgid']],
   }
   exec { '/usr/sbin/a2enmod include':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/include.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/include.load',
     notify      => Service['apache2'],
     require     => Package['apache2'],
   }
   exec { '/usr/sbin/a2enmod suexec':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/suexec.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/suexec.load',
     notify      => Service['apache2'],
     require     => [Package['apache2'], File['/usr/lib/apache2/suexec']],
   }
   exec { '/usr/sbin/a2enmod suphp':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/suphp.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/suphp.load',
     notify      => Service['apache2'],
     require     => [Package['apache2'], Package['libapache2-mod-suphp']],
   }
   exec { '/usr/sbin/a2enmod ssl':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/ssl.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/ssl.load',
     notify      => Service['apache2'],
     require     => [ File['/etc/ssl/certs/secure.ocf.berkeley.edu.crt'], File['/etc/ssl/private/secure.ocf.berkeley.edu.key'], Package['apache2'] ],
   }
   exec { '/usr/sbin/a2enmod userdir':
-    unless      => "/bin/readlink -e /etc/apache2/mods-enabled/userdir.load",
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/userdir.load',
     notify      => Service['apache2'],
     require     => Package['apache2'],
   }
 
   # disable default site
   exec { '/usr/sbin/a2dissite 000-default':
-    onlyif      => "/bin/readlink -e /etc/apache2/sites-enabled/000-default",
+    onlyif      => '/bin/readlink -e /etc/apache2/sites-enabled/000-default',
     require     => Package['apache2'],
     notify      => Service['apache2'],
   }
 
   # disable the php5 module because it conflicts with suphp
   exec { '/usr/sbin/a2dismod php5':
-    onlyif      => "/bin/readlink -e /etc/apache2/mods-enabled/php5.load",
+    onlyif      => '/bin/readlink -e /etc/apache2/mods-enabled/php5.load',
     require     => Package['apache2', 'php5'],
     notify      => Service['apache2'],
   }
 
   # enable sites
   exec { '/usr/sbin/a2ensite 21-account_tools.conf':
-    unless      => "/bin/readlink -e /etc/apache2/sites-enabled/21-account_tools.conf",
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/21-account_tools.conf',
     notify      => Service['apache2'],
     require     => [Exec['/usr/sbin/a2enmod rewrite'], File['/etc/apache2/sites-available/21-account_tools.conf']],
   }
   exec { '/usr/sbin/a2ensite 02-ssl.conf':
-    unless      => "/bin/readlink -e /etc/apache2/sites-enabled/02-ssl.conf",
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/02-ssl.conf',
     notify      => Service['apache2'],
     require     => [Exec['/usr/sbin/a2enmod rewrite'], File['/etc/apache2/sites-available/02-ssl.conf']],
   }
   exec { '/usr/sbin/a2ensite 22-staff_hours.conf':
-    unless      => "/bin/readlink -e /etc/apache2/sites-enabled/22-staff_hours.conf",
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/22-staff_hours.conf',
     notify      => Service['apache2'],
     require     => File['/etc/apache2/sites-available/22-staff_hours.conf'],
   }
   exec { '/usr/sbin/a2ensite 03-userdir.conf':
-    unless      => "/bin/readlink -e /etc/apache2/sites-enabled/03-userdir.conf",
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/03-userdir.conf',
     notify      => Service['apache2'],
     require     => [Exec['/usr/sbin/a2enmod userdir'], File['/etc/apache2/sites-available/03-userdir.conf']],
   }
   exec { '/usr/sbin/a2ensite 01-www.conf':
-    unless      => "/bin/readlink -e /etc/apache2/sites-enabled/01-www.conf",
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/01-www.conf',
     notify      => Service['apache2'],
     require     => [Exec['/usr/sbin/a2enmod rewrite'], Exec['/usr/sbin/a2enmod include'], File['/etc/apache2/sites-available/01-www.conf']],
   }
@@ -200,8 +200,8 @@ class ocf::local::death {
 
   # apache must subscribe to all conf files
   service { 'apache2':
-    ensure  => 'running',
-    provider => 'debian',
+    ensure      => 'running',
+    provider    => 'debian',
   }
 
   # nfs export of apache logs
@@ -211,6 +211,14 @@ class ocf::local::death {
       source    => 'puppet:///modules/ocf/local/death/exports',
       require   => Package['nfs-kernel-server', 'apache2'],
       notify    => Service['nfs-kernel-server'],
+  }
+
+  # make sure logratate changes the permissions so that it is viewable when exported
+  file {
+    '/etc/logrotate.d/apache2':
+      ensure    => file,
+      source    => 'puppet:///modules/ocf/local/death/logrotate/apache2',
+      require   => Package['logrotate'],
   }
 
   service { 'nfs-kernel-server':
