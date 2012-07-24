0863e7bc0ffa3aaf8fe81d0cb71275dcba7788e6
Dara Adib <daradib@ocf.berkeley.edu>
2012-07-24 07:39:11 -0700

    libvirt r/w access to specified group, default to admin

--- modules/ocf/manifests/services/kvm.pp
+++ modules/ocf/manifests/services/kvm.pp
@@ -1,4 +1,4 @@
-class ocf::services::kvm( $octet ) {
+class ocf::services::kvm( $octet, $group = 'admin' ) {
 
   # install kvm, libvirt, lvm, bridge networking
   ocf::repackage { [ 'bridge-utils', 'libvirt-bin', 'lvm2', 'netcat-openbsd', 'qemu-kvm', 'virtinst', 'virt-top' ]:
@@ -7,13 +7,27 @@ class ocf::services::kvm( $octet ) {
 
   # provide network interfaces
   file { '/etc/network/interfaces':
-    content => template('ocf/services/kvm/interfaces.erb'),
-    notify  => Service['networking']
+    content     => template('ocf/services/kvm/interfaces.erb'),
+    notify      => Service['networking']
   }
   # start network interfaces
   exec { 'ifup -a':
     refreshonly => true,
     subscribe   => File['/etc/network/interfaces']
+  }
+
+  # group access to libvirt
+  augeas { '/etc/libvirt/libvirtd.conf':
+    context   => "/files/etc/libvirt/libvirtd.conf",
+    changes   =>  [
+                    "set unix_sock_group $group",
+                    'set unix_sock_rw_perms 0770'
+                  ],
+    require   => [ File['/etc/nsswitch.conf'], Ocf::Repackage['libvirt-bin'] ]
+  }
+  service { 'libvirt-bin':
+    subscribe => Augeas['/etc/libvirt/libvirtd.conf'],
+    require   => Ocf::Repackage['libvirt-bin']
   }
 
 }
