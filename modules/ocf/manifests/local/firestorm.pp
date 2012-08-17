44c3631a6f6a5eb05eb35eb7f97efd30066db797
Sanjay Krishnan <sanjayk@ocf.berkeley.edu>
2012-08-17 01:20:28 -0700

    Aesthetic changes on firestorm manifest

--- modules/ocf/manifests/local/firestorm.pp
+++ modules/ocf/manifests/local/firestorm.pp
@@ -11,13 +11,14 @@ class ocf::local::firestorm {
 
   file {
     '/etc/logrotate.d/heimdal-kdc':
-    source => 'puppet:///modules/ocf/local/firestorm/heimdal-kdc-logrotate',
+    source  => 'puppet:///modules/ocf/local/firestorm/heimdal-kdc-logrotate',
     require => [ Package['heimdal-kdc'], Package['logrotate'] ]
   }
 
   #define the heimdal service
-  service { 'heimdal-kdc':
-         subscribe => File[ '/etc/heimdal-kdc/kdc.conf', '/etc/heimdal-kdc/kadmind.acl' ]
+  service {
+    'heimdal-kdc':
+      subscribe => File[ '/etc/heimdal-kdc/kdc.conf', '/etc/heimdal-kdc/kadmind.acl' ]
   }
 
   file {
@@ -37,51 +38,52 @@ class ocf::local::firestorm {
   package { ['slapd', 'libsasl2-modules-gssapi-heimdal']: }
 
   #define service
-  service { 'slapd':
-         subscribe => File[ '/etc/ldap/slapd.conf', '/etc/ldap/schema/ocf.schema', '/etc/ldap/ocf_ldap.key', '/etc/default/slapd','/etc/ldap/sasl2/slapd.conf']
+  service {
+    'slapd':
+      subscribe => File[ '/etc/ldap/slapd.conf', '/etc/ldap/schema/ocf.schema', '/etc/ldap/ocf_ldap.key', '/etc/default/slapd','/etc/ldap/sasl2/slapd.conf']
   }
 
   #needed config files
   file {
     '/etc/ldap/slapd.conf':
-      source => 'puppet:///modules/ocf/local/firestorm/slapd.conf',
+      source  => 'puppet:///modules/ocf/local/firestorm/slapd.conf',
       require => [ Package['slapd'] ],
   }
 
   file {
     '/etc/ldap/schema/ocf.schema':
-      source => 'puppet:///modules/ocf/local/firestorm/ocf.schema',
+      source  => 'puppet:///modules/ocf/local/firestorm/ocf.schema',
       require => [ Package['slapd'] ],
   }
 
   file {
     '/etc/ldap/ocf_ldap.key':
-      source => 'puppet:///private/ocf_ldap.key',
+      source  => 'puppet:///private/ocf_ldap.key',
       require => [ Package['slapd'], Package['openssl'] ],
-      mode => '0600',
-      owner => 'openldap',
-      group => 'openldap',
+      mode    => '0600',
+      owner   => 'openldap',
+      group   => 'openldap',
   }
 
   file {
     '/etc/default/slapd':
-      source => 'puppet:///modules/ocf/local/firestorm/slapd-defaults',
+      source  => 'puppet:///modules/ocf/local/firestorm/slapd-defaults',
       require =>  [ Package['slapd'], Package['openssl'] ],
   }
 
   file {
     '/etc/ldap/sasl2/slapd.conf':
-      source => 'puppet:///modules/ocf/local/firestorm/sasl2-slapd',
+      source  => 'puppet:///modules/ocf/local/firestorm/sasl2-slapd',
       require =>  [ Package['slapd'], Package['libsasl2-modules-gssapi-heimdal'] ],
   }
 
   file {
     '/etc/ldap/krb5.keytab':
-      source => 'puppet:///private/krb5-ldap.keytab',
+      source  => 'puppet:///private/krb5-ldap.keytab',
       require => [ Package['slapd'], Package['heimdal-clients'] ],
-      mode => '0600',
-      owner => 'openldap',
-      group => 'openldap',
+      mode    => '0600',
+      owner   => 'openldap',
+      group   => 'openldap',
   }
 
 }
