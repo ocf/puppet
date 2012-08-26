6d8bf0381e11480f13ba5ef5e49fdade926c6381
Dara Adib <daradib@ocf.berkeley.edu>
2012-08-26 08:50:35 -0700

    Generate keytabs with both lab.ocf and ocf

--- modules/ocf/files/local/lightning/puppet-scripts/gen-keytab.sh
+++ modules/ocf/files/local/lightning/puppet-scripts/gen-keytab.sh
@@ -4,27 +4,15 @@
 read -p 'Your username: ' user
 echo 'List of hosts (leave off domain) to generate principals for, separated by spaces:'
 read hostnames
-read -p 'int.ocf.berkeley.edu principals only? [yn]' int
 
 cd /opt/puppet/private
 for host in $hostnames; do
   mkdir -p $host
-  if [ $int = 'y' -o $int = 'Y' ]; then
-    echo "Creating host/$host.int.ocf.berkeley.edu principal"
-    /usr/sbin/kadmin -p $user/admin add -r --use-defaults host/$host.int.ocf.berkeley.edu
-    echo "Exporting host/$host.int.ocf.berkeley.edu keytab"
-    rm -f $host/krb5.keytab
-    /usr/sbin/kadmin -p $user/admin ext_keytab -k $host/krb5.keytab host/$host.int.ocf.berkeley.edu
-  elif [ $int = 'n' -o $int = 'N' ]; then
-    echo "Creating host/$host.int.ocf.berkeley.edu and host/$host.ocf.berkeley.edu principals and keytab"
-    /usr/sbin/kadmin -p $user/admin add -r --use-defaults host/$host.int.ocf.berkeley.edu host/$host.ocf.berkeley.edu
-    echo "Exporting host/$host.int.ocf.berkeley.edu and host/$host.ocf.berkeley.edu keytab"
-    rm -f $host/krb5.keytab
-    /usr/sbin/kadmin -p $user/admin ext_keytab -k $host/krb5.keytab host/$host.int.ocf.berkeley.edu host/$host.ocf.berkeley.edu
-  else
-    echo "Error parsing: $int"
-    exit 2
-  fi
+  echo "Creating host/$host.lab.ocf.berkeley.edu and host/$host.ocf.berkeley.edu principals and keytab"
+  /usr/sbin/kadmin -p $user/admin add -r --use-defaults host/$host.lab.ocf.berkeley.edu host/$host.ocf.berkeley.edu
+  echo "Exporting host/$host.lab.ocf.berkeley.edu and host/$host.ocf.berkeley.edu keytab"
+  rm -f $host/krb5.keytab
+  /usr/sbin/kadmin -p $user/admin ext_keytab -k $host/krb5.keytab host/$host.lab.ocf.berkeley.edu host/$host.ocf.berkeley.edu
 done
 
 chown -R puppet:puppet /opt/puppet/private
