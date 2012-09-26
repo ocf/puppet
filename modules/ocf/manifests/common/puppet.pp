afb27a8dae25fdcc6b2a15eb667747557f74abaa
Dara Adib <daradib@ocf.berkeley.edu>
2012-09-26 04:23:44 -0700

    Run bug7680 workaround once not at every run

--- modules/ocf/manifests/common/puppet.pp
+++ modules/ocf/manifests/common/puppet.pp
@@ -8,12 +8,13 @@ class ocf::common::puppet {
   }
 
   package { 'puppet':
-    ensure    => latest,
-    require   => Exec['aptitude update']
+    ensure      => latest,
+    require     => Exec['aptitude update']
   }
   exec { 'puppet-fix_bug7680':
-    command   => 'sed -i "s/metadata.links == :manage/resource[:links] == :manage/g" /usr/lib/ruby/1.8/puppet/type/file/source.rb',
-    subscribe => Package['puppet']
+    command     => 'sed -i "s/metadata.links == :manage/resource[:links] == :manage/g" /usr/lib/ruby/1.8/puppet/type/file/source.rb',
+    refreshonly => true,
+    subscribe   => Package['puppet']
   }
 
   file {
