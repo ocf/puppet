dd51ec26d2f5a8caa954affdb3dd1f92228680ac
Dara Adib <daradib@ocf.berkeley.edu>
2012-09-29 02:37:58 -0700

    Iceweasel on desktops authenticates to rt.ocf using Kerberos/GSSAPI

--- modules/ocf/files/desktop/iceweasel/prefs.js
+++ modules/ocf/files/desktop/iceweasel/prefs.js
@@ -1,6 +1,4 @@
-# OCF config
 # Mozilla User Preferences
-
 user_pref("browser.cache.disk.capacity", 0);
 user_pref("browser.download.useDownloadDir", false);
 user_pref("browser.privatebrowsing.autostart", true);
@@ -8,4 +6,5 @@ user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
 user_pref("browser.startup.homepage", "http://www.ocf.berkeley.edu/");
 user_pref("general.smoothScroll", true);
+user_pref("network.negotiate-auth.trusted-uris", "https://rt.ocf.berkeley.edu");
 user_pref("signon.rememberSignons", false);
