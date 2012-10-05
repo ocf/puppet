a8392e62f6921fe7a0a76115060b6a555c73c4cd
Dara Adib <daradib@ocf.berkeley.edu>
2012-10-04 18:32:27 -0700

    OCF #223: use file for ssh_config instead of augeas which doesn't work

--- modules/ocf/manifests/common/ssh.pp
+++ modules/ocf/manifests/common/ssh.pp
@@ -3,27 +3,21 @@ class ocf::common::ssh {
   # install ssh client and server
   package { [ 'openssh-client', 'openssh-server' ]: }
 
-  # for readability, do not hash known_hosts file by default
-  # enable GSSAPI host key verification
-  augeas { '/etc/ssh/ssh_config':
-    context => '/files/etc/ssh/ssh_config',
-    changes => [ 
-                 'set HashKnownHosts no',
-                 'set GSSAPIAuthentication yes',
-                 'set GSSAPIKeyExchange yes',
-                 'set GSSAPIDelegateCredentials no'
-                ],
-    require => Package['openssh-client']
-  }
-
-  # provide list of ssh hosts
-  file { '/etc/ssh/ssh_known_hosts':
+  file {
+    # enable GSSAPI host key verification
+    '/etc/ssh/ssh_config':
+      source  => 'puppet:///modules/ocf/common/ssh_config',
+      require => Package['openssh-client'],
+    ;
+    # provide list of ssh hosts
+    '/etc/ssh/ssh_known_hosts':
       source  => 'puppet:///contrib/common/ssh_known_hosts',
-      require => Package['openssh-client'];
+      require => Package['openssh-client'],
+    ;
   }
 
   service { 'ssh':
-    require => Package['openssh-server']
+    require => Package['openssh-server'],
   }
 
 }
