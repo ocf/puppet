40866f4e9a96a23996f776dcc7d5e535c9f477b9
Felix Wong <waf@ocf.berkeley.edu>
2012-11-09 23:07:07 -0800

    no more zabbix binary

--- modules/ocf/manifests/common/zabbix.pp
+++ modules/ocf/manifests/common/zabbix.pp
@@ -9,16 +9,17 @@ class ocf::common::zabbix {
       source  => 'puppet:///modules/ocf/common/zabbix_agentd.conf',
       require => Package[ 'zabbix-agent' ];
     # provide newer version of zabbix binary
-    '/usr/sbin/zabbix_agentd':
-      mode    => '0755',
-      backup  => false,
-      source  => 'puppet:///contrib/common/zabbix_agentd',
-      require => Package[ 'zabbix-agent' ];
+    #'/usr/sbin/zabbix_agentd':
+    #  mode    => '0755',
+    #  backup  => false,
+    #  source  => 'puppet:///contrib/common/zabbix_agentd',
+    #  require => Package[ 'zabbix-agent' ];
   }
 
   # restart zabbix
   service { 'zabbix-agent':
-    subscribe => [ Package['zabbix-agent'], File[ '/etc/zabbix/zabbix_agentd.conf', '/usr/sbin/zabbix_agentd' ] ]
+    subscribe => [ Package['zabbix-agent'], File[ '/etc/zabbix/zabbix_agentd.conf' ] ]
+    #subscribe => [ Package['zabbix-agent'], File[ '/etc/zabbix/zabbix_agentd.conf', '/usr/sbin/zabbix_agentd' ] ]
   }
 
 }
