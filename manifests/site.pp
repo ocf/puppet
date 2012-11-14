3632ecff04f3ab6003384f69065a0db2165bb2b5
Dara Adib <daradib@ocf.berkeley.edu>
2012-11-14 14:51:06 -0800

    Cleanup site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,22 +2,24 @@
 
 # backup existing files to puppetmaster
 # path must be explicitly undefined, see puppet bug #5362
-filebucket { 'main': path => false }
+filebucket { 'main': path => false, }
+
 # create first stage to run before everything else
-stage { 'first': before => Stage['main'] }
+stage { 'first': before => Stage['main'], }
 
 ### global defaults ###
 
 # default path for executions
-Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
+Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin', }
+
 # default file permissions, follow symlinks when serving files, backup existing files to puppetmaster
-File { mode => 0644, owner => root, group => root, links => follow, backup => main }
+File { mode => 0644, owner => root, group => root, links => follow, backup => main, }
+
 # add managed filesystems to fstab by default
-Mount { ensure => defined }
+Mount { ensure => defined, }
+
 # use aptitude for package installation
-Package { provider => aptitude }
-# use init scripts
-Service { hasstatus => true, hasrestart => true }
+Package { provider => aptitude, }
 
 ### node definitions ###
 
