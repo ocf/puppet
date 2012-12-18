60a24462accc81d240bcaa8e59b1fad4c45ce46f
Dara Adib <daradib@ocf.berkeley.edu>
2012-12-18 01:20:21 -0800

    Temporary workaround to use bridge on hypervisors

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -19,10 +19,15 @@ node default {
   include ocf::common::ldap
   include ocf::common::smart
   if $::ipoctet != undef {
+    case $::hostname {
+      hal, fallingrocks, pandemic: {$bridge = true}
+      default: {$bridge = false}
+    }
     class { 'networking':
       ipaddress   => "169.229.172.$::ipoctet",
       netmask     => '255.255.255.192',
       gateway     => '169.229.172.65',
+      bridge      => $bridge,
       domain      => 'ocf.berkeley.edu',
       nameservers => ['169.229.172.66', '128.32.206.12', '128.32.136.9'],
     }
