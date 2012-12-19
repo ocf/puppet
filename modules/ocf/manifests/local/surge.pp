28ff31ae4db6b4dec1ae6b5d6345c5eda8a41d42
Dara Adib <daradib@ocf.berkeley.edu>
2012-12-18 21:39:45 -0800

    Cleanup unused fallout and surge manifests

--- modules/ocf/manifests/local/surge.pp
+++ modules/ocf/manifests/local/surge.pp
@@ -72,30 +72,4 @@ class ocf::local::surge {
     require   => User['rsync']
   }
 
-  # set up tftp for network booting
-  package { 'tftpd-hpa': }
-  file {
-    '/opt/tftp':
-      ensure  => directory;
-    '/etc/default/tftpd-hpa':
-      source  => 'puppet:///modules/ocf/local/surge/tftpd-hpa',
-      require => [ Package['tftpd-hpa'], File['/opt/tftp'] ]
-  }
-  service { 'tftpd-hpa':
-    subscribe => File[ '/opt/tftp', '/etc/default/tftpd-hpa' ]
-  }
-
-  # set up netboot image
-  package { 'pax': }
-  file {
-    '/usr/local/sbin/ocf-netboot':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf/local/surge/ocf-netboot',
-      require => Package['pax'];
-    '/etc/cron.weekly/ocf-netboot':
-      ensure  => symlink,
-      links   => manage,
-      target  => '/usr/local/sbin/ocf-netboot'
-  }
-
 }
