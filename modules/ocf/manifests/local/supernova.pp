e16fbba498b281f9a5efae368769325ab637e772
Dara Adib <daradib@ocf.berkeley.edu>
2013-01-26 22:02:07 -0800

    Add ldapvi on supernova

--- modules/ocf/manifests/local/supernova.pp
+++ modules/ocf/manifests/local/supernova.pp
@@ -1,10 +1,18 @@
 class ocf::local::supernova {
+
   package {
-    'python-twisted':
-      ;
-    'python-argparse':
-      ;
-    'python-crypto':
-      ;
+    # account creation dependecies
+    ['python-twisted', 'python-argparse', 'python-crypto']:
+    ;
+    # ldap management
+    'ldapvi':
+    ;
   }
+
+  # ldapvi config
+  file { '/etc/ldapvi.conf':
+    content => "profile default\nldap-conf: yes\nsasl-mech: GSSAPI\n",
+    require => Package['ldapvi'],
+  }
+
 }
