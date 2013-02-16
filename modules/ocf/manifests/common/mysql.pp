e558645ff6699da6cb914f673f6d0a4f00101cc3
Felix Wong <waf@ocf.berkeley.edu>
2013-02-15 22:33:30 -0800

    added mysql client package

--- modules/ocf/manifests/common/mysql.pp
+++ modules/ocf/manifests/common/mysql.pp
@@ -1,8 +1,11 @@
 class ocf::common::mysql {
 
+  package { 'mysql-client': }
+
   # MySQL client config
   file { '/etc/mysql/my.cnf':
     content => "[client]\nhost=mysql\npassword",
+    require => Package['mysql-client'],
   }
 
 }
