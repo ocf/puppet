94808d1119c3a66fac00081acca118ebc35753b1
Dara Adib <daradib@ocf.berkeley.edu>
2013-02-18 02:23:04 -0800

    Fix pkprinters path for resetting daily print quota

--- modules/ocf/files/local/printhost/reset_daily_quota.sh
+++ modules/ocf/files/local/printhost/reset_daily_quota.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
+
+pkprinters=/usr/local/bin/pkprinters
 
 rm /etc/pykota/db/daily-database.sqlite
 sqlite3 /etc/pykota/db/daily-database.sqlite < /usr/local/share/pykota/sqlite/pykota-sqlite.sql
 chown lp /etc/pykota/db/daily-database.sqlite
-pkprinters -a -c 1 logjam-double > /dev/null
-pkprinters -a -c 1 logjam-single > /dev/null
-pkprinters -a -c 1 deforestation-single > /dev/null
-pkprinters -a -c 1 deforestation-double > /dev/null
+$pkprinters -a -c 1 logjam-double > /dev/null
+$pkprinters -a -c 1 logjam-single > /dev/null
+$pkprinters -a -c 1 deforestation-single > /dev/null
+$pkprinters -a -c 1 deforestation-double > /dev/null
