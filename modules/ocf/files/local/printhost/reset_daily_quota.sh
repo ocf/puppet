a5ea67e6952e5fb09d369073a958469e65530202
Nolan Lum <nolm@berkeley.edu>
2013-03-07 13:17:05 -0800

    reset_daily_quota needs to allow ocfstaff to modify daily database.

--- modules/ocf/files/local/printhost/reset_daily_quota.sh
+++ modules/ocf/files/local/printhost/reset_daily_quota.sh
@@ -4,7 +4,8 @@ pkprinters=/usr/local/bin/pkprinters
 
 rm /etc/pykota/db/daily-database.sqlite
 sqlite3 /etc/pykota/db/daily-database.sqlite < /usr/local/share/pykota/sqlite/pykota-sqlite.sql
-chown lp /etc/pykota/db/daily-database.sqlite
+chown lp:ocfstaff /etc/pykota/db/daily-database.sqlite
+chmod g+w /etc/pykota/db/daily-database.sqlite
 $pkprinters -a -c 1 logjam-double > /dev/null
 $pkprinters -a -c 1 logjam-single > /dev/null
 $pkprinters -a -c 1 deforestation-single > /dev/null
