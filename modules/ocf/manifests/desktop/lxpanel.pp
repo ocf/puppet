597cc76ba7e4b8da1eec6d2ebbf29be214750879
Felix Wong <waf@ocf.berkeley.edu>
2013-03-16 23:17:42 -0700

    fixed lxpanel for wheezy

--- modules/ocf/manifests/desktop/lxpanel.pp
+++ modules/ocf/manifests/desktop/lxpanel.pp
@@ -1,6 +1,10 @@
 class ocf::desktop::lxpanel {
   # provide non lxpanel fail
+  if  $::lsbdistcodename == 'squeeze' {
   file { '/usr/share/lxpanel/profile/LXDE/panels/panel':
+    source => 'puppet:///modules/ocf/desktop/lxpanel.conf'
+  } else {
+  file { '/usr/share/lxpanel/profile/default/panels/panel':
     source => 'puppet:///modules/ocf/desktop/lxpanel.conf'
   }
 }
