1cc6d5e97fb93f9a85a3ad0bbd3a3c6cdf70f063
Dara Adib <daradib@ocf.berkeley.edu>
2013-03-20 05:08:29 -0700

    Move ldapvi from supernova to common manifest

--- modules/ocf/manifests/local/supernova.pp
+++ modules/ocf/manifests/local/supernova.pp
@@ -4,15 +4,6 @@ class ocf::local::supernova {
     # account creation dependecies
     ['python-twisted', 'python-argparse', 'python-crypto']:
     ;
-    # ldap management
-    'ldapvi':
-    ;
-  }
-
-  # ldapvi config
-  file { '/etc/ldapvi.conf':
-    content => "profile default\nldap-conf: yes\nsasl-mech: GSSAPI\n",
-    require => Package['ldapvi'],
   }
 
 }
