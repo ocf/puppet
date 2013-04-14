6dd9e4b59524334577e4eb5fec7d03560db60862
Felix Wong <waf@ocf.berkeley.edu>
2013-04-13 22:16:02 -0700

    added kiosk user to riot config

--- modules/ocf/manifests/local/riot.pp
+++ modules/ocf/manifests/local/riot.pp
@@ -1,39 +1,38 @@
 class ocf::local::riot {
 
-  # raspbian repository
+  package { 'pulseaudio': }
+
+  user { 'kiosk':
+      comment => 'Lab Display',
+      home    => '/opt/kiosk',
+      shell   => '/opt/kiosk/shell',
+      system  => true,
+      groups  => 'sys',
+  }
+
   file {
       '/etc/apt/sources.list.d/raspbian.list':
           ensure   => file,
-          content  => "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free\ndeb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free\n";
-      
+          content  => "deb http://archive.raspbian.org/raspbian wheezy main contrib non-free\ndeb-src http://archive.raspbian.org/raspbian wheezy main contrib non-free\n",
+      ;
       '/opt/kiosk/':
           ensure   => directory,
-          owner    => 'root',
-          group    => 'root',
-          mode     => '0755';
-
+          purge    => true,
+      ;
       '/opt/kiosk/display':
-          ensure   => file,
           source   => 'puppet:///modules/ocf/local/riot/display',
-          owner    => 'root',
-          group    => 'root',
-          mode     => '0755';
-      '/opt/kiosk/kiosk':
-          ensure   => file,
-          source   => 'puppet:///modules/ocf/local/riot/kiosk',
-          owner    => 'root',
-          group    => 'root',
-          mode     => '0755';
-
+          mode     => '0755',
+      ;
+      '/opt/kiosk/shell':
+          source   => 'puppet:///modules/ocf/local/riot/shell',
+          mode     => '0755',
+      ;
       '/etc/inittab':
-          ensure   => file,
           source   => 'puppet:///modules/ocf/local/riot/inittab',
-          owner    => 'root',
-          group    => 'root',
-          mode     => '0644';
-      
+      ;
       '/etc/cron.d/update-motd':
-          ensure   => file,
-          source   => 'puppet:///modules/ocf/local/riot/crontab';
+          source   => 'puppet:///modules/ocf/local/riot/crontab',
+      ;
   }
+
 }
