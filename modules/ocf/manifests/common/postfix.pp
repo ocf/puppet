03f15480a9243b131a73d1b49e447ff7502e9826
Dara Adib <daradib@ocf.berkeley.edu>
2013-04-19 18:19:01 -0700

    Have Postfix use OCF domain on all mail

--- modules/ocf/manifests/common/postfix.pp
+++ modules/ocf/manifests/common/postfix.pp
@@ -1,32 +1,41 @@
 class ocf::common::postfix {
 
   # remove exim
-  package { [ 'exim4', 'exim4-base', 'exim4-config', 'exim4-daemon-light' ]:
-    ensure => purged
+  package { ['exim4', 'exim4-base', 'exim4-config', 'exim4-daemon-light']:
+    ensure => purged,
+    before => Package['postfix'],
   }
 
-  # install postfix
-  package { [ 'postfix', 'bsd-mailx' ]:
-    require => Package[ 'exim4', 'exim4-base', 'exim4-config', 'exim4-daemon-light' ]
+  package {
+    'postfix':
+    ;
+    'bsd-mailx':
+      require => Package['postfix'],
+    ;
   }
 
-  # provide mail configuration files
+  # disable incoming TCP/IP connections
+  augeas { '/etc/postfix/master.cf':
+    context => "/files/etc/postfix/master.cf",
+    changes => "rm *[type='inet']",
+    notify  => Service['postfix'],
+    require => Package['postfix'],
+  }
+
+  # mail configuration
   file {
     '/etc/mailname':
-      content  => 'ocf.berkeley.edu',
-      require  => Package['postfix'];
+      content => 'ocf.berkeley.edu',
+    ;
     '/etc/postfix/main.cf':
-      content  => template('ocf/common/main.cf.erb'),
-      require  => Package['postfix'];
-    '/etc/postfix/master.cf':
-      source   => 'puppet:///modules/ocf/common/master.cf',
-      require  => Package['postfix']
+      source  => 'puppet:///modules/ocf/common/postfix/main.cf',
+      notify  => Service['postfix'],
+      require => Package['postfix'],
+    ;
   }
 
-  # start and restart postfix
   service { 'postfix':
-    subscribe => File[ '/etc/mailname', '/etc/postfix/main.cf', '/etc/postfix/master.cf' ],
-    require   => Package['postfix']
+    require => Package['postfix'],
   }
 
 }
