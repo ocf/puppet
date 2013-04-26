fda37d618179a5be84a2e6108ae36d4bb132eef3
Nolan Lum <nolm@berkeley.edu>
2013-04-26 14:48:33 -0700

    can't be bothered to fix this so remove it.

--- modules/ocf/manifests/local/supernova.pp
+++ modules/ocf/manifests/local/supernova.pp
@@ -9,8 +9,6 @@ class ocf::local::supernova {
   # receive remote syslog from tsunami
   file { '/etc/rsyslog.d/tsunami.conf':
     content => "if $fromhost startswith 'tsunami' then /var/log/tsunami.log\n& ~\n",
-    require => Package['rsyslog'],
-    notify => Service['rsyslog'],
   }
 
 }
