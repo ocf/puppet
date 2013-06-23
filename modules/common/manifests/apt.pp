9b007eaa600ee1bd0eab8e9a15b0bc17b7397e47
Dara Adib <daradib@ocf.berkeley.edu>
2013-06-23 01:05:12 -0700

    Cleanup and fix APT sources.list dependency logic
    Make sure aptitude update is executed whenever APT sources or keys change.
    mozilla.debian.net repository now supports both squeeze and wheezy.

--- modules/common/manifests/apt.pp
+++ modules/common/manifests/apt.pp
@@ -3,7 +3,7 @@ class common::apt ( $nonfree = false, $desktop = false, $kiosk = false ) {
   package { 'aptitude': }
   exec { 'aptitude update':
     refreshonly => true,
-    subscribe   => File['/etc/apt/sources.list']
+    require     => Package['aptitude'],
   }
 
   # remote package update management support
@@ -13,65 +13,56 @@ class common::apt ( $nonfree = false, $desktop = false, $kiosk = false ) {
     # provide sources.list
     '/etc/apt/sources.list':
       content => template('common/apt/sources.list.erb'),
-      require => Package['aptitude'];
-    # we previously override conffiles on package installation, not a good idea anymore
-    '/etc/apt/apt.conf.d/90conffiles':
-      ensure  => absent;
-    # update apt list, report missing updates,  and clear apt cache and old config daily
+      notify  => Exec['aptitude update'],
+      before  => File['/etc/cron.daily/ocf-apt'],
+    ;
+    # update apt list, report missing updates, and clear apt cache and old config daily
     '/etc/cron.daily/ocf-apt':
       mode    => '0755',
       content => template('common/apt/ocf-apt.erb'),
-      require => [ Package['aptitude'], File['/etc/apt/sources.list'] ];
+      require => Package['aptitude'],
+    ;
   }
 
   if $::architecture in ['amd64', 'i386'] {
     # provide puppetlabs sources.list
     file { '/etc/apt/sources.list.d/puppetlabs.list':
-      content => "deb http://apt.puppetlabs.com/ $::lsbdistcodename main dependencies",
-      require => Package['aptitude'],
-      before  => File['/etc/cron.daily/ocf-apt']
+      content => "deb http://apt.puppetlabs.com/ ${::lsbdistcodename} main dependencies",
+      notify  => Exec['aptitude update'],
+      before  => File['/etc/cron.daily/ocf-apt'],
     }
     # trust puppetlabs GPG key
     exec { 'puppetlabs':
-    command => 'wget -q https://apt.puppetlabs.com/pubkey.gpg -O- | apt-key add - && aptitude update',
-    unless  => 'apt-key list | grep 4BD6EC30',
-    require => File['/etc/apt/sources.list']
+      command => 'wget -q https://apt.puppetlabs.com/pubkey.gpg -O- | apt-key add -',
+      unless  => 'apt-key list | grep 4BD6EC30',
+      notify  => Exec['aptitude update'],
+      before  => File['/etc/cron.daily/ocf-apt'],
     }
   }
 
   if $::operatingsystem == 'Debian' and $desktop {
-    if $::lsbdistcodename == 'squeeze' {
-      # provide desktop sources.list
-      file { '/etc/apt/sources.list.d/desktop.list':
-        content => "deb http://www.deb-multimedia.org/ $::lsbdistcodename main non-free\ndeb http://mozilla.debian.net/ $::lsbdistcodename-backports iceweasel-release",
-        require => Package['aptitude'],
-        before  => File['/etc/cron.daily/ocf-apt']
-      }
-      # trust debian-multimedia and mozilla.debian.net GPG key
-      exec {
-        'debian-multimedia':
-          command => 'aptitude update && aptitude -o Aptitude::CmdLine::Ignore-Trust-Violations=true install deb-multimedia-keyring && aptitude update',
-          unless  => 'dpkg -l deb-multimedia-keyring | grep ^ii',
-          require => File['/etc/apt/sources.list','/etc/apt/sources.list.d/desktop.list'];
-        'debian-mozilla':
-          command => 'aptitude update && aptitude -o Aptitude::CmdLine::Ignore-Trust-Violations=true install pkg-mozilla-archive-keyring && aptitude update',
-          unless  => 'dpkg -l pkg-mozilla-archive-keyring | grep ^ii',
-          require => File['/etc/apt/sources.list','/etc/apt/sources.list.d/desktop.list']
-      }
-    } elsif $::lsbdistcodename == 'wheezy' {
-      # provide desktop sources.list
-      file { '/etc/apt/sources.list.d/desktop.list':
-        content => "deb http://www.deb-multimedia.org/ $::lsbdistcodename main non-free",
-        require => Package['aptitude'],
-        before  => File['/etc/cron.daily/ocf-apt']
-      }
-      # trust debian-multimedia and mozilla.debian.net GPG key
-      exec {
-        'debian-multimedia':
-          command => 'aptitude update && aptitude -o Aptitude::CmdLine::Ignore-Trust-Violations=true install deb-multimedia-keyring && aptitude update',
-          unless  => 'dpkg -l deb-multimedia-keyring | grep ^ii',
-          require => File['/etc/apt/sources.list','/etc/apt/sources.list.d/desktop.list'];
-      }
+    # provide desktop sources.list
+    file { '/etc/apt/sources.list.d/desktop.list':
+      content => "deb http://www.deb-multimedia.org/ $::lsbdistcodename main non-free\ndeb http://mozilla.debian.net/ $::lsbdistcodename-backports iceweasel-release",
+      notify  => Exec['aptitude update'],
+      before  => File['/etc/cron.daily/ocf-apt'],
+    }
+    # trust debian-multimedia and mozilla.debian.net GPG key
+    exec {
+      'debian-multimedia':
+        command => 'aptitude update && aptitude -o Aptitude::CmdLine::Ignore-Trust-Violations=true install deb-multimedia-keyring',
+        unless  => 'dpkg -l deb-multimedia-keyring | grep ^ii',
+        notify  => Exec['aptitude update'],
+        require => [Package['aptitude'], File['/etc/apt/sources.list','/etc/apt/sources.list.d/desktop.list']],
+        before  => File['/etc/cron.daily/ocf-apt'],
+      ;
+      'debian-mozilla':
+        command => 'aptitude update && aptitude -o Aptitude::CmdLine::Ignore-Trust-Violations=true install pkg-mozilla-archive-keyring',
+        unless  => 'dpkg -l pkg-mozilla-archive-keyring | grep ^ii',
+        notify  => Exec['aptitude update'],
+        require => [Package['aptitude'], File['/etc/apt/sources.list','/etc/apt/sources.list.d/desktop.list']],
+        before  => File['/etc/cron.daily/ocf-apt'],
+      ;
     }
   }
 
