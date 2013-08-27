4ebaadebc7d14c2d78754d480a445c8be5f764a4
Timmy Zhu <tzhu@ocf.berkeley.edu>
2013-08-27 01:47:34 -0700

    Add Percona apt source for maelstrom

--- modules/maelstrom/manifests/init.pp
+++ modules/maelstrom/manifests/init.pp
@@ -23,4 +23,25 @@ class maelstrom {
     require   => Package['mysql-server-5.1']
   }
 
+  # add percona repo
+  file { '/etc/apt/sources.list.d/percona.list':
+    content => "deb http://repo.percona.com/apt ${::lsbdistcodename} main",
+    before  => Exec['percona'],
+  }
+
+  # trust percona key
+  exec { 'percona':
+    command => 'apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A',
+    unless  => 'apt-key list | grep CD2EFD2A',
+    before  => Exec['force aptitude update'],
+    require => File['/etc/apt/sources.list.d/percona.list'],
+  }
+
+  # local copy to avoid dependency cycles
+  exec { 'force aptitude update':
+    command     => 'aptitude update',
+    refreshonly => true,
+    require     => Package['aptitude'],
+  }
+
 }
