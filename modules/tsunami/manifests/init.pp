2e1c7209d483325988c1d317813402030b3b6e98
Felix Wong <waf@ocf.berkeley.edu>
2013-09-02 16:43:00 -0700

    all the goodness of new tsunami

--- modules/tsunami/manifests/init.pp
+++ modules/tsunami/manifests/init.pp
@@ -1,9 +1,178 @@
 class tsunami {
 
+  # all the mounts
+  file {
+    '/services':
+      ensure  => directory;
+    '/home':
+      ensure  => directory;
+    '/opt/ocf':
+      ensure  => directory;
+    '/var/mail':
+      ensure  => directory,
+      owner   => 'root',
+      group   => 'disk',
+      mode    => '1777';
+    '/etc/pykota':
+      ensure  => directory,
+      owner   => 'nobody',
+      group   => 'nogroup',
+      mode    => '0755';
+    '/opt/httpd':
+      ensure  => directory,
+      group   => 'adm';
+  }
+
+  mount {
+    '/services':
+    require => File[ '/services' ],
+    device  => 'services:/services',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'rw,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+    '/home':
+    require => File[ '/home' ],
+    device  => 'homes:/home',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'rw,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+    '/opt/ocf':
+    require => File[ '/opt/ocf' ],
+    device  => 'opt:/i686-real',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'ro,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+    '/var/mail':
+    require => File[ '/var/mail' ],
+    device  => 'mailbox:/',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'rw,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+    '/etc/pykota':
+    require => File[ '/etc/pykota' ],
+    device  => 'printhost:/',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'ro,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+    '/opt/httpd':
+    require => File[ '/opt/httpd' ],
+    device  => 'www:/',
+    fstype  => 'nfs4',
+    atboot  => 'true',
+    options => 'ro,bg,noatime,nodev,nosuid',
+    ensure  => 'mounted';
+  }
+
   package {
-    # redirect to www and shell
+    # redirect to shell
     ['apache2']:
     ;
+
+    # web ssh
+    ['shellinabox']:
+    ;
+  }
+
+  # dem ssh keys
+  file {
+    '/etc/ssh/ssh_host_dsa_key':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      source  => 'puppet:///private/ssh_host_dsa_key';
+    '/etc/ssh/ssh_host_dsa_key.pub':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      source  => 'puppet:///private/ssh_host_dsa_key.pub';
+    '/etc/ssh/ssh_host_rsa_key':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0600',
+      source  => 'puppet:///private/ssh_host_rsa_key';
+    '/etc/ssh/ssh_host_rsa_key.pub':
+      ensure  => file,
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      source  => 'puppet:///private/ssh_host_rsa_key.pub';
+  }
+
+  # apache must subscribe to all conf files
+  service { 'apache2': }
+
+  file { '/etc/apache2/sites-available/01-ssh.conf':
+    ensure    => file,
+    source    => 'puppet:///modules/tsunami/apache/sites/ssh.conf',
+  }
+
+  file { '/etc/apache2/sites-available/02-ssl.conf':
+    ensure    => file,
+    source    => 'puppet:///modules/tsunami/apache/sites/ssl.conf',
+  }
+
+  exec { '/usr/sbin/a2enmod rewrite':
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/rewrite.load',
+    notify      => Service['apache2'],
+    require     => Package['apache2'],
+  }
+
+  exec { '/usr/sbin/a2enmod ssl':
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/ssl.load',
+    notify      => Service['apache2'],
+    require     => [ File['/etc/ssl/certs/tsunami.ocf.berkeley.edu.crt'], File['/etc/ssl/private/tsunami.ocf.berkeley.edu.key'], Package['apache2'] ],
+  }
+
+  exec { '/usr/sbin/a2enmod proxy':
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/proxy.load',
+    notify      => Service['apache2'],
+    require     => [ Package['apache2'] ],
+  }
+
+  exec { '/usr/sbin/a2enmod proxy_http':
+    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/proxy_http.load',
+    notify      => Service['apache2'],
+    require     => [ Package['apache2'] ],
+  }
+
+  exec { '/usr/sbin/a2ensite 01-ssh.conf':
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/01-ssh.conf',
+    notify      => Service['apache2'],
+    require     => [Package['apache2'], Exec['/usr/sbin/a2enmod rewrite'], File['/etc/apache2/sites-available/01-ssh.conf']],
+  }
+
+  exec { '/usr/sbin/a2ensite 02-ssl.conf':
+    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/02-ssl.conf',
+    notify      => Service['apache2'],
+    require     => [Package['apache2'], Exec['/usr/sbin/a2enmod proxy'], Exec['/usr/sbin/a2enmod proxy_http'], File['/etc/apache2/sites-available/02-ssl.conf']],
+  }
+
+  # copy ssl files
+  file {
+    '/etc/ssl/certs/tsunami.ocf.berkeley.edu.crt':
+      ensure    => file,
+      owner     => 'root',
+      group     => 'root',
+      mode      => '0600',
+      source    => 'puppet:///private/tsunami.ocf.berkeley.edu.crt';
+    '/etc/ssl/private/tsunami.ocf.berkeley.edu.key':
+      ensure    => file,
+      owner     => 'root',
+      group     => 'root',
+      source    => 'puppet:///private/tsunami.ocf.berkeley.edu.key';
+    '/etc/ssl/certs/CA-BUNDLE.CRT':
+      ensure    => file,
+      owner     => 'root',
+      group     => 'root',
+      source    => 'puppet:///private/CA-BUNDLE.CRT';
   }
 
 }
