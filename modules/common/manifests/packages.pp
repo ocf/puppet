d17746c7f11256ee9630d09aaf39dd3e5e097713
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2013-11-03 12:00:03 -0800

    Add chicken-bin package to login server.
    chicken is a small Scheme compiler and interpreter, mostly compatible
    with UCB Scheme.

--- modules/common/manifests/packages.pp
+++ modules/common/manifests/packages.pp
@@ -63,7 +63,7 @@ class common::packages( $extra = false, $login = false ) {
       # ruby
       ['rails', 'ruby-dev']:;
       # useful for CS classes
-      ['bison', 'flex', 'ipython3', 'libncurses5-dev', 'python3', 'python3-tk']:;
+      ['bison', 'flex', 'ipython3', 'libncurses5-dev', 'python3', 'python3-tk', 'chicken-bin']:;
     }
   }
 
