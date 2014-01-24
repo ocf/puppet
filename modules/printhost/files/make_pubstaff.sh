ef95a02983e24a1f3e265e5ec2aef19be59a496b
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-01-24 08:39:11 -0800

    Use full path to pkusers in make_pubstaff

--- modules/printhost/files/make_pubstaff.sh
+++ modules/printhost/files/make_pubstaff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # creates pubstaff user and grants it 500 daily page quota
-pkusers -a pubstaff
-pkusers -b +500 pubstaff
+/usr/local/bin/pkusers -a pubstaff
+/usr/local/bin/pkusers -b +500 pubstaff
