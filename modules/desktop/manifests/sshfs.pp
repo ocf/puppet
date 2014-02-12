b682f5e4de1795f0500822a60a1187fca0af61a1
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-02-12 11:50:13 -0800

    Disable pam_delay on desktops to fix lightdm freezing
    Temporary fix to prevent lightdm from freezing for ~120 seconds after an
    invalid password is entered. Disabling pam_delay avoids this.
    
    This is a temporary fix (hence it being placed in a manifest which is
    only tangentially related to the problem) which should be removed after
    the actual source of the problem is found and fixed.

--- modules/desktop/manifests/sshfs.pp
+++ modules/desktop/manifests/sshfs.pp
@@ -46,4 +46,13 @@ class desktop::sshfs {
     require => File[ '/etc/skel/remote', '/etc/pam.d/ocf-pammount' ]
   }
 
+  # "temporary" fix which seems to prevent lightdm from hanging when users
+  # attempt to log in with bad passwords
+  # (disables pam delay after failed auth attempts)
+  file {
+    '/etc/pam.d/common-auth':
+      source => 'puppet:///modules/desktop/pam/common-auth';
+    '/etc/pam.d/login':
+      source => 'puppet:///modules/desktop/pam/login';
+  }
 }
