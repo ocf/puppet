bab6f44aa26740bffff9b2850c5ed5257c787740
Dara Adib <daradib@ocf.berkeley.edu>
2014-03-17 05:39:21 -0700

    Change Iceweasel homepage and Safe Browsing to match Chrome

--- modules/desktop/files/iceweasel/prefs.js
+++ modules/desktop/files/iceweasel/prefs.js
@@ -1,10 +1,8 @@
 user_pref("browser.cache.disk.capacity", 0);
 user_pref("browser.download.useDownloadDir", false);
 user_pref("browser.privatebrowsing.autostart", true);
-user_pref("browser.safebrowsing.enabled", false);
-user_pref("browser.safebrowsing.malware.enabled", false);
-user_pref("browser.startup.homepage", "http://www.ocf.berkeley.edu/");
+user_pref("browser.startup.homepage", "https://secure.ocf.berkeley.edu/");
 user_pref("general.smoothScroll", true);
 user_pref("network.negotiate-auth.trusted-uris", "https://rt.ocf.berkeley.edu");
-user_pref("signon.rememberSignons", false);
 user_pref("pdfjs.disabled", true);
+user_pref("signon.rememberSignons", false);
