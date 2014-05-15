d495d1bba5f7f17a71d1bc7b0bba9ce72c8abbd9
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-05-15 12:08:50 -0700

    Generate desktop static DHCP leases via script

--- modules/pestilence/manifests/init.pp
+++ modules/pestilence/manifests/init.pp
@@ -2,10 +2,20 @@ class pestilence {
 
   # setup dhcp server
   package { 'isc-dhcp-server': }
-  file { '/etc/dhcp/dhcpd.conf':
-    source   => 'puppet:///modules/pestilence/dhcpd.conf',
-    require  => Package['isc-dhcp-server'],
-    notify   => Service['isc-dhcp-server']
+  file {
+    '/etc/dhcp/dhcpd.conf':
+      source   => 'puppet:///modules/pestilence/dhcpd.conf',
+      require  => [Package['isc-dhcp-server'], Exec['gen-desktop-leases']],
+      notify   => Service['isc-dhcp-server'];
+    '/usr/local/sbin/gen-desktop-leases':
+      source   => 'puppet:///modules/pestilence/gen-desktop-leases',
+      mode     => 755;
+  }
+  exec { 'gen-desktop-leases':
+    command    => '/usr/local/sbin/gen-desktop-leases > /etc/dhcp/desktop-leases.conf',
+    creates    => '/etc/dhcp/desktop-leases.conf',
+    require    => File['/usr/local/sbin/gen-desktop-leases'],
+    notify     => Service['isc-dhcp-server'];
   }
   service { 'isc-dhcp-server':
     subscribe => File['/etc/dhcp/dhcpd.conf']
