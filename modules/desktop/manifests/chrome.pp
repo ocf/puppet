b7e4cf31d82d0d4ee7fedcb344347fb6e12670b9
Dara Adib <daradib@ocf.berkeley.edu>
2014-05-18 13:52:03 -0700

    Restrict Chrome integration with Google
    Primary author Dara Adib with modifications by Chris Kuehl:
     - Always use Chrome theme (not system theme which is far too ugly)
     - Keep search suggestions enabled (too useful even though it sends
       Google your URLs)
     - Explicitly enable incognito mode for people who don't want their
       queries sent to Google
     - Keep Translate enabled (useful and not a privacy risk; strictly
       opt-in and language detection is done offline by the browser)

--- modules/desktop/manifests/chrome.pp
+++ modules/desktop/manifests/chrome.pp
@@ -1,13 +1,20 @@
 class desktop::chrome {
+
   file {
-    # policies
-    ["/etc/opt", "/etc/opt/chrome", "/etc/opt/chrome/policies", "/etc/opt/chrome/policies/managed"]:
-      ensure => directory;
-    "/etc/opt/chrome/policies/managed/ocf_policy.json":
-      source => "puppet:///modules/desktop/chrome/ocf_policy.json";
+    ['/etc/opt/chrome', '/etc/opt/chrome/policies', '/etc/opt/chrome/policies/managed']:
+      ensure  => directory,
+      require => Package['google-chrome-stable'],
+    ;
+    '/etc/opt/chrome/policies/managed/ocf_policy.json':
+      source  => 'puppet:///modules/desktop/chrome/ocf_policy.json',
+      require => Package['google-chrome-stable'],
+    ;
+    '/opt/google/chrome/master_preferences':
+      source  => 'puppet:///modules/desktop/chrome/master_preferences',
+      require => Package['google-chrome-stable'],
+    ;
   }
 
-  package {
-    ["google-chrome-stable"]:;
-  }
+  package {'google-chrome-stable':}
+
 }
