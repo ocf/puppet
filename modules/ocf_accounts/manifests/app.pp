4dc9610f627250aa232b347646ee1b5cc3833339
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-08-17 23:26:50 -0700

    ocf_accounts: clone repo, provide settings.py

--- modules/ocf_accounts/manifests/app.pp
+++ modules/ocf_accounts/manifests/app.pp
@@ -16,11 +16,16 @@ class ocf_accounts::app {
   }
 
   file {
-    '/srv/atool':
+    ['/srv/atool', '/srv/atool/env', '/srv/atool/etc']:
       ensure   => directory,
-      require  => User['atool'],
       owner    => atool,
       group    => atool;
+
+    '/srv/atool/etc/settings.py':
+      source   => 'puppet:///private/settings.py',
+      owner    => atool,
+      group    => atool,
+      mode     => 400;
 
     # supervise app with daemontools
     '/etc/service/atool':
@@ -30,5 +35,28 @@ class ocf_accounts::app {
     '/etc/service/atool/run':
       source   => 'puppet:///modules/ocf_accounts/atool/run',
       mode     => 755;
+
+    # src is a symlink to the current deployed source;
+    #
+    # puppet will initially point it at the master branch, but will not
+    # override it if changed, allowing you to point it at your own environment
+    '/srv/atool/src':
+      ensure   => link,
+      links    => manage,
+      target   => '/srv/atool/env/master',
+      replace  => false;
+
+    '/srv/atool/env/master/account_tools/settings.py':
+      ensure   => link,
+      links    => manage,
+      target   => '/srv/atool/etc/settings.py',
+      require  => Vcsrepo['/srv/atool/env/master'];
+  }
+
+  vcsrepo { '/srv/atool/env/master':
+    provider => git,
+    ensure   => latest,
+    revision => 'master',
+    source   => 'https://github.com/ocf/account-tools.git';
   }
 }
