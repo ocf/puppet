75d8377e329728808860a977486531a336f65dc9
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-10-07 22:46:37 -0700

    ocf_ssh: replace giant apache config with nginx

--- modules/ocf_ssh/manifests/webssh.pp
+++ modules/ocf_ssh/manifests/webssh.pp
@@ -1,64 +1,29 @@
 class ocf_ssh::webssh {
-  package {
-    # Reverse proxy for shellinabox
-    ['apache2']:
-    ;
+  package { ['shellinabox']:; }
 
-    # web ssh
-    ['shellinabox']:
-    ;
+  class { 'nginx':
+    # if we let nginx manage its own repo, it uses the `apt` module; this
+    # creates an unresolvable dependency cycle because we declare class `apt`
+    # in stage first (and we're currently in stage main)
+    manage_repo => false;
   }
 
-  # apache must subscribe to all conf files
-  service { 'apache2': }
-
-  file { '/etc/apache2/sites-available/01-ssh.conf':
-    ensure    => file,
-    content   => template('ocf_ssh/apache/sites/ssh.conf.erb'),
-    notify    => Service['apache2'],
-    require   => [ Package['apache2'] ],
+  nginx::resource::upstream { 'webssh':
+    members => ['localhost:4200'];
   }
 
-  file { '/etc/apache2/sites-available/02-ssl.conf':
-    ensure    => file,
-    content   => template('ocf_ssh/apache/sites/ssl.conf.erb'),
-    notify    => Service['apache2'],
-    require   => [ Package['apache2'] ],
-  }
+  nginx::resource::vhost {
+    'ssh.ocf.berkeley.edu':
+      server_name => [
+        'ssh.ocf.berkeley.edu'
+      ],
 
-  exec { '/usr/sbin/a2enmod rewrite':
-    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/rewrite.load',
-    notify      => Service['apache2'],
-    require     => Package['apache2'],
-  }
+      proxy => 'http://webssh',
 
-  exec { '/usr/sbin/a2enmod ssl':
-    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/ssl.load',
-    notify      => Service['apache2'],
-    require     => [ File["/etc/ssl/private/${::fqdn}.crt"], File["/etc/ssl/private/${::fqdn}.key"], Package['apache2'] ],
-  }
+      ssl      => true,
+      ssl_cert => "/etc/ssl/private/${::fqdn}.bundle",
+      ssl_key  => "/etc/ssl/private/${::fqdn}.key",
 
-  exec { '/usr/sbin/a2enmod proxy':
-    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/proxy.load',
-    notify      => Service['apache2'],
-    require     => [ Package['apache2'] ],
-  }
-
-  exec { '/usr/sbin/a2enmod proxy_http':
-    unless      => '/bin/readlink -e /etc/apache2/mods-enabled/proxy_http.load',
-    notify      => Service['apache2'],
-    require     => [ Package['apache2'] ],
-  }
-
-  exec { '/usr/sbin/a2ensite 01-ssh.conf':
-    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/01-ssh.conf',
-    notify      => Service['apache2'],
-    require     => [Package['apache2'], Exec['/usr/sbin/a2enmod rewrite'], File['/etc/apache2/sites-available/01-ssh.conf']],
-  }
-
-  exec { '/usr/sbin/a2ensite 02-ssl.conf':
-    unless      => '/bin/readlink -e /etc/apache2/sites-enabled/02-ssl.conf',
-    notify      => Service['apache2'],
-    require     => [Package['apache2'], Exec['/usr/sbin/a2enmod proxy'], Exec['/usr/sbin/a2enmod proxy_http'], File['/etc/apache2/sites-available/02-ssl.conf']],
+      rewrite_to_https => true;
   }
 }
