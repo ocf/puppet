266a98304ad78c99753b7afabb8685a7a2fdbf1c
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-10-16 19:22:50 -0700

    Rename blight -> ocf_wiki

--- modules/ocf_wiki/manifests/init.pp
+++ modules/ocf_wiki/manifests/init.pp
@@ -1,5 +1,4 @@
-class blight {
-
+class ocf_wiki {
   # for ikiwiki and ikiwiki search
   package { [ 'ikiwiki', 'xapian-omega', 'libsearch-xapian-perl' ]: }
   package { ['libyaml-perl']: }
@@ -25,7 +24,7 @@ class blight {
       group  => 'www-data',
       mode   => '0750';
     '/var/www/.ssh/config':
-      source => 'puppet:///modules/blight/ssh/config',
+      source => 'puppet:///modules/ocf_wiki/ssh/config',
       owner  => 'www-data',
       group  => 'www-data',
       mode   => '0640';
@@ -50,7 +49,7 @@ class blight {
       ensure  => 'directory';
   }
   file { '/srv/ikiwiki/.ikiwiki/IkiWiki/Plugin/serverlist.pm':
-    source => 'puppet:///modules/blight/ikiwiki/plugins/serverlist.pm',
+    source => 'puppet:///modules/ocf_wiki/ikiwiki/plugins/serverlist.pm',
   }
 
   # the location of the wiki public_html
@@ -80,12 +79,12 @@ class blight {
   # the config file replaces the default
   file {
     '/srv/ikiwiki/wiki.git/config':
-      source  => 'puppet:///modules/blight/ikiwiki/git_config',
+      source  => 'puppet:///modules/ocf_wiki/ikiwiki/git_config',
       owner   => 'root',
       group   => 'ocfstaff',
       mode    => '0775';
     '/srv/ikiwiki/wiki.git/hooks/post-receive':
-      source  => 'puppet:///modules/blight/ikiwiki/post-receive',
+      source  => 'puppet:///modules/ocf_wiki/ikiwiki/post-receive',
       owner   => 'www-data',
       group   => 'ocfstaff',
       mode    => '0774';
@@ -110,7 +109,7 @@ class blight {
 
   file {
     '/srv/ikiwiki/wiki/.git/hooks/post-commit':
-      source  => 'puppet:///modules/blight/ikiwiki/post-commit',
+      source  => 'puppet:///modules/ocf_wiki/ikiwiki/post-commit',
       owner   => 'www-data',
       group   => 'ocfstaff',
       mode    => '0774';
@@ -132,7 +131,7 @@ class blight {
   }
 
   file { '/srv/ikiwiki/wiki.setup':
-    source => 'puppet:///modules/blight/ikiwiki/wiki.setup',
+    source => 'puppet:///modules/ocf_wiki/ikiwiki/wiki.setup',
     owner  => 'root',
     group  => 'ocfstaff',
     mode   => '0640',
@@ -147,13 +146,13 @@ class blight {
 
   file {
     '/etc/apache2/sites-available/ikiwiki':
-      source => 'puppet:///modules/blight/apache2/ikiwiki';
+      source => 'puppet:///modules/ocf_wiki/apache2/ikiwiki';
     '/etc/apache2/sites-enabled/ikiwiki':
       ensure => symlink,
       links  => manage,
       target => '/etc/apache2/sites-available/ikiwiki';
     '/etc/apache2/sites-available/gitweb':
-      source => 'puppet:///modules/blight/apache2/gitweb';
+      source => 'puppet:///modules/ocf_wiki/apache2/gitweb';
     '/etc/apache2/sites-enabled/gitweb':
       ensure => symlink,
       links  => manage,
@@ -179,13 +178,13 @@ class blight {
     '/srv/gitweb/projects.list':
       content => 'wiki.git';
     '/etc/gitweb.conf':
-      source  => 'puppet:///modules/blight/gitweb/gitweb.conf';
+      source  => 'puppet:///modules/ocf_wiki/gitweb/gitweb.conf';
   }
 
   # old wiki (docs)
   file { '/etc/apache2/sites-available/docs':
     ensure => file,
-    source => 'puppet:///modules/blight/apache2/docs',
+    source => 'puppet:///modules/ocf_wiki/apache2/docs',
   }
   exec { '/usr/sbin/a2ensite docs':
     unless  => '/bin/readlink -e /etc/apache2/sites-enabled/docs',
@@ -193,10 +192,10 @@ class blight {
     require => File['/etc/apache2/sites-available/docs'],
   }
 
-  # default blight site
+  # default wiki site
   file { '/etc/apache2/sites-available/000-default':
     ensure => file,
-    source => 'puppet:///modules/blight/apache2/000-default',
+    source => 'puppet:///modules/ocf_wiki/apache2/000-default',
   }
   exec { '/usr/sbin/a2ensite default':
     unless  => '/bin/readlink -e /etc/apache2/sites-enabled/000-default',
