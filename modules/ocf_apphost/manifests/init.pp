d5cbd2497c84ce76c70c2eaa45581e4d43715b0c
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-10-23 21:25:02 -0700

    ocf_apphost: add daemontools for process supervision
    This is the best way I can find for users to supervise their processes.
    They can just put a line in their crontab:
      @reboot svscan ~/apps
    ...and now when biohazard is restarted, their app will be started (and
    later restarted as needed). It also makes it a lot easier to signal the
    process (svc -h ~/apps/foo for SIGHUP, etc.).
    
    One thing that isn't quite ideal is that you can't easily attach to the
    running process to see what it's spitting out on stdout/stderr (like you
    could if you ran it in screen/tmux). But competent people will be able
    to figure out how to get to their logs, and everyone else shouldn't be
    on the apphost anyway.
    
    (I'm sorry Timmy)

--- modules/ocf_apphost/manifests/init.pp
+++ modules/ocf_apphost/manifests/init.pp
@@ -2,6 +2,9 @@ class ocf_apphost {
   include common::nfs
   include proxy
 
+  # useful for supervising user processes
+  package { 'daemontools':; }
+
   file {
     '/srv/apps':
       ensure => directory,
