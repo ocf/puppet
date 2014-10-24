6ef8e10004bbdebc33c0150649b668625300c084
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-10-23 19:55:09 -0700

    ocf_apphost: add proxy and vhost config

--- modules/ocf_apphost/manifests/init.pp
+++ modules/ocf_apphost/manifests/init.pp
@@ -1,3 +1,10 @@
 class ocf_apphost {
   include common::nfs
+  include proxy
+
+  file {
+    '/srv/apps':
+      ensure => directory,
+      mode   => '0755';
+  }
 }
---
+++ modules/ocf_apphost/manifests/proxy.pp
@@ -0,0 +1,25 @@
+class ocf_apphost::proxy {
+  package { 'nginx':; }
+  service { 'nginx':
+    require => Package['nginx'];
+  }
+
+  file {
+    '/etc/nginx/sites-enabled/default':
+      ensure  => file, # originally a link
+      source  => 'puppet:///modules/ocf_apphost/default',
+      require => Package['nginx'],
+      notify  => Service['nginx'];
+
+    '/usr/local/sbin/rebuild-vhosts':
+      source  => 'puppet:///modules/ocf_apphost/rebuild-vhosts',
+      mode    => '0755';
+  }
+
+  cron {
+    'rebuild-vhosts':
+      command => '/usr/local/sbin/rebuild-vhosts > /dev/null',
+      minute  => '*/10',
+      require => Package['nginx'];
+  }
+}
