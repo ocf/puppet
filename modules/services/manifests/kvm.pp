a5647196a108fce40ea7833fef5de823b85b9354
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-10-31 22:37:17 -0700

    kvm: set libvirt default connection to qemu

--- modules/services/manifests/kvm.pp
+++ modules/services/manifests/kvm.pp
@@ -5,15 +5,24 @@ class services::kvm($group = 'ocfroot') {
   }
 
   # group access to libvirt
-  augeas { '/etc/libvirt/libvirtd.conf':
-    context => '/files/etc/libvirt/libvirtd.conf',
-    changes =>  [
-                  "set unix_sock_group ${group}",
-                  'set unix_sock_rw_perms 0770'
-                ],
-    require => [ File['/etc/nsswitch.conf'], Package['libvirt-bin'] ],
-    notify  => Service['libvirt-bin'],
+  augeas {
+    '/etc/libvirt/libvirtd.conf':
+      context => '/files/etc/libvirt/libvirtd.conf',
+      changes =>  [
+                    "set unix_sock_group ${group}",
+                    'set unix_sock_rw_perms 0770'
+                  ],
+      require => [ File['/etc/nsswitch.conf'], Package['libvirt-bin'] ],
+      notify  => Service['libvirt-bin'];
+
+    '/etc/libvirt/libvirt.conf':
+      # libvirtd lens produces a validation error for this file
+      lens    => 'Shellvars.lns',
+      incl    => '/etc/libvirt/libvirt.conf',
+      changes =>  ['set uri_default \'"qemu:///system"\''],
+      require => Package['libvirt-bin'];
   }
+
   service { 'libvirt-bin':
     require => Package['libvirt-bin']
   }
