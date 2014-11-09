0155f1aa8244ea8e4ce50c722696ee51a168574c
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-08 22:59:29 -0800

    Rename typhoon -> ocf_rt

--- modules/ocf_rt/manifests/init.pp
+++ modules/ocf_rt/manifests/init.pp
@@ -1,14 +1,14 @@
-class typhoon {
+class ocf_rt {
   include ocf_ssl
 
-  service { 'postfix': }
+  service { 'postfix':; }
 
   file {
     '/etc/postfix/main.cf':
-      source => 'puppet:///modules/typhoon/postfix/main.cf',
+      source => 'puppet:///modules/ocf_rt/postfix/main.cf',
       notify => Service['postfix'];
     '/etc/postfix/master.cf':
-      source => 'puppet:///modules/typhoon/postfix/master.cf',
+      source => 'puppet:///modules/ocf_rt/postfix/master.cf',
       notify => Service['postfix'];
   }
 }
