7ea0c27810501d5c6410cb9ed61e09df84f66ce9
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-15 15:02:24 -0800

    desktop: install patched evince to workaround bug
    This bug prevents printing of PDFs using default evince settings.
    
    See Debian bug #768133 and rt#3086 for details.

--- modules/desktop/manifests/packages.pp
+++ modules/desktop/manifests/packages.pp
@@ -11,10 +11,35 @@ class desktop::packages {
     recurse => true
   }
 
+  # install patched evince packages (version 3.14.1-1~ocf1) to get around
+  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=768133
+  package {
+    'libevdocument3-4':
+      ensure   => latest,
+      provider => dpkg,
+      source   => '/opt/share/puppet/packages/libevdocument3-4_3.14.1-1~ocf1_amd64.deb',
+      require  => File['/opt/share/puppet/packages'];
+    'libevview3-3':
+      ensure   => latest,
+      provider => dpkg,
+      source   => '/opt/share/puppet/packages/libevview3-3_3.14.1-1~ocf1_amd64.deb',
+      require  => File['/opt/share/puppet/packages'];
+    'evince-common':
+      ensure   => latest,
+      provider => dpkg,
+      source   => '/opt/share/puppet/packages/evince-common_3.14.1-1~ocf1_all.deb',
+      require  => [Package['libevview3-3'], Package['libevdocument3-4']];
+    'evince-gtk':
+      ensure   => latest,
+      provider => dpkg,
+      source   => '/opt/share/puppet/packages/evince-gtk_3.14.1-1~ocf1_amd64.deb',
+      require  => Package['evince-common'];
+  }
+
   # install a lot of other packages
   package {
     # applications
-    [ 'evince-gtk', 'claws-mail', 'geany', 'filezilla', 'inkscape', 'mssh', 'numlockx', 'remmina', 'simple-scan', 'vlc', 'zenmap', 'gimp' ]:;
+    [ 'claws-mail', 'geany', 'filezilla', 'inkscape', 'mssh', 'numlockx', 'remmina', 'simple-scan', 'vlc', 'zenmap', 'gimp' ]:;
     # desktop
     [ 'desktop-base', 'desktop-file-utils', 'gpicview', 'xarchiver', 'xterm', 'lightdm', 'accountsservice' ]:;
     # fonts
