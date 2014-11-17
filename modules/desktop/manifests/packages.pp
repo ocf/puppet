14bfd84ff93a914b5b56f923e97e41f6462860f2
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-16 20:57:06 -0800

    desktop: use superior version for evince patch
    Since 3.14.1-1~ocf1 < 3.14.1-1, upgrading packages would have
    uninstalled our locally-patched evince packages (at least until puppet
    ran and undid it).

--- modules/desktop/manifests/packages.pp
+++ modules/desktop/manifests/packages.pp
@@ -11,28 +11,28 @@ class desktop::packages {
     recurse => true
   }
 
-  # install patched evince packages (version 3.14.1-1~ocf1) to get around
+  # install patched evince packages (version 3.14.1-1+ocf1) to get around
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=768133
   package {
     'libevdocument3-4':
       ensure   => latest,
       provider => dpkg,
-      source   => '/opt/share/puppet/packages/libevdocument3-4_3.14.1-1~ocf1_amd64.deb',
+      source   => '/opt/share/puppet/packages/libevdocument3-4_3.14.1-1+ocf1_amd64.deb',
       require  => File['/opt/share/puppet/packages'];
     'libevview3-3':
       ensure   => latest,
       provider => dpkg,
-      source   => '/opt/share/puppet/packages/libevview3-3_3.14.1-1~ocf1_amd64.deb',
+      source   => '/opt/share/puppet/packages/libevview3-3_3.14.1-1+ocf1_amd64.deb',
       require  => File['/opt/share/puppet/packages'];
     'evince-common':
       ensure   => latest,
       provider => dpkg,
-      source   => '/opt/share/puppet/packages/evince-common_3.14.1-1~ocf1_all.deb',
+      source   => '/opt/share/puppet/packages/evince-common_3.14.1-1+ocf1_all.deb',
       require  => [Package['libevview3-3'], Package['libevdocument3-4']];
     'evince-gtk':
       ensure   => latest,
       provider => dpkg,
-      source   => '/opt/share/puppet/packages/evince-gtk_3.14.1-1~ocf1_amd64.deb',
+      source   => '/opt/share/puppet/packages/evince-gtk_3.14.1-1+ocf1_amd64.deb',
       require  => Package['evince-common'];
   }
 
