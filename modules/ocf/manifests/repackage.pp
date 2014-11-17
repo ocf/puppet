13c8ab0fca5c8f59a0011d8ac84b0f30a916a933
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-16 20:05:01 -0800

    ocf/repackage: improve handling of backports
    This should be a more reasonable approach to handling backports and
    packages without recommends (probably better than `dpkg -l | grep ^ii`,
    anyway).
    
    The manifest isn't super pretty because Puppet's limitations (can't
    modify/append to variables in the same scope) make writing this the
    obvious way hard/impossible.
    
    I'm open to suggestions on improving this, but I suspect this approach
    is preferable to the old one.
    
    (With that said, I was a big fan of the whole...
        $b = "-t ${dist}-backports ", $g = '| grep ~bpo'
        aptitude -y ${r}${b}install
    magic)

--- modules/ocf/manifests/repackage.pp
+++ modules/ocf/manifests/repackage.pp
@@ -1,29 +1,33 @@
 define ocf::repackage(
     $package    = $title,
-    $recommends = undef,
-    $backports  = undef,
+    $recommends = true,
+    $backports  = false,
     $dist       = $::lsbdistcodename,
   ) {
-
-  case $recommends {
-    default, undef: { $r = '' }
-    true:           { $r = '--with-recommends ' }
-    false:          { $r = '--without-recommends ' }
+  $install_options = $recommends ? {
+    true    => ['--no-install-recommends'],
+    default => []
   }
 
-  if ($dist == 'wheezy') and ($::architecture != 'armv6l') and
-  ($backports == true) {
-    $b = "-t ${dist}-backports "
-    $g = '| grep ~bpo'
-  }
-  else {
-    $b = ''
-    $g = ''
-  }
+  if $dist == 'wheezy' and $::architecture != 'armv6l' and $backports {
+    apt::pin { "bpo-${package}":
+      release  => "${dist}-backports",
+      priority => 600,
+      packages => [$package];
+    }
 
-  exec { "aptitude install ${package}":
-    command => "aptitude -y ${r}${b}install ${package}",
-    unless  => "dpkg -l ${package} | grep ^ii ${g}"
-  }
+    package { $package:
+      # in case the non-backported package is already present
+      ensure          => latest,
 
+      # even though we've pinned the package, we still need -t dist-backports
+      # so that dependencies don't hold it back
+      install_options => concat($install_options, ['-t', "${dist}-backports"]),
+      require         => Apt::Pin["bpo-${package}"];
+    }
+  } else {
+    package { $package:
+      install_options => $install_options;
+    }
+  }
 }
