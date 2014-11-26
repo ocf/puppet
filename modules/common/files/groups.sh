2eca4bff9f2871f4eb3a2eb0011939093d16a91d
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-11-25 22:31:49 -0800

    groups.sh: lower FIRST_SYSTEM_UID to 100
    ocfstaff has been renumbered to 1001, and since it was the only LDAP
    group colliding with the normal system gid range, we can use the normal
    one now (and stop breaking upgrades).
    
    We should still eventually renumber the ocf group, but that's going to
    be a lot more of a challenge.
    
    See rt#3096 for context.

--- modules/common/files/groups.sh
+++ modules/common/files/groups.sh
@@ -3,7 +3,7 @@
 # move gid range of system users and groups to prevent collisions with LDAP
 sed -i -e s/FIRST_SYSTEM_UID=.*/FIRST_SYSTEM_UID=100/g \
        -e s/LAST_SYSTEM_UID=.*/LAST_SYSTEM_UID=499/g \
-       -e s/FIRST_SYSTEM_GID=.*/FIRST_SYSTEM_GID=300/g \
+       -e s/FIRST_SYSTEM_GID=.*/FIRST_SYSTEM_GID=100/g \
        -e s/LAST_SYSTEM_GID=.*/LAST_SYSTEM_GID=499/g \
        -e s/FIRST_UID=.*/FIRST_UID=500/g \
        -e s/LAST_UID=.*/LAST_UID=999/g \
@@ -11,7 +11,7 @@ sed -i -e s/FIRST_SYSTEM_UID=.*/FIRST_SYSTEM_UID=100/g \
        -e s/LAST_GID=.*/LAST_GID=999/g /etc/adduser.conf
 
 # renumber existing system groups
-for gid in 20 110; do
+for gid in 20; do
   new_gid=`expr $gid + 100`
   sed -i "s/:$gid:/:$new_gid:/g" /etc/group
 done
