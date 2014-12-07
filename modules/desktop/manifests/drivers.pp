5f4b6c4b5208f659a2c178c0dc438a1293fec3c3
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-12-07 01:13:01 -0800

    desktop/drivers: add i386 drivers

--- modules/desktop/manifests/drivers.pp
+++ modules/desktop/manifests/drivers.pp
@@ -5,11 +5,16 @@ class desktop::drivers {
 
   # install proprietary nvidia drivers
   if $::gfx_brand == 'nvidia' {
-    package { 'nvidia-driver':; }
+    package { ['nvidia-driver', 'libgl1-nvidia-glx:i386']:; }
 
     file { '/etc/X11/xorg.conf':
       source => 'puppet:///modules/desktop/drivers/nvidia/xorg.conf',
       notify => Service['lightdm'];
     }
+  } elsif $::gfx_brand == 'intel' {
+    package { ['libgl1-mesa-glx:i386']:; }
   }
+
+  # this is used even with nvidia drivers
+  package { ['libgl1-mesa-dri:i386']:; }
 }
