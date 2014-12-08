f3ce9a6b8c1426804076fa829334f3c09544b001
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-12-08 15:24:06 -0800

    desktop/steam: install from local package
    There's a bug (Debian bug#772598) preventing preseeding acceptance of
    license answers in the current steam package.
    
    We use a custom-built package with my patch to avoid that. We can remove
    a lot of the crappy exec code once we switch to using apt (either
    because we finally set up our own apt repo, or because the bug gets
    fixed in Debian).

--- modules/desktop/manifests/steam.pp
+++ modules/desktop/manifests/steam.pp
@@ -1,3 +1,32 @@
 class desktop::steam {
-  package { ['steam:i386', 'libc6:i386']:; }
+  package {
+    ['libc6:i386', 'libxinerama1:i386']:;
+
+    # we use a custom-built package since otherwise we can't preseed license
+    # acceptance (see https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=772598)
+    'steam':
+      provider     => dpkg,
+      source       => '/opt/share/puppet/packages/steam_1.0.0.49-1+ocf1_i386.deb',
+
+      # dpkg doesn't support responsefile, but this will be useful when we
+      # switch back to apt provider
+      # responsefile => '/var/cache/debconf/steam.preseed',
+
+      require      => [Exec['accept-steam-license'], Package['libc6:i386', 'libxinerama1:i386']];
+
+    'steam-installer':
+      ensure => purged;
+  }
+
+  file {
+    '/var/cache/debconf/steam.preseed':
+      source => 'puppet:///modules/desktop/steam.preseed';
+  }
+
+  # we only need to do this because we use dpkg instead of apt
+  exec { 'accept-steam-license':
+    command     => 'debconf-set-selections /var/cache/debconf/steam.preseed',
+    unless      => 'dpkg -l steam | grep ^ii',
+    require     => File['/var/cache/debconf/steam.preseed'];
+  }
 }
