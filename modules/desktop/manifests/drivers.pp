9ee1c18907789bbc637189fc38ea6850b579511a
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-12-11 13:24:46 -0800

    desktop: don't restart lightdm on drivers changes
    While restarting lightdm is probably the appropriate step in these
    cases, it will mercilessly kill any current user sessions.
    
    Since driver installs are essentially a one-time thing, we'll just
    handle the restarts manually (we have to do a few restarts during
    desktop bootstrapping anyway).

--- modules/desktop/manifests/drivers.pp
+++ modules/desktop/manifests/drivers.pp
@@ -1,15 +1,10 @@
 class desktop::drivers {
-  service { 'lightdm':
-    require => Package['lightdm'];
-  }
-
   # install proprietary nvidia drivers
   if $::gfx_brand == 'nvidia' {
     package { ['nvidia-driver', 'libgl1-nvidia-glx:i386']:; }
 
     file { '/etc/X11/xorg.conf':
-      source => 'puppet:///modules/desktop/drivers/nvidia/xorg.conf',
-      notify => Service['lightdm'];
+      source => 'puppet:///modules/desktop/drivers/nvidia/xorg.conf';
     }
   } elsif $::gfx_brand == 'intel' {
     package { ['libgl1-mesa-glx:i386']:; }
