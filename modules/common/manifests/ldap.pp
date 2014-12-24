c3d0df38a23a03bf7f5d0a698687faa2ec21757a
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2014-12-24 13:21:14 -0800

    common/ldap: cleanup manifest, remove old cacert
    We now use a certificate signed by an actual CA (see 7ed95de8dc), so
    don't need to provide the old self-signed cacert.pem.

--- modules/common/manifests/ldap.pp
+++ modules/common/manifests/ldap.pp
@@ -1,24 +1,13 @@
 class common::ldap {
-
-  # LDAP packages
-  package { [ 'ldap-utils', 'openssl' ]: }
+  package { ['ldap-utils', 'openssl']:; }
 
   file {
-    # provide LDAP connection config
     '/etc/ldap.conf':
       source  => 'puppet:///modules/common/auth/ldap/ldap.conf',
-      require => [ Package['ldap-utils'], File['/etc/ldap/cacert.pem'] ]
-    ;
+      require => Package['ldap-utils'];
+
     '/etc/ldap/ldap.conf':
       ensure  => symlink,
-      links   => manage,
-      target  => '/etc/ldap.conf',
-    ;
-    # provide LDAP CA certificate
-    '/etc/ldap/cacert.pem':
-      source  => 'puppet:///modules/common/auth/ldap/cacert.pem',
-      require => Package['openssl'],
-    ;
+      target  => '/etc/ldap.conf';
   }
-
 }
