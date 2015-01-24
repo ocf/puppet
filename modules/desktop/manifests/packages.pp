002fbacb948471b66497651d62274cb28f7187c6
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-23 20:20:51 -0800

    desktop/{packages,steam}: use internal apt server

--- modules/desktop/manifests/packages.pp
+++ modules/desktop/manifests/packages.pp
@@ -7,37 +7,6 @@ class desktop::packages {
       ensure => purged;
   }
 
-  file { '/opt/share/puppet/packages':
-    ensure  => directory,
-    source  => 'puppet:///contrib/desktop/packages',
-    recurse => true
-  }
-
-  # install patched evince packages (version 3.14.1-1+ocf1) to get around
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=768133
-  package {
-    'libevdocument3-4':
-      ensure   => latest,
-      provider => dpkg,
-      source   => '/opt/share/puppet/packages/libevdocument3-4_3.14.1-1+ocf1_amd64.deb',
-      require  => File['/opt/share/puppet/packages'];
-    'libevview3-3':
-      ensure   => latest,
-      provider => dpkg,
-      source   => '/opt/share/puppet/packages/libevview3-3_3.14.1-1+ocf1_amd64.deb',
-      require  => File['/opt/share/puppet/packages'];
-    'evince-common':
-      ensure   => latest,
-      provider => dpkg,
-      source   => '/opt/share/puppet/packages/evince-common_3.14.1-1+ocf1_all.deb',
-      require  => [Package['libevview3-3'], Package['libevdocument3-4']];
-    'evince-gtk':
-      ensure   => latest,
-      provider => dpkg,
-      source   => '/opt/share/puppet/packages/evince-gtk_3.14.1-1+ocf1_amd64.deb',
-      require  => Package['evince-common'];
-  }
-
   # install packages specific to desktops
   #
   # in general, prefer to install packages to common::packages so that they are
@@ -48,7 +17,7 @@ class desktop::packages {
   # server (such as gimp)
   package {
     # applications
-    ['claws-mail', 'geany', 'filezilla', 'inkscape', 'mssh', 'numlockx', 'remmina', 'simple-scan', 'vlc', 'zenmap', 'gimp', 'gparted']:;
+    ['claws-mail', 'geany', 'filezilla', 'inkscape', 'mssh', 'numlockx', 'remmina', 'simple-scan', 'vlc', 'zenmap', 'gimp', 'gparted', 'evince-gtk']:;
     # desktop
     ['desktop-base', 'desktop-file-utils', 'gpicview', 'xarchiver', 'xterm', 'lightdm', 'accountsservice']:;
     # fonts
