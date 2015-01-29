dad46461541de03b5f0c6ebd7c4abb28fda16ec8
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-01-28 22:13:54 -0800

    ocf_ssh/makeservices: update makemysql path

--- modules/ocf_ssh/manifests/makeservices.pp
+++ modules/ocf_ssh/manifests/makeservices.pp
@@ -1,6 +1,6 @@
 class ocf_ssh::makeservices {
   # enable regular users to run makemysql.py as mysql
   file { '/etc/sudoers.d/makeservices':
-    content => "ALL ALL=(mysql) NOPASSWD: /opt/ocf/packages/makeservices/bin/makemysql.py\n";
+    content => "ALL ALL=(mysql) NOPASSWD: /opt/share/utils/makeservices/makemysql-real\n";
   }
 }
