1f0866b80cc52d7aeeaf917c23e9cd73638cb96a
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-02-16 01:58:05 -0800

    groups.sh: don't renumber existing groups

--- modules/common/files/groups.sh
+++ modules/common/files/groups.sh
@@ -9,9 +9,3 @@ sed -i -e s/FIRST_SYSTEM_UID=.*/FIRST_SYSTEM_UID=100/g \
        -e s/LAST_UID=.*/LAST_UID=999/g \
        -e s/FIRST_GID=.*/FIRST_GID=500/g \
        -e s/LAST_GID=.*/LAST_GID=999/g /etc/adduser.conf
-
-# renumber existing system groups
-for gid in 20; do
-  new_gid=`expr $gid + 100`
-  sed -i "s/:$gid:/:$new_gid:/g" /etc/group
-done
