6959643760bd1b591bd22688449851597c04d7d7
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-03-11 12:07:50 -0700

    ocf_stats: clean up ca manifest

--- modules/ocf_stats/manifests/ca.pp
+++ modules/ocf_stats/manifests/ca.pp
@@ -1,52 +1,31 @@
 class ocf_stats::ca {
   file {
-    '/etc/ssl/stats':
+    ['/etc/ssl/stats', '/etc/ssl/stats/ca', '/etc/ssl/stats/ca/certs', '/etc/ssl/stats/ca/crl']:
       ensure => directory,
-      owner  => root,
-      group  => root,
       mode   => '0755';
-    '/etc/ssl/stats/ca':
-      ensure => directory,
-      owner  => root,
-      group  => root,
-      mode   => '0755';
-    '/etc/ssl/stats/ca/certs':
-      ensure => directory,
-      owner  => root,
-      group  => root,
-      mode   => '0755';
-    '/etc/ssl/stats/ca/crl':
-      ensure => directory,
-      owner  => root,
-      group  => root,
-      mode   => '0755';
+
     '/etc/ssl/stats/ca/openssl.cnf':
       source => 'puppet:///modules/ocf_stats/ca/openssl.cnf',
-      owner  => root,
-      group  => root,
       mode   => '0644';
+
     '/etc/ssl/stats/ca/ca.key':
-      owner  => root,
-      group  => root,
       mode   => '0400',
       source => 'puppet:///private/stats/ca.key';
+
     '/etc/ssl/stats/ca/ca.crt':
-      owner  => root,
-      group  => root,
       mode   => '0444',
       source => 'puppet:///private/stats/ca.crt';
+
     '/etc/ssl/stats/ca/create-cert.sh':
       source => 'puppet:///modules/ocf_stats/ca/create-cert.sh',
-      owner  => root,
-      group  => root,
       mode   => '0755';
+
     '/etc/ssl/private/stats.crt':
       ensure => link,
-      links  => manage,
       target => '/etc/ssl/stats/ca/certs/stats.ocf.berkeley.edu/stats.ocf.berkeley.edu.crt';
+
     '/etc/ssl/private/stats.key':
       ensure => link,
-      links  => manage,
       target => '/etc/ssl/stats/ca/certs/stats.ocf.berkeley.edu/stats.ocf.berkeley.edu.key';
   }
 }
