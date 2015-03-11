b9c658ef12e11c9ea146bf17c01b6d401ae93462
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-03-10 23:18:37 -0700

    ocf/webhook: add secrets directory
    This also moves services into a `services` subdirectory, since we manage
    those recursively.
    
    Eventually it would be a nicer API if it accepted `secretpath` which
    could be a Puppet resource like `puppet:///private/mysecret` but that
    requires a bit more work (need some way to disambiguate multiple
    webhooks; maybe by path? -- but that requires some escaping first, so
    it's not totally trivial). With that change, it wouldn't be necessary to
    first copy a secretfile manually, either.

--- modules/ocf/manifests/webhook.pp
+++ modules/ocf/manifests/webhook.pp
@@ -13,8 +13,21 @@ define ocf::webhook($service, $command, $secretfile, $path = $title, $owner = 'r
 
   ensure_resource('file', '/opt/share/webhook', {
       ensure  => directory,
+      mode    => '0755'
+    }
+  )
+
+  ensure_resource('file', '/opt/share/webhook/services', {
+      ensure  => directory,
       source  => 'puppet:///modules/ocf/webhook',
       recurse => true,
+      mode    => '0755'
+    }
+  )
+
+  # TODO: copy secrets from secretpath to secrets automatically
+  ensure_resource('file', '/opt/share/webhook/secrets', {
+      ensure  => directory,
       mode    => '0755'
     }
   )
