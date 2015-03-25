71752e98515e489992d4dc66920c7b353c17fb94
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-03-25 01:47:58 -0700

    common/rootpw: use a single root password
    Per discussion in rt#51 (woah, 2-digits!), we will stop having multiple
    root passwords.
    
    In practice nothing really changes (all the root passwords were already
    the same, we just set them individually to the same thing...).
    
    There is an accompanying commit in utils:
    https://github.com/ocf/utils/commit/ccabd0d6584f1

--- modules/common/manifests/rootpw.pp
+++ modules/common/manifests/rootpw.pp
@@ -1,5 +1,12 @@
+# We use a single root password across all OCF machines. We use a stupid number
+# of iterations of SHA-512 so that the hash itself is not valuable.
+#
+# The root password is a last resort that is only used in rare cases where
+# there is no other option.
+#
+# To regenerate the root password, see /opt/puppet/scripts/gen-rootpw
 class common::rootpw {
   user { 'root':
-    password => file("/opt/puppet/shares/private/${::hostname}/rootpw")
+    password => file('/opt/puppet/shares/private/rootpw')
   }
 }
