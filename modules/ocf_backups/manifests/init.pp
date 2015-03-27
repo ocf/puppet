ae708869f1decd39bedae1b09d803cd8d257a906
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-03-27 03:04:42 -0700

    ocf_backups: use ocfbackups user for remote rsync
    Currently, we only backup crontabs from tsunami, but we will very soon
    add more things on different servers. (There's a partial list on the
    wiki.)

--- modules/ocf_backups/manifests/init.pp
+++ modules/ocf_backups/manifests/init.pp
@@ -5,5 +5,10 @@ class ocf_backups {
   file {
     '/opt/share/backups':
       ensure => directory;
+
+    # keytab for ocfbackups user, used to rsync from remote servers
+    '/opt/share/backups/ocfbackups.keytab':
+      source => 'puppet:///private/ocfbackups.keytab',
+      mode   => '0600';
   }
 }
