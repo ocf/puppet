9936e3bec40e853f4d96a7bf5710f6cd71d6bcf7
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-04-05 20:55:57 -0700

    ocf_wiki: major cleanup, remove old wiki
    This is a cleanup of the existing ikiwiki installation:
    
      * Switches to Apache module to vastly simplify Apache config
    
      * Stops providing old wiki (will provide these from death instead,
        like PMA)
    
      * Removes tons of unnecessary ikiwiki setup code and unused plugins
    
      * Fixes some glitchy ikiwiki behaviors in the process by properly
        disabling CGI

--- modules/ocf_wiki/manifests/init.pp
+++ modules/ocf_wiki/manifests/init.pp
@@ -1,150 +1,85 @@
 class ocf_wiki {
   include ocf_ssl
 
-  # for ikiwiki and ikiwiki search
-  package { [ 'ikiwiki', 'xapian-omega', 'libsearch-xapian-perl' ]: }
-  package { ['libyaml-perl']: }
+  package { 'ikiwiki':; }
 
-  # for old wiki
-  package { ['php5', 'php5-cli', 'php5-mysql']: }
-
-  # the location of ikwiki
-  file { '/srv/ikiwiki':
-    ensure => 'directory',
-    owner  => 'root',
-    group  => 'ocfstaff',
-    mode   => '0775',
-  }
-
-  # the serverlist ikiwiki plugin needs to be in a certain folder
   file {
-    '/srv/ikiwiki/.ikiwiki/IkiWiki/Plugin':
-      ensure  => 'directory',
-      owner   => 'root',
-      group   => 'ocfstaff',
-      recurse => true,
-      mode    => '0775';
-    '/srv/ikiwiki/.ikiwiki/IkiWiki':
-      ensure  => 'directory';
-    '/srv/ikiwiki/.ikiwiki':
-      ensure  => 'directory';
-  }
-  file { '/srv/ikiwiki/.ikiwiki/IkiWiki/Plugin/serverlist.pm':
-    source => 'puppet:///modules/ocf_wiki/ikiwiki/plugins/serverlist.pm',
-  }
-
-  # the location of the wiki public_html
-  file {
-    '/srv/ikiwiki/public_html':
+    ['/srv/wiki', '/srv/wiki/webhook']:
       ensure => directory,
-      owner  => root,
-      group  => root,
-      mode   => '0755';
+      mode   => '0775';
 
-    '/srv/ikiwiki/public_html/wiki':
-      ensure  => 'directory',
-      owner   => 'www-data',
-      mode    => '0755';
+    ['/srv/wiki/wiki', '/srv/wiki/public_html']:
+      ensure => directory,
+      group  => 'www-data',
+      mode   => '0775';
 
-    '/srv/ikiwiki/public_html/webhook':
-      ensure  => directory,
-      owner   => root,
-      mode    => '0755';
-  }
-
-  ocf::webhook { '/srv/ikiwiki/public_html/webhook/github.cgi':
-    service    => 'github',
-    secretfile => '/opt/share/webhook/secrets/github.secret',
-    command    => '/srv/ikiwiki/rebuild-wiki';
-  }
-
-  file {
-    '/srv/ikiwiki/wiki.git':
-      ensure  => 'directory',
-      owner   => 'www-data';
-  }
-
-  # the lockfile is necessary for 'ikiwiki --setup'
-  file {
-    '/srv/ikiwiki/wiki':
-      ensure  => 'directory',
-      owner   => 'www-data';
-  }
-  file {
-    '/srv/ikiwiki/wiki/.ikiwiki':
-      ensure  => 'directory',
-      owner   => 'www-data',
-      mode    => '0775';
-  }
-
-  exec { 'ikiwiki_setup':
-    require => File[ '/srv/ikiwiki/wiki.setup'],
-    command => 'ikiwiki --setup wiki.setup',
-    creates => '/srv/ikiwiki/public_html/wiki',
-    cwd     => '/srv/ikiwiki',
-  }
-
-  exec { 'refresh_ikiwiki_setup':
-    require     => File[ '/srv/ikiwiki/wiki.setup'],
-    command     => 'ikiwiki --setup wiki.setup',
-    cwd         => '/srv/ikiwiki',
-    subscribe   => File['/srv/ikiwiki/wiki.setup'],
-    refreshonly => true,
-  }
-
-  file {
-    # holds no secrets
-    '/srv/ikiwiki/wiki.setup':
-      source => 'puppet:///modules/ocf_wiki/ikiwiki/wiki.setup',
-      owner  => root,
-      group  => root,
+    '/srv/wiki/wiki.setup':
+      source => 'puppet:///modules/ocf_wiki/wiki.setup',
       mode   => '0644';
 
-    '/srv/ikiwiki/rebuild-wiki':
+    '/srv/wiki/rebuild-wiki':
       source => 'puppet:///modules/ocf_wiki/rebuild-wiki',
-      owner  => root,
       mode   => '0755';
 
     '/opt/share/webhook/secrets/github.secret':
       source => 'puppet:///private/github.secret',
-      owner  => root,
       group  => www-data,
       mode   => '0640';
   }
 
-  service { 'apache2':
-    subscribe => File[ '/etc/apache2/sites-available/ikiwiki',
-      '/etc/apache2/sites-enabled/ikiwiki'];
+  exec { 'rebuild-wiki':
+    command   => '/srv/wiki/rebuild-wiki',
+    user      => www-data,
+    subscribe => File['/srv/wiki/wiki.setup'],
+    require   => [
+      Package['ikiwiki'],
+      File['/srv/wiki/wiki.setup', '/srv/wiki/rebuild-wiki', '/srv/wiki/wiki'],
+      Apache::Vhost['wiki.ocf.berkeley.edu']];
   }
 
-  file {
-    '/etc/apache2/sites-available/ikiwiki':
-      source => 'puppet:///modules/ocf_wiki/apache2/ikiwiki';
-    '/etc/apache2/sites-enabled/ikiwiki':
-      ensure => symlink,
-      links  => manage,
-      target => '/etc/apache2/sites-available/ikiwiki';
+  ocf::webhook { '/srv/wiki/webhook/github.cgi':
+    service    => 'github',
+    secretfile => '/opt/share/webhook/secrets/github.secret',
+    command    => '/srv/wiki/rebuild-wiki';
   }
 
-  # old wiki (docs)
-  file { '/etc/apache2/sites-available/docs':
-    ensure => file,
-    source => 'puppet:///modules/ocf_wiki/apache2/docs',
-  }
-  exec { '/usr/sbin/a2ensite docs':
-    unless  => '/bin/readlink -e /etc/apache2/sites-enabled/docs',
-    notify  => Service['apache2'],
-    require => File['/etc/apache2/sites-available/docs'],
+  class { '::apache':
+    default_vhost => false;
   }
 
-  # default wiki site
-  file { '/etc/apache2/sites-available/000-default':
-    ensure => file,
-    source => 'puppet:///modules/ocf_wiki/apache2/000-default',
-  }
-  exec { '/usr/sbin/a2ensite default':
-    unless  => '/bin/readlink -e /etc/apache2/sites-enabled/000-default',
-    notify  => Service['apache2'],
-    require => File['/etc/apache2/sites-available/000-default'],
+  apache::vhost {
+    'wiki.ocf.berkeley.edu-redirect':
+      servername      => 'wiki.ocf.berkeley.edu',
+      serveraliases   => ['wiki'],
+      port            => 80,
+      docroot         => '/var/www',
+      redirect_status => 301,
+      redirect_dest   => 'https://wiki.ocf.berkeley.edu/';
+
+    'wiki.ocf.berkeley.edu':
+      servername      => 'wiki.ocf.berkeley.edu',
+      port            => 443,
+      docroot         => '/srv/wiki/public_html',
+
+      aliases => [{
+        alias => '/webhook',
+        path  => '/srv/wiki/webhook'
+      }],
+
+      directories => [{
+        path        => '/srv/wiki/webhook',
+        options     => ['ExecCGI'],
+        addhandlers => [{
+          handler    => 'cgi-script',
+          extensions => ['.cgi']
+        }]
+      }],
+
+      ssl             => true,
+      ssl_key         => "/etc/ssl/private/${::fqdn}.key",
+      ssl_cert        => "/etc/ssl/private/${::fqdn}.crt",
+      ssl_chain       => '/etc/ssl/certs/incommon-intermediate.crt',
+
+      headers         => 'set Strict-Transport-Security "max-age=31536000"';
   }
 }
