dfcc06da530972c825db61f03b713acf63ed566c
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-06-16 22:18:35 -0700

    Add ocf_ns for hosting DNS

---
+++ modules/ocf_ns/manifests/init.pp
@@ -0,0 +1,28 @@
+class ocf_ns {
+  package { 'bind9':; }
+  service { 'bind9':
+    require => Package['bind9'];
+  }
+
+  file {
+    '/etc/bind/named.conf.options':
+      source  => 'puppet:///modules/ocf_ns/named.conf.options',
+      require => Package['bind9'],
+      notify  => Service['bind9'];
+
+    '/etc/bind/named.conf.local':
+      ensure  => link,
+      target  => '/srv/dns/named.conf.local',
+      require => [Package['bind9'], Vcsrepo['/srv/dns']],
+      notify  => Service['bind9'];
+  }
+
+  vcsrepo { '/srv/dns':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://github.com/ocf/dns.git',
+    require  => Package['bind9'],
+    notify   => Service['bind9'];
+  }
+}
