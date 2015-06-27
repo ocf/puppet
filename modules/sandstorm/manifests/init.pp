0b0e932ff2104cedefbd5f646cb37258535bf96a
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-06-27 00:40:46 -0700

    sandstorm: don't install apache or php
    We don't use these anymore. We're just trying to keep sandstorm hobbling
    along for a bit longer while we work on a replacement.

--- modules/sandstorm/manifests/init.pp
+++ modules/sandstorm/manifests/init.pp
@@ -2,15 +2,4 @@ class sandstorm {
   include ocf_ssl
   include ocf::acct
   include ocf::limits
-
-  package {
-    [ 'apache2' ]:
-    ;
-    # php
-    ['php5', 'php5-mysql', 'libapache2-mod-php5', 'php5-mcrypt']:
-    ;
-  }
-
-  # apache must subscribe to all conf files
-  service { 'apache2': }
 }
