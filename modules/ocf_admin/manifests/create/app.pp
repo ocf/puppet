bc8a974bb9e77d49bab23af6cfcee3dac4fd759b
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-08-18 12:24:12 -0700

    Update ocf_admin for new create

--- modules/ocf_admin/manifests/create/app.pp
+++ modules/ocf_admin/manifests/create/app.pp
@@ -5,22 +5,64 @@ class ocf_admin::create::app {
     require => Package['ocf-create'];
   }
 
+  # these get shoved into URIs, and we can't deal with escaping
   $redis_password = file('/opt/puppet/shares/private/create/redis-password')
   validate_re($redis_password, '^[a-zA-Z0-9]*$', 'Bad Redis password')
+  $mysql_password = file('/opt/puppet/shares/private/create/mysql-password')
+  validate_re($mysql_password, '^[a-zA-Z0-9]*$', 'Bad MySQL password')
 
   $broker = "redis+socket://:${redis_password}@/var/run/redis/redis.sock"
   $backend = $broker
 
-
   augeas { '/etc/ocf-create/ocf-create.conf':
-    lens    => 'Puppet.lns',
-    incl    => '/etc/ocf-create/ocf-create.conf',
-    changes =>  [
+    lens      => 'Puppet.lns',
+    incl      => '/etc/ocf-create/ocf-create.conf',
+    changes   =>  [
+      "set mysql/uri mysql+pymysql://ocfcreate:${mysql_password}@mysql/ocfcreate",
       "set celery/broker ${broker}",
       "set celery/backend ${backend}",
     ],
     show_diff => false,
-    notify  => Service['ocf-create'],
-    require => Package['ocf-create'];
+    notify    => Service['ocf-create'],
+    require   => Package['ocf-create'];
+  }
+
+  File {
+    require => Package['ocf-create'],
+  }
+
+  file {
+    # TODO: ideally this file wouldn't be directly readable by staff
+    '/etc/ocf-create/ocf-create.conf':
+      owner  => create,
+      group  => approve,
+      mode   => '0440';
+
+    '/etc/ocf-create/create.keytab':
+      owner  => create,
+      group  => root,
+      mode   => '0400',
+      source => 'puppet:///private/create.keytab';
+
+    '/etc/ocf-create/create.key':
+      owner  => create,
+      group  => root,
+      mode   => '0400',
+      source => 'puppet:///private/create.key';
+
+    '/etc/ocf-create/create.pub':
+      owner  => create,
+      group  => root,
+      mode   => '0444',
+      source => 'puppet:///private/create.pub';
+
+    '/etc/sudoers.d/create':
+      mode   => '0440',
+      source => 'puppet:///modules/ocf_admin/create.sudoers';
+
+    # TODO: is this link used by anything?
+    '/opt/acct/sorry':
+      ensure => link,
+      target => '/opt/share/utils/staff/acct/sorry';
   }
 }
--- modules/ocf_admin/manifests/init.pp
+++ modules/ocf_admin/manifests/init.pp
@@ -6,12 +6,6 @@ class ocf_admin {
   include apt_dater
   include create
 
-  package {
-    # remove accidentally-installed packages
-    ['php5', 'libapache2-mod-php5', 'apache2']:
-      ensure => purged;
-  }
-
   class { 'ocf::nfs':
     pykota => true,
     cron   => true,
@@ -20,66 +14,5 @@ class ocf_admin {
 
   class { 'ocf::docker':
     admin_group => 'ocfroot';
-  }
-
-  service { 'rsyslog': }
-
-  user { 'atool':
-    comment  => 'OCF Account Creation',
-    gid      => approve,
-    home     => '/srv/atool',
-    system   => true,
-    groups   => ['sys'];
-  }
-
-  # log directory, crontab, and keytab for create
-  file {
-    '/opt/create/private':
-      ensure  => 'directory',
-      owner   => 'atool',
-      group   => 'approve',
-      mode    => '0750';
-
-    '/opt/create/private/backup':
-      ensure  => 'directory',
-      owner   => 'atool',
-      group   => 'approve',
-      mode    => '0750';
-
-    '/etc/cron.d/create':
-      source => 'puppet:///modules/ocf_admin/create.cron';
-
-    '/opt/create/private/create.keytab':
-      owner  => 'atool',
-      group  => 'approve',
-      mode   => '0400',
-      source => 'puppet:///private/create.keytab';
-
-    '/opt/create/private/mid_approved.users':
-      ensure => 'file',
-      owner  => 'atool',
-      group  => 'approve',
-      mode   => '0640';
-
-    '/opt/create/private/private_pass.pem':
-      ensure => 'file',
-      owner  => 'atool',
-      group  => 'approve',
-      mode   => '0400',
-      source => 'puppet:///private/private_pass.pem';
-
-    '/etc/sudoers.d/atool':
-      mode   => '0440',
-      source => 'puppet:///modules/ocf_admin/atool.sudoers';
-
-    '/opt/acct/sorry':
-      ensure => link,
-      target => '/opt/share/utils/staff/acct/sorry';
-  }
-
-  # provide logrotate rule for account creation scripts
-  file { '/etc/logrotate.d/account-creation':
-    ensure => file,
-    source => 'puppet:///modules/ocf_admin/logrotate/account-creation';
   }
 }
