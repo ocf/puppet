c4a2969de09ef31acecc524e5994d6d4c0a13681
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-09-19 00:52:37 -0700

    ocf_desktop: switch home page from lab temporarily

--- modules/ocf_desktop/files/iceweasel/prefs.js
+++ modules/ocf_desktop/files/iceweasel/prefs.js
@@ -1,7 +1,7 @@
 user_pref("browser.cache.disk.capacity", 0);
 user_pref("browser.download.useDownloadDir", false);
 user_pref("browser.privatebrowsing.autostart", true);
-user_pref("browser.startup.homepage", "https://hello.ocf.berkeley.edu/lab.html");
+user_pref("browser.startup.homepage", "https://www.ocf.berkeley.edu/");
 user_pref("general.smoothScroll", true);
 user_pref("network.negotiate-auth.trusted-uris", "https://rt.ocf.berkeley.edu");
 user_pref("pdfjs.disabled", true);
