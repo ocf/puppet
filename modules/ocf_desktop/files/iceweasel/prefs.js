36aad29d8a0bc4274eaff0c059f1a9051ddfb16f
Kevin Peng <kpengboy@ocf.berkeley.edu>
2015-09-22 21:34:32 -0700

    Iceweasel: Don't make Yahoo the default search engine
    Trying to directly set browser.search.defaultenginename.US doesn't work
    because Iceweasel overwrites it with what it thinks that should be upon first
    launch

--- modules/ocf_desktop/files/iceweasel/prefs.js
+++ modules/ocf_desktop/files/iceweasel/prefs.js
@@ -6,3 +6,4 @@ user_pref("general.smoothScroll", true);
 user_pref("network.negotiate-auth.trusted-uris", "https://rt.ocf.berkeley.edu");
 user_pref("pdfjs.disabled", true);
 user_pref("signon.rememberSignons", false);
+user_pref("browser.search.geoSpecificDefaults", false);
