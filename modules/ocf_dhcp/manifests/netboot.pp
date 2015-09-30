d9acbdd9f780634250bbd02382dd31becab14dcd
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-09-30 14:21:24 -0700

    ocf_dhcp: serve Finnix via PXE-boot
    This is not great because sadly Finnix's netboot support requires
    booting Finnix and then running a command to generate the initrd.
    
    At least it works well...

--- modules/ocf_dhcp/manifests/netboot.pp
+++ modules/ocf_dhcp/manifests/netboot.pp
@@ -23,12 +23,27 @@ class ocf_dhcp::netboot {
       mode    => '0755',
       source  => 'puppet:///modules/ocf_dhcp/netboot/ocf-netboot',
       require => Package['pax'];
+
+    # Unfortunately, there is no good way to get a finnix tarball via script.
+    # The best way is:
+    #   - boot finnix from ISO in a virtual machine
+    #   - run `finnix-netboot-server`.
+    #   - make sure to select "fat initrd" instead of the NFS option; this
+    #     requires more memory but lets us avoid an NFS server during netboot
+    #   - cd /srv/tfp && tar cfz finnix.tar.gz finnix
+    #   - scp finnix.tar.gz puppet:/opt/puppet/shares/contrib/local/ocf_dhcp/
+    # Note that the "fat" initrd requires ~0.75 GB of memory, so if you're
+    # testing with a VM, be sure to give it sufficient RAM.
+    '/var/lib/finnix.tar.gz':
+      source => 'puppet:///contrib/local/ocf_dhcp/finnix.tar.gz',
+      backup => false,
   }
 
   cron { 'ocf-netboot':
     command     => '/usr/local/sbin/ocf-netboot > /dev/null',
     user        => root,
     environment => 'PATH=/bin:/usr/bin:/usr/sbin',
-    special     => 'weekly';
+    special     => 'weekly',
+    require     => File['/var/lib/finnix.tar.gz'];
   }
 }
