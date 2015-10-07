8126d3ce6acb4f477d521cb69e7b4479e0171910
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-10-07 15:04:21 -0700

    ocf/staff_users: don't create homedirs for staff on desktops
    Whoops, this was never intended. I already made an exception for NFS,
    forgot to make another for desktops (tmpfs /home).

--- modules/ocf/manifests/staff_users.pp
+++ modules/ocf/manifests/staff_users.pp
@@ -1,7 +1,7 @@
 # Create home directories for staff members if NFS isn't mounted.
 
 class ocf::staff_users {
-  if !str2bool($::ocf_nfs) {
+  if !str2bool($::ocf_nfs) and $::type == 'server' {
     $staff = split($::ocf_staff, ',')
     ocf::staff_users::user { $staff:; }
   }
