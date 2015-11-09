04feff496a135e3c3dce10c8c5eae91b351af069
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-11-08 21:05:35 -0800

    Add ocf_rancid module for monitoring Cisco switch changes

---
+++ modules/ocf_rancid/manifests/init.pp
@@ -0,0 +1,45 @@
+# Configure rancid, for diffing and storing of OCF network configs.
+class ocf_rancid {
+  package { 'rancid':; }
+
+  exec {
+    'rancid-add-ocf-group':
+      command => 'echo "LIST_OF_GROUPS=\"ocf\"" >> /etc/rancid/rancid.conf',
+      unless  => 'grep -q ^LIST_OF_GROUPS /etc/rancid/rancid.conf',
+      require => Package['rancid'];
+
+    'rancid-filter-passwords':
+      command => 'echo "FILTER_PWDS=ALL; export FILTER_PWDS" >> /etc/rancid/rancid.conf',
+      unless  => 'grep -q ^FILTER_PWDS /etc/rancid/rancid.conf',
+      require => Package['rancid'];
+
+    # idempotent command to update rancid cvs groups
+    'rancid-cvs-update':
+      command     => '/var/lib/rancid/bin/rancid-cvs',
+      refreshonly => true,
+      require     => Package['rancid'],
+      subscribe   => [
+        Package['rancid'],
+        Exec['rancid-add-ocf-group'],
+      ];
+  }
+
+  file {
+    '/var/lib/rancid/ocf/router.db':
+      content => "blackhole:cisco:up\n",
+      require => [
+        Package['rancid'],
+        Exec['rancid-cvs-update'],
+      ];
+
+    '/root/.cloginrc':
+      source  => 'puppet:///private/cloginrc',
+      mode    => '0600',
+      require => Package['rancid'];
+  }
+
+  cron { 'rancid-run':
+    command => 'rancid-run',
+    special => 'hourly';
+  }
+}
