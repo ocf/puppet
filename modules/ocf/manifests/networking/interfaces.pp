c77354f322e40e74680ccc0943ecaa003a6c4e17
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-11-15 01:53:42 -0800

    Clean up old VLAN configuration

--- modules/ocf/manifests/networking/interfaces.pp
+++ modules/ocf/manifests/networking/interfaces.pp
@@ -1,22 +1,13 @@
 class ocf::networking::interfaces($ipaddress, $netmask, $gateway, $bridge) {
-  $newip = regsubst($ipaddress, '^169\.229\.10\.', '169.229.226.')
-  $interface = $bridge ? {
-    true    => 'br0',
-    default => 'eth0',
-  }
-
   file {
     '/etc/network/interfaces':
       content => template('ocf/networking/interfaces.erb');
 
     '/etc/network/if-up.d/ocf-vpn-route':
-      content => template('ocf/networking/ocf-vpn-route.erb'),
-      mode    => '0755';
+      ensure  => absent;
   }
 
   if $bridge {
     package { 'bridge-utils': }
   }
-
-  package { 'vlan': }
 }
