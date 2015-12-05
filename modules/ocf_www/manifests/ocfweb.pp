28b50d4c0812c14e1c4cc09238d8e33b6295b76e
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2015-12-05 02:22:28 -0800

    ocfweb: additional Django config, add local Redis and spiped tunnel

--- modules/ocf_www/manifests/ocfweb.pp
+++ modules/ocf_www/manifests/ocfweb.pp
@@ -1,9 +1,36 @@
 class ocf_www::ocfweb {
   include ocf_ssl
-  package { 'ocfweb':
-    ensure  => latest,
-    require => Exec['ocfweb: apt-get update'];
+  package {
+    'ocfweb':
+      ensure  => latest,
+      require => [
+        Package['redis-server'],
+        Exec['ocfweb: apt-get update'],
+        Augeas['/etc/redis/redis.conf'],
+      ],
+      # restart redis when we push to invalidate cache and sessions
+      notify  => Service['redis-server'];
+
+    'redis-server':;
   }
+
+  # Redis config for caching and sessions.
+  augeas { '/etc/redis/redis.conf':
+    lens    => 'Spacevars.simple_lns',
+    incl    => '/etc/redis/redis.conf',
+    changes => [
+      # no persistence, in-memory only
+      'set appendonly no',
+      'rm save',
+    ],
+    require => Package['redis-server'],
+    notify  => Service['redis-server'];
+  }
+
+  service { 'redis-server':
+    require => Package['redis-server'];
+  }
+
   exec { 'ocfweb: apt-get update':
     command => 'apt-get update';
   }
@@ -61,5 +88,34 @@ class ocf_www::ocfweb {
 
       listen_port      => 443,
       rewrite_to_https => true;
+  }
+
+  # TODO: stop copy-pasting this everywhere
+  $redis_password = file('/opt/puppet/shares/private/create/redis-password')
+  validate_re($redis_password, '^[a-zA-Z0-9]*$', 'Bad Redis password')
+  $django_secret = file("/opt/puppet/shares/private/${::hostname}/django-secret")
+  validate_re($django_secret, '^[a-zA-Z0-9]*$', 'Bad Django secret')
+
+  $broker = "redis://:${redis_password}@localhost:6378"
+  $backend = $broker
+
+  augeas { '/etc/ocfweb/ocfweb.conf':
+    lens      => 'Puppet.lns',
+    incl      => '/etc/ocfweb/ocfweb.conf',
+    changes   =>  [
+      "set django/secret ${django_secret}",
+      "set celery/broker ${broker}",
+      "set celery/backend ${backend}",
+    ],
+    show_diff => false,
+    notify    => Service['ocfweb'],
+    require   => Package['ocfweb'];
+  }
+
+  # create redis
+  spiped::tunnel::client { 'create-redis':
+    source  => 'localhost:6378',
+    dest    => 'create:6379',
+    secret  => file('/opt/puppet/shares/private/create/spiped-key');
   }
 }
