1848a4d8b8302be05756ce05d4d14ce7c5931ebd
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-02-05 00:00:25 -0800

    Add back OCF php.ini settings

--- modules/ocf_www/manifests/mod/php.pp
+++ modules/ocf_www/manifests/mod/php.pp
@@ -3,18 +3,23 @@ class ocf_www::mod::php {
 
   package { 'php5-cgi':; }
 
-  file { '/opt/suexec/php5-cgi':
-    ensure  => link,
-    target  => '/usr/bin/php5-cgi',
-    require => Package['php5-cgi'],
-  }
+  file {
+    '/opt/suexec/php5-cgi':
+        ensure  => link,
+        target  => '/usr/bin/php5-cgi',
+        require => Package['php5-cgi'];
 
-  # can't parse this with augeas because the PHP lens doesn't support weird
-  # keys like "application/x-httpd-suphp" :\
-  file { '/etc/suphp/suphp.conf':
-    source  => 'puppet:///modules/ocf_www/apache/mods/suphp.conf',
-    require => Package['libapache2-mod-suphp'],
-    notify  => Service['apache2'];
+    '/etc/php5/cgi/conf.d/99-ocf.ini':
+      source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
+      require => Package['php5-cgi'],
+      notify  => Service['apache2'];
+
+    # Can't parse this with augeas because the PHP lens doesn't support weird
+    # keys like "application/x-httpd-suphp" :\
+    '/etc/suphp/suphp.conf':
+      source  => 'puppet:///modules/ocf_www/apache/mods/suphp.conf',
+      require => Package['libapache2-mod-suphp'],
+      notify  => Service['apache2'];
   }
 
   apache::custom_config { 'php_suexec_options':
