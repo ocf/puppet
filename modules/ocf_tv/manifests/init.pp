6a0792cdcf01fe79df92fccf4edca5b325260a98
Jason Perrin <jasonvperrin@gmail.com>
2016-02-06 18:26:16 -0800

    Add ocf_tv module for NUC

---
+++ modules/ocf_tv/manifests/init.pp
@@ -0,0 +1,17 @@
+class ocf_tv {
+  include ocf_desktop::chrome
+
+  package { ['i3', 'vlc', 'ffmpeg', 'nodm', 'xinit', 'iceweasel', 'pulseaudio', 'pavucontrol', 'arandr', 'flashplugin-nonfree']:; }
+
+  user { 'ocftv':
+    comment => 'TV NUC',
+    home    => '/opt/tv',
+    groups  => ['sys'],
+    shell   => '/bin/bash';
+  }
+
+  file {
+    '/etc/X11/xorg.conf':
+      source => 'puppet:///modules/ocf_tv/X11/xorg.conf';
+  }
+}
