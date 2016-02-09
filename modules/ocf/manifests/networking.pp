dd828d5875fec7c18dfb9ab35169f05d8e532988
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-02-08 21:38:13 -0800

    Add IPv6 addresses to all hosts

--- modules/ocf/manifests/networking.pp
+++ modules/ocf/manifests/networking.pp
@@ -1,21 +1,39 @@
-class ocf::networking($ipaddress = undef, $netmask = undef, $gateway = undef,
-  $bridge = false, $domain = undef, $nameservers = undef,
-  ) {
+class ocf::networking(
+    $bridge     = false,
 
-  class {
-    'ocf::networking::interfaces':
-      ipaddress   => $ipaddress,
-      netmask     => $netmask,
-      gateway     => $gateway,
-      bridge      => $bridge;
+    $ipaddress  = $::ipHostNumber,
+    $netmask    = '255.255.255.0',
+    $gateway    = '169.229.226.1',
 
-    'ocf::networking::resolvconf':
-      domain      => $domain,
-      nameservers => $nameservers;
+    $ipaddress6 = regsubst($::ipHostNumber, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$', '2607:f140:8801::1:\4'),
+    $netmask6   = '64',
+    $gateway6   = '2607:f140:8801::1',
 
-    'ocf::networking::hostname':
-      ipaddress   => $ipaddress;
+    $domain      = 'ocf.berkeley.edu',
+    $nameservers = ['169.229.226.22', '128.32.206.12', '128.32.136.9'],
+) {
+  $fqdn = $::clientcert
+  $hostname = regsubst($::clientcert, '^(\w+)\..*$', '\1')
+
+  # packages
+  if $bridge {
+    package { 'bridge-utils': }
   }
 
+  package { 'resolvconf':
+    ensure => purged,
+  }
 
+  # network configuration
+  file {
+    '/etc/network/interfaces':
+      content => template('ocf/networking/interfaces.erb');
+    '/etc/hostname':
+      content => "${hostname}\n";
+    '/etc/hosts':
+      content => template('ocf/networking/hosts.erb');
+    '/etc/resolv.conf':
+      content => template('ocf/networking/resolv.conf.erb'),
+      require => Package['resolvconf'];
+  }
 }
