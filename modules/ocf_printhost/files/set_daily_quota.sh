14279db9f1daf3b9d01de66b83fc01b83452424d
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-02-10 04:41:41 -0800

    ocf_printhost: drop semesterly quota to 100 for new accounts

--- modules/ocf_printhost/files/set_daily_quota.sh
+++ modules/ocf_printhost/files/set_daily_quota.sh
@@ -3,7 +3,7 @@
 pkusers --config /etc/pykota/pykota.semester -L $1 > /dev/null #test run of pkusers if the user exists in the database
 if [[ $? -ne 0 ]]
 then
-   pkusers --config /etc/pykota/pykota.semester -l balance -b 250.0 -a $1
+   pkusers --config /etc/pykota/pykota.semester -l balance -b 100.0 -a $1
 fi
 
 page_total=`pkusers --config /etc/pykota/pykota.semester -L $1 | grep 'Account balance' | cut -d':' -f2 | sed 's/ //g' | cut -d'.' -f1`
