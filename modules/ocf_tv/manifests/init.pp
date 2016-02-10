02592602ec92018449ee9603b6cd8819e05738e9
Jason Perrin <jasonvperrin@gmail.com>
2016-02-10 00:28:04 -0800

    ocf_tv: Set up x11vnc as a service

--- modules/ocf_tv/manifests/init.pp
+++ modules/ocf_tv/manifests/init.pp
@@ -1,7 +1,7 @@
 class ocf_tv {
   include ocf::packages::chrome
 
-  package { ['i3', 'vlc', 'ffmpeg', 'nodm', 'xinit', 'iceweasel', 'pulseaudio', 'pavucontrol', 'arandr', 'flashplugin-nonfree']:; }
+  package { ['i3', 'vlc', 'ffmpeg', 'nodm', 'xinit', 'iceweasel', 'pulseaudio', 'pavucontrol', 'arandr', 'flashplugin-nonfree', 'x11vnc']:; }
 
   user { 'ocftv':
     comment => 'TV NUC',
@@ -20,5 +20,25 @@ class ocf_tv {
 
     '/etc/X11/xorg.conf':
       source => 'puppet:///modules/ocf_tv/X11/xorg.conf';
+
+    '/etc/systemd/system/x11vnc.service':
+      mode   => '0664',
+      source => 'puppet:///modules/ocf_tv/x11vnc.service';
+  }
+
+  service {
+    'x11vnc':
+      ensure   => 'running',
+      enable   => true,
+      provider => systemd,
+      require  => Package['x11vnc'];
+  }
+
+  exec {
+    'x11vnc-update':
+      command     => 'systemctl daemon-reload',
+      refreshonly => true,
+      require     => User['ocftv'],
+      subscribe   => File['/etc/systemd/system/x11vnc.service'];
   }
 }
