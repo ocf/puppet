037451204ae7c684a3a62fa1c2cc24b3a00d1d0e
Jason Perrin <jasonvperrin@gmail.com>
2016-02-13 18:10:05 -0800

    Replace xfce4 application menu with whisker menu
    Also add shortcut to open the menu using the Super key
    
    The whisker menu is much nicer to use than the default xfce4 menu, since
    it can be searched, and has nice things shown like the user's name. It is
    also very extensible, since custom actions can be added to it very easily.

--- modules/ocf_desktop/manifests/packages.pp
+++ modules/ocf_desktop/manifests/packages.pp
@@ -13,7 +13,7 @@ class ocf_desktop::packages {
     # applications
     ['arandr', 'claws-mail', 'geany', 'filezilla', 'inkscape', 'mssh', 'numlockx', 'remmina', 'simple-scan', 'vlc', 'zenmap', 'gimp', 'gparted', 'evince-gtk', 'galculator', 'xchat', 'hexchat', 'atom']:;
     # desktop
-    ['desktop-base', 'desktop-file-utils', 'gpicview', 'xarchiver', 'xterm', 'lightdm', 'lightdm-gtk-greeter', 'accountsservice', 'redshift']:;
+    ['desktop-base', 'desktop-file-utils', 'gpicview', 'xarchiver', 'xterm', 'lightdm', 'lightdm-gtk-greeter', 'accountsservice', 'redshift', 'xfce4-whiskermenu-plugin']:;
     # fonts
     ['cm-super', 'fonts-croscore', 'fonts-crosextra-caladea', 'fonts-crosextra-carlito', 'fonts-inconsolata', 'fonts-linuxlibertine', 'fonts-unfonts-core', 'ttf-ancient-fonts']:;
     # games
