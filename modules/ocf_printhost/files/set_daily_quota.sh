86ca52ed7e6a8109eade980ad9b9e48dd22f1018
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-02-15 02:04:45 -0800

    ocf_printhost: implement new daily quotas

--- modules/ocf_printhost/files/set_daily_quota.sh
+++ modules/ocf_printhost/files/set_daily_quota.sh
@@ -8,14 +8,14 @@ fi
 
 page_total=`pkusers --config /etc/pykota/pykota.semester -L $1 | grep 'Account balance' | cut -d':' -f2 | sed 's/ //g' | cut -d'.' -f1`
 if [[ $(date +%u) -gt 5 ]] ; then
-	if [ $page_total -gt 29 ]; then
-    		/usr/local/bin/autopykota --initbalance 30.0
+	if [ $page_total -gt 15 ]; then
+    		/usr/local/bin/autopykota --initbalance 16.0
 	else
     		/usr/local/bin/autopykota --initbalance $page_total
 	fi
 else
-	if [ $page_total -gt 14 ]; then
-                /usr/local/bin/autopykota --initbalance 15.0
+	if [ $page_total -gt 7 ]; then
+                /usr/local/bin/autopykota --initbalance 8.0
         else
                 /usr/local/bin/autopykota --initbalance $page_total
         fi
