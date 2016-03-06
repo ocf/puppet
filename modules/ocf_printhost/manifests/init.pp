7ed5e417786d6ee2537eada3c867bb23d718af47
Matthew McAllister <matthew.mcallister@berkeley.edu>
2016-03-06 15:58:32 -0800

    Nix pykota, configure cups
    This configuration is just enough to print from dev-pollution, with no
    accounting set up. Committing generated files (printers.conf and classes.conf)
    is admittedly distasteful, but it will make rebuilding convenient.

--- modules/ocf_printhost/manifests/init.pp
+++ modules/ocf_printhost/manifests/init.pp
@@ -7,99 +7,36 @@ class ocf_printhost {
   ocf::repackage { 'hplip':
     recommends => false
   }
-  file {
-    # provide cups config
-    '/etc/cups/cupsd.conf':
-      source  => 'puppet:///modules/ocf_printhost/cupsd.conf',
-      require => Package['cups'],
-      notify  => Service['cups'],
-    ;
-    # provide more sensitive cups config
-    '/etc/cups/cups-files.conf':
-      content => template('ocf_printhost/cups/cups-files.conf.erb'),
-      require => Package['cups'],
-      notify  => Service['cups'],
-    ;
-    # set default printer double
-    '/etc/cups/lpoptions':
-      content => 'Default double',
-      require => Package[ 'cups'],
-      notify  => Service['cups'],
-    ;
-    # deny printing raw jobs
-    [ '/etc/cups/raw.convs', '/etc/cups/raw.types' ]:
-      content => '# deny printing raw jobs',
-      require => Package['cups'],
-      notify  => Service['cups'],
-    ;
+  service { 'cups': }
+
+  File {
+    require => Package['cups'],
+    notify  => Service['cups'],
   }
+
+  file {
+    '/etc/cups/cupsd.conf':
+      content => template('ocf_printhost/cups/cupsd.conf.erb');
+    '/etc/cups/cups-files.conf':
+      content => template('ocf_printhost/cups/cups-files.conf.erb');
+    '/etc/cups/lpoptions':
+      content => 'Default double';
+    ['/etc/cups/raw.convs', '/etc/cups/raw.types']:
+      content => '# deny printing raw jobs';
+    '/etc/cups/ppd':
+      ensure  => directory;
+    ['/etc/cups/ppd/deforestation-double.ppd', '/etc/cups/ppd/logjam-double.ppd']:
+      source  => 'puppet:///modules/ocf_printhost/cups/double.ppd',
+      require => File['/etc/cups/ppd'];
+    ['/etc/cups/ppd/deforestation-single.ppd', '/etc/cups/ppd/logjam-single.ppd']:
+      source  => 'puppet:///modules/ocf_printhost/cups/single.ppd',
+      require => File['/etc/cups/ppd'];
+  }
+
   # mount /var/spool/cups in tmpfs
   mount { '/var/spool/cups':
     device  => 'tmpfs',
     fstype  => 'tmpfs',
     options => 'mode=0710,gid=lp,noatime,nodev,noexec,nosuid';
-  }
-  service { 'cups': }
-
-  # set up pykota
-  package {
-    # pykota python dependencies
-    [ 'pkpgcounter', 'python-egenix-mxdatetime', 'python-imaging', 'python-jaxml', 'python-minimal', 'python-mysqldb', 'python-osd', 'python-pysnmp4', 'python-reportlab' ]:
-  }
-  file {
-    # configuration directory
-    '/etc/pykota':
-      ensure  => directory,
-    ;
-    '/etc/pykota/pykota.semester':
-      ensure  => directory,
-    ;
-    # public configuration
-    '/etc/pykota/pykota.semester/pykota.conf':
-      source  => 'puppet:///modules/ocf_printhost/pykota-semester.conf',
-    ;
-    '/etc/pykota/pykota.conf':
-      source  => 'puppet:///modules/ocf_printhost/pykota.conf',
-    ;
-    '/etc/pykota/pykotadmin.conf':
-      owner   => 'lp',
-      group   => 'ocfstaff',
-      mode    => '0640',
-      source  => 'puppet:///modules/ocf_printhost/pykotadmin.conf',
-    ;
-    '/etc/pykota/update_semester_quota.sh':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf_printhost/update_semester_quota.sh',
-    ;
-    '/etc/pykota/set_daily_quota.sh':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf_printhost/set_daily_quota.sh',
-    ;
-    '/etc/pykota/reset_daily_quota.sh':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf_printhost/reset_daily_quota.sh',
-    ;
-    '/etc/pykota/make_pubstaff.sh':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf_printhost/make_pubstaff.sh',
-    ;
-    # private configuration
-    '/etc/pykota/pykota.semester/pykotadmin.conf':
-      owner   => 'lp',
-      group   => 'ocfstaff',
-      mode    => '0640',
-      source  => 'puppet:///private/pykotadmin-semester.conf'
-    ;
-  }
-
-  # share pykota configuration over NFS
-  package { 'nfs-kernel-server': }
-  file { '/etc/exports':
-    source  => 'puppet:///modules/ocf_printhost/exports',
-    require => Package['nfs-kernel-server']
-  }
-  service { 'nfs-kernel-server':
-    subscribe => File['/etc/exports'],
-    require   => Package['nfs-kernel-server']
   }
 }
