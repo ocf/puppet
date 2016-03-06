2697b76d72abb8e3a031fd78ef40cd07e5a17488
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-03-05 19:16:31 -0800

    packages/chrome: disable the crummy Google crobjob (rt#4589)

--- modules/ocf/manifests/packages/chrome.pp
+++ modules/ocf/manifests/packages/chrome.pp
@@ -1,18 +1,22 @@
 class ocf::packages::chrome {
+  package { 'google-chrome-stable':; }
+
   file {
     ['/etc/opt/chrome', '/etc/opt/chrome/policies', '/etc/opt/chrome/policies/managed']:
       ensure  => directory,
-      require => Package['google-chrome-stable'],
-    ;
+      require => Package['google-chrome-stable'];
+
     '/etc/opt/chrome/policies/managed/ocf_policy.json':
       source  => 'puppet:///modules/ocf/chrome/ocf_policy.json',
-      require => Package['google-chrome-stable'],
-    ;
+      require => Package['google-chrome-stable'];
+
     '/opt/google/chrome/master_preferences':
       source  => 'puppet:///modules/ocf/chrome/master_preferences',
-      require => Package['google-chrome-stable'],
-    ;
-  }
+      require => Package['google-chrome-stable'];
 
-  package { 'google-chrome-stable':; }
+    '/opt/google/chrome/cron/google-chrome':
+      mode    => '0755',
+      content => "# Disabled by OCF (rt#4589)\n",
+      require => Package['google-chrome-stable'];
+  }
 }
