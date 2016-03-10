7dcb1aa39ba4fd0525a40cc42070a8aaa071cb53
Matthew McAllister <matthew.mcallister@berkeley.edu>
2016-03-09 19:43:08 -0800

    Install private enforcer mysql config

--- modules/ocf_printhost/manifests/enforcer.pp
+++ modules/ocf_printhost/manifests/enforcer.pp
@@ -2,9 +2,14 @@ class ocf_printhost::enforcer {
   # enforcer will probably depend on cups-tea4cups in the future
   package { ['cups-tea4cups', 'enforcer']: }
 
-  file { '/etc/cups/tea4cups.conf':
-    source  => 'puppet:///modules/ocf_printhost/cups/tea4cups.conf',
-    require => Package['cups-tea4cups', 'enforcer'],
-    notify  => Service['cups'];
+  file {
+    '/etc/cups/tea4cups.conf':
+      source  => 'puppet:///modules/ocf_printhost/cups/tea4cups.conf',
+      require => Package['cups-tea4cups', 'enforcer'];
+    '/opt/share/enforcer':
+      ensure => directory,
+      mode   => '0500';
+    '/opt/share/enforcer/enforcer.conf':
+      source => 'puppet:///private/enforcer/enforcer.conf';
   }
 }
