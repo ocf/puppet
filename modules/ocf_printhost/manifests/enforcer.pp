65ffd0226c55da95c8f790dc109f8b9447282a83
Matthew McAllister <matthew.mcallister@berkeley.edu>
2016-03-12 00:54:31 -0800

    Install enforcer to enforce print quotas.
    Currently, launch is blocked by the fact that cups/tea4cups adds a trailing
    page to the document before it is sent to the printer to make sure it has an
    even number of pages. Hopefully there's something in the printer driver or the
    *2pdf config to fix this.
    
    Also, PDF is now the backend filetype. Hopefully this turns out to be highly
    compatible and flexible.

--- modules/ocf_printhost/manifests/enforcer.pp
+++ modules/ocf_printhost/manifests/enforcer.pp
@@ -1,15 +1,17 @@
 class ocf_printhost::enforcer {
-  # enforcer will probably depend on cups-tea4cups in the future
-  package { ['cups-tea4cups', 'enforcer']: }
+  package { ['cups-tea4cups']: }
 
   file {
     '/etc/cups/tea4cups.conf':
       source  => 'puppet:///modules/ocf_printhost/cups/tea4cups.conf',
-      require => Package['cups-tea4cups', 'enforcer'];
+      require => Package['cups-tea4cups'];
+    '/usr/local/bin/enforcer':
+      source  => 'puppet:///modules/ocf_printhost/enforcer',
+      mode    => '0755';
     '/opt/share/enforcer':
-      ensure => directory,
-      mode   => '0500';
+      ensure  => directory,
+      mode    => '0500';
     '/opt/share/enforcer/enforcer.conf':
-      source => 'puppet:///private/enforcer/enforcer.conf';
+      source  => 'puppet:///private/enforcer/enforcer.conf';
   }
 }
