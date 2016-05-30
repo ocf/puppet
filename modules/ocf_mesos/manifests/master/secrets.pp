fabc0c83760f44e68de7a8cbdd5dbe8d66054560
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-05-29 19:06:15 -0700

    ocf_mesos/master/secrets: fix mode, purge and force

--- modules/ocf_mesos/manifests/master/secrets.pp
+++ modules/ocf_mesos/manifests/master/secrets.pp
@@ -1,7 +1,9 @@
 class ocf_mesos::master::secrets {
   file { '/opt/share/secrets':
-    mode    => '0700',
+    mode    => '0600',
     source  => 'puppet:///private-docker/',
     recurse => true,
+    purge   => true,
+    force   => true,
   }
 }
