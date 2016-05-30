6f8fb808775b4db90b64e11912796d3c47fea08a
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-05-29 18:59:54 -0700

    Add internal Docker registry service

---
+++ modules/ocf_docker/manifests/init.pp
@@ -0,0 +1,16 @@
+class ocf_docker {
+  include ocf::packages::docker
+  require ocf_ssl
+
+  file { '/var/lib/registry':
+    ensure => directory,
+  }
+
+  ocf::systemd::service { 'docker-registry':
+    content => template('ocf_docker/docker-registry.service.erb'),
+    require => [
+      Package['docker.io'],
+      File['/var/lib/registry'],
+    ],
+  }
+}
