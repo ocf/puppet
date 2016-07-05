dfa67ce27ae76d3e9a981d1010ed781c84cab1fc
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-07-04 20:36:06 -0700

    site.pp: fix outdated comment

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,7 +11,7 @@ stage { 'first': before => Stage['main']; }
 # default path for executions
 Exec { path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
 
-# default file permissions, follow symlinks when serving files, backup existing files to puppetmaster
+# default file permissions, backup existing files to puppetmaster
 File { mode => '0644', owner => root, group => root, backup => main }
 
 # add managed filesystems to fstab by default
