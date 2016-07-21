eb6da58789bcb597d2eb1f07523346fae68091d6
Jason Perrin <jasonvperrin@gmail.com>
2016-07-21 09:16:38 -0700

    Add TODO note about removing nodesource apt repo

--- modules/ocf_irc/manifests/nodejs/apt.pp
+++ modules/ocf_irc/manifests/nodejs/apt.pp
@@ -4,6 +4,9 @@ class ocf_irc::nodejs::apt {
   }
 
   # Install a newer version of Node.js for the Slack bridge and web IRC app
+  #
+  # TODO: Remove this when we move to stretch, since it bundles nodejs 4.x, which
+  # is a high enough version to run the web IRC
   apt::key { 'nodejs':
     id     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
     source => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key';
