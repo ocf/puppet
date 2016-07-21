0d0fabd13432d3a15e1c70de69125600690cdfdf
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-07-20 22:42:53 -0700

    ocf_mesos: add keepalived and high-availability load balancing
    and more buzzwords

--- modules/ocf_mesos/manifests/master/load_balancer.pp
+++ modules/ocf_mesos/manifests/master/load_balancer.pp
@@ -3,8 +3,41 @@ class ocf_mesos::master::load_balancer {
 
   ocf::systemd::service { 'ocf-lb':
     ensure  => running,
-    source  => 'puppet:///modules/ocf_mesos/master/ocf-lb.service',
+    source  => 'puppet:///modules/ocf_mesos/master/load_balancer/ocf-lb.service',
     enable  => true,
     require => Package['docker-engine'],
+  }
+
+  # keepalived config
+  package { 'keepalived':; }
+  $keepalived_secret = 'hunter2'
+
+  # Virtual addresses are owned by all of the mesos masters.
+  # At a given time, only one master will actually have the IP, but
+  # all masters are capable of holding the IP.
+  #
+  # Currently we have just a primary. Potentially other services which can't
+  # use name-based hosts (e.g. SMTP or something) might need TCP forwarding,
+  # and thus a different address.
+  $virtual_addresses = [
+    # Primary load balancer IP
+    '169.229.226.53',
+  ]
+
+  service { 'keepalived':
+    require => Package['keepalived'],
+  }
+
+  file { '/etc/keepalived/keepalived.conf':
+    content => template('ocf_mesos/master/load_balancer/keepalived.conf.erb'),
+    require => Package['keepalived'],
+    notify  => Service['keepalived'],
+  }
+
+  # Service virtual host definitions
+  ocf_mesos::master::load_balancer::http_vhost { 'fluffy':
+    server_name  => ['fluffy.ocf.berkeley.edu'],
+    service_port => 10000,
+    ssl          => false,
   }
 }
---
+++ modules/ocf_mesos/manifests/master/load_balancer/http_vhost.pp
@@ -0,0 +1,44 @@
+# Proxy HTTP and possibly HTTPS requests to a Marathon service.
+#
+# Because HTTP services can use name-based virtual hosts, all HTTP vhosts bind
+# to the same IP (currently 169.229.226.51).
+#
+# By default, it will be an HTTP-only service. If $ssl is true, it will be
+# HTTPS-only, which a 301 redirect from HTTP -> HTTPS.
+# We don't currently support both HTTP and HTTPS services.
+define ocf_mesos::master::load_balancer::http_vhost(
+  $server_name,
+  $service_port,
+
+  $ssl = false,
+  $ssl_cert = undef,
+  $ssl_key = undef,
+  $ssl_dhparam = '/etc/ssl/dhparam.pem',
+) {
+  nginx::resource::upstream { "marathon_service_${service_port}":
+    members => ["localhost:${service_port}"],
+  }
+
+  if $ssl {
+    # TODO: SSL is untested
+    nginx::resource::vhost {
+      "${title}-https":
+        server_name => $server_name,
+        proxy       => "http://marathon_service_${service_port}",
+
+        ssl         => true,
+        ssl_cert    => $ssl_cert,
+        ssl_key     => $ssl_key,
+        ssl_dhparam => $ssl_dhparam,
+
+        listen_port => 443,
+
+        rewrite_to_https => true;
+    }
+  } else {
+    nginx::resource::vhost { "${title}-http":
+      server_name => $server_name,
+      proxy       => "http://marathon_service_${service_port}",
+    }
+  }
+}
