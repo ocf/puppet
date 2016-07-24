316983478c079d03c2914c40ad070c1755aa02f2
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-07-24 15:49:35 -0700

    Revert "Fix dependency cycle with docker apt source"
    This reverts commit 15f0423fdbc9d75bad5b32eccc261f2524d8dc19.

--- modules/ocf/manifests/apt.pp
+++ modules/ocf/manifests/apt.pp
@@ -78,21 +78,6 @@ class ocf::apt {
     require  => Apt::Key['mesosphere'],
   }
 
-  apt::key { 'docker':
-    id     => '58118E89F3A912897C070ADBF76221572C52609D',
-    server => 'keyserver.ubuntu.com',
-  }
-
-  apt::source { 'docker':
-    location    => 'http://apt.dockerproject.org/repo',
-    release     => 'debian-jessie',
-    repos       => 'main',
-    include   => {
-      src => false
-    },
-    require     => Apt::Key['docker'];
-  }
-
   file { '/etc/cron.daily/ocf-apt':
     mode    => '0755',
     content => template('ocf/apt/ocf-apt.erb'),
