24b5555bb61e6522fd24e1e3bae314d45f11b59f
Jason Perrin <jvperrin@ocf.berkeley.edu>
2016-08-22 15:06:46 -0700

    Add redirect for web IRC
    Setting attributes from a hash is pretty cool, we should do it more now
    that we actually can!

--- modules/ocf_irc/manifests/nodejs/webirc.pp
+++ modules/ocf_irc/manifests/nodejs/webirc.pp
@@ -38,19 +38,39 @@ class ocf_irc::nodejs::webirc {
     members => ['localhost:9000'];
   }
 
+  $ssl_options = {
+    ssl         => true,
+    ssl_cert    => "/etc/ssl/private/${::fqdn}.bundle",
+    ssl_key     => "/etc/ssl/private/${::fqdn}.key",
+    ssl_dhparam => '/etc/ssl/dhparam.pem',
+
+    add_header => {
+      'Strict-Transport-Security' => 'max-age=31536000',
+    },
+  }
+
   nginx::resource::vhost {
     $webirc_fqdn:
       server_name => [$webirc_fqdn],
       proxy       => 'http://thelounge',
 
-      ssl        => true,
-      ssl_cert   => "/etc/ssl/private/${::fqdn}.bundle",
-      ssl_key    => "/etc/ssl/private/${::fqdn}.key",
-
-      add_header => {
-        'Strict-Transport-Security' => 'max-age=31536000',
-      },
+      * => $ssl_options,
 
       rewrite_to_https => true;
+
+    "${webirc_fqdn}-redirect":
+      # Needs a www_root even though we just redirect
+      www_root => '/var/www',
+
+      server_name => [
+        $::hostname,
+        $::fqdn
+      ],
+
+      * => $ssl_options,
+
+      vhost_cfg_append => {
+        'return' => "301 https://${webirc_fqdn}\$request_uri"
+      };
   }
 }
