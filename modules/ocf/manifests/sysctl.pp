a710c8f0a95ba3b762d0d8ee2197086d7e6662dc
Kevin Peng <kpengboy@ocf.berkeley.edu>
2016-08-24 10:31:39 -0700

    Whoops, wrong suffix

--- modules/ocf/manifests/sysctl.pp
+++ modules/ocf/manifests/sysctl.pp
@@ -1,11 +1,15 @@
 class ocf::sysctl {
-  file { '/etc/sysctl.d/98-temp-workaround-cve-2016-5696.pp':
+  file { '/etc/sysctl.d/98-temp-workaround-cve-2016-5696.conf':
     ensure  => file,
     content => "net.ipv4.tcp_challenge_ack_limit = 999999999\n";
   }
 
-  exec { 'sysctl -p /etc/sysctl.d/98-temp-workaround-cve-2016-5696.pp':
-    subscribe   => File['/etc/sysctl.d/98-temp-workaround-cve-2016-5696.pp'],
+  file { '/etc/sysctl.d/98-temp-workaround-cve-2016-5696.pp':
+    ensure => absent;
+  }
+
+  exec { 'sysctl -p /etc/sysctl.d/98-temp-workaround-cve-2016-5696.conf':
+    subscribe   => File['/etc/sysctl.d/98-temp-workaround-cve-2016-5696.conf'],
     refreshonly => true;
   }
 }
