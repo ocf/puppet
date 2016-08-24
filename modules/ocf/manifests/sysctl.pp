5874dadddd0086fcdb01eaed8f25d999dcca6997
Kevin Peng <kpengboy@ocf.berkeley.edu>
2016-08-24 15:53:54 -0700

    Delete temporary lines

--- modules/ocf/manifests/sysctl.pp
+++ modules/ocf/manifests/sysctl.pp
@@ -4,10 +4,6 @@ class ocf::sysctl {
     content => "net.ipv4.tcp_challenge_ack_limit = 999999999\n";
   }
 
-  file { '/etc/sysctl.d/98-temp-workaround-cve-2016-5696.pp':
-    ensure => absent;
-  }
-
   exec { 'sysctl -p /etc/sysctl.d/98-temp-workaround-cve-2016-5696.conf':
     subscribe   => File['/etc/sysctl.d/98-temp-workaround-cve-2016-5696.conf'],
     refreshonly => true;
