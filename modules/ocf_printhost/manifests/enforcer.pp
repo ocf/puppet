31d6535f11a510f83b87b3bd943b86cd76d94cf8
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-08-29 20:18:08 -0700

    Remove old document titles from the enforcer DB (rt#5021)

--- modules/ocf_printhost/manifests/enforcer.pp
+++ modules/ocf_printhost/manifests/enforcer.pp
@@ -1,5 +1,5 @@
 class ocf_printhost::enforcer {
-  package { ['cups-tea4cups']: }
+  package { ['cups-tea4cups', 'mariadb-client']: }
 
   file {
     '/etc/cups/tea4cups.conf':
@@ -19,5 +19,25 @@ class ocf_printhost::enforcer {
       mode    => '0500';
     '/opt/share/enforcer/enforcer.conf':
       source  => 'puppet:///private/enforcer/enforcer.conf';
+  }
+
+  # We remove old document titles from the enforcer database for privacy reasons.
+  cron { 'remove old document titles':
+    command => 'mysql --defaults-file=/opt/share/enforcer-mysql.defaults < /opt/share/enforcer-remove-old-docs.sql',
+    special => 'hourly',
+  }
+
+  file {
+    '/opt/share/enforcer-remove-old-docs.sql':
+      content => '
+        UPDATE jobs
+        SET doc_name = NULL
+        WHERE time < DATE_SUB(NOW(), INTERVAL 14 DAY);
+      ';
+
+    '/opt/share/enforcer-mysql.defaults':
+      source    => 'puppet:///private/enforcer-mysql.defaults',
+      mode      => '0600',
+      show_diff => false;
   }
 }
