09539dd49a20a3249ac9f845b47913a08a6dcba2
Jason Perrin <jvperrin@ocf.berkeley.edu>
2016-11-18 16:10:17 -0800

    Use backported nvidia driver on jessie

--- modules/ocf_desktop/manifests/drivers.pp
+++ modules/ocf_desktop/manifests/drivers.pp
@@ -3,7 +3,15 @@ class ocf_desktop::drivers {
 
   # install proprietary nvidia drivers
   if $::gfx_brand == 'nvidia' {
-    package { ['nvidia-driver', 'libgl1-nvidia-glx:i386']:; }
+    # Install nvidia-driver from backports to support the GTX 1060s in the lab
+    # Only needed on jessie, since stretch has drivers that are recent (367+)
+    ocf::repackage { 'nvidia-driver':
+      backport_on => 'jessie',
+    }
+
+    ocf::repackage { 'libgl1-nvidia-glx:i386':
+      backport_on => 'jessie',
+    }
 
     file { '/etc/X11/xorg.conf':
       source => 'puppet:///modules/ocf_desktop/drivers/nvidia/xorg.conf';
