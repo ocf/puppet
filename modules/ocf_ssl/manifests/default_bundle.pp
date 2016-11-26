027848cc84a1d0db48e3f798a7a2bde9f7635100
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2016-11-25 16:41:33 -0800

    Correct a comment

--- modules/ocf_ssl/manifests/default_bundle.pp
+++ modules/ocf_ssl/manifests/default_bundle.pp
@@ -1,8 +1,5 @@
 # Provides the key, certificate, and InCommon CA certificate bundle at
-# /etc/ssl/private/$cert_name.{key,crt,bundle}
-#
-# Provides the InCommon intermediate chain at
-# /etc/ssl/certs/incommon-intermediate.crt
+# /etc/ssl/private/$cert_name.{key,crt,bundle,intermediate}
 class ocf_ssl::default_bundle {
   ocf_ssl::bundle { $::fqdn:; }
 }
