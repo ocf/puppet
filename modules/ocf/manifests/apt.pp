55b8d2a56adfa832b660b57a0720dce8298fd304
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-01-04 23:21:46 -0800

    Simplify puppet apt key importing

--- modules/ocf/manifests/apt.pp
+++ modules/ocf/manifests/apt.pp
@@ -82,18 +82,10 @@ class ocf::apt($stage = 'first') {
     source => 'https://apt.ocf.berkeley.edu/pubkey.gpg';
   }
 
-  # Add the puppetlabs key even though we use our local mirror
-  apt::key { 'puppetlabs':
+  # Add the puppetlabs apt repo key
+  apt::key { 'puppet gpg key':
     id     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
-    source => 'https://mirrors.ocf.berkeley.edu/puppetlabs/apt/pubkey.gpg';
-  }
-
-  # Hack to update the puppetlabs APT signing key with the same signature
-  # See https://tickets.puppetlabs.com/browse/MODULES-3307 for more info
-  exec { 'apt-key puppetlabs':
-    path    => '/bin:/usr/bin',
-    unless  => 'apt-key list | grep 4BD6EC30 | grep -vE "expired|revoked"',
-    command => 'apt-key adv --keyserver keys.gnupg.net --recv-keys 1054b7a24bd6ec30';
+    server => 'pgp.mit.edu',
   }
 
   file { '/etc/cron.daily/ocf-apt':
