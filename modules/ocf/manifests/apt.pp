4d14d8f59a2a89899411e8214038da4aa614308e
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-01-17 00:38:32 -0800

    Remove hack to update apt key

--- modules/ocf/manifests/apt.pp
+++ modules/ocf/manifests/apt.pp
@@ -89,14 +89,6 @@ class ocf::apt($stage = 'first') {
       id     => '6F6B15509CF8E59E6E469F327F438280EF8D349F',
       server => 'pgp.mit.edu',
     }
-
-    # Hack to update the puppetlabs APT signing key with the same signature
-    # See https://tickets.puppetlabs.com/browse/MODULES-3307 for more info
-    exec { 'apt-key puppetlabs':
-      path    => '/bin:/usr/bin',
-      unless  => 'apt-key list | grep 4BD6EC30 | grep -vE "expired|revoked"',
-      command => 'apt-key adv --keyserver keys.gnupg.net --recv-keys 1054b7a24bd6ec30';
-    }
   }
 
   apt::key { 'ocf':
