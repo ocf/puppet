84dc22171b437d1cbbfffcc31a0c7f53535ded52
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-01-27 15:35:52 -0800

    Pin only unstable to 150

--- modules/ocf/manifests/packages/firefox/apt.pp
+++ modules/ocf/manifests/packages/firefox/apt.pp
@@ -15,14 +15,22 @@ class ocf::packages::firefox::apt {
   } else {
     # TODO: switch to mozilla.debian.net once they support stretch.
     apt::source {
-      'debian-experimental':
+      'debian-unstable':
         location => 'http://mirrors/debian/',
         release  => 'unstable',
         repos    => 'main',
-        pin      => '150',
         include  => {
           src => true
         },
+    }
+
+    # Pin ONLY unstable to 150 so that it isn't preferred over regular repos or
+    # even backports. Previously, putting pin into apt::source above pinned the
+    # stable repo to the same value, which was bad.
+    apt::pin {
+      'debian-unstable':
+        priority => 150,
+        release  => 'unstable';
     }
   }
 }
