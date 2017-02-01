1a0f0cf78d1898e4fcba203dcd4bc61d9df00349
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-02-01 11:50:42 -0800

    ocf::staff_users: use each

--- modules/ocf/manifests/staff_users.pp
+++ modules/ocf/manifests/staff_users.pp
@@ -6,6 +6,19 @@ class ocf::staff_users($noop = false) {
   #   - it hasn't been turned off in Hiera
   if !str2bool($::ocf_nfs) and !$noop and $::ocf_staff {
     $staff = split($::ocf_staff, ',')
-    ocf::staff_users::user { $staff:; }
+    $staff.each |$user| {
+      $parent1 = regsubst($user, '^([a-z]).*$', '/home/\1')
+      $parent2 = regsubst($user, '^([a-z])([a-z]).*$', '/home/\1/\1\2')
+      $homedir = regsubst($user, '^([a-z])([a-z])([a-z]*)$', '/home/\1/\1\2/\1\2\3')
+
+      ensure_resource('file', [$parent1, $parent2], {'ensure' => 'directory'})
+
+      file { $homedir:
+        ensure  => directory,
+        owner   => $user,
+        mode    => '0700',
+        group   => ocfstaff;
+      }
+    }
   }
 }
