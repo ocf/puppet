013a4292112f4a50486154666e6c357539b7242f
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-02-25 12:21:12 -0800

    ocf_ssh: set show_diff=>false on makemysql.conf

--- modules/ocf_ssh/manifests/makeservices.pp
+++ modules/ocf_ssh/manifests/makeservices.pp
@@ -1,7 +1,7 @@
 class ocf_ssh::makeservices {
   # enable regular users to run makemysql.py as mysql
   file { '/etc/sudoers.d/makeservices':
-    content => "ALL ALL=(mysql) NOPASSWD: /opt/share/utils/makeservices/makemysql-real\n";
+    content => "ALL ALL=(mysql) NOPASSWD: /opt/share/utils/makeservices/makemysql-real\n",
   }
 
   file {
@@ -9,7 +9,9 @@ class ocf_ssh::makeservices {
       ensure => directory,
       mode   => '0700',
       owner  => 'mysql';
+
     '/opt/share/makeservices/makemysql.conf':
-      source => 'puppet:///private/makeservices/makemysql.conf';
+      source    => 'puppet:///private/makeservices/makemysql.conf',
+      show_diff => false;
   }
 }
