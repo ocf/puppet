55c5ffdfee5b8e82cc1f9d5cd5ad1eb32504d512
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-04-26 12:49:04 -0700

    Use new rsync host for puppetlabs mirror

--- modules/ocf_mirrors/manifests/puppetlabs.pp
+++ modules/ocf_mirrors/manifests/puppetlabs.pp
@@ -1,6 +1,6 @@
 class ocf_mirrors::puppetlabs {
   ocf_mirrors::ftpsync { 'puppetlabs':
-    rsync_host  => 'apt.puppetlabs.com',
+    rsync_host  => 'rsync.puppet.com',
     rsync_path  => 'packages/apt',
     cron_minute => '55';
   }
