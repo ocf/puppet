c5de38f1eeb0281873a4a0494ba10d9f20068b2c
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-05-03 03:09:56 -0700

    ocf_desktop: remove out-of-date comment

--- modules/ocf_desktop/manifests/steam.pp
+++ modules/ocf_desktop/manifests/steam.pp
@@ -2,12 +2,6 @@ class ocf_desktop::steam {
   include ocf::apt::i386
 
   package {
-    # Preseeded installations are currently broken on Debian steam:
-    # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=772598
-    #
-    # We have an OCF-patched version (steam 1.0.0.49-1+ocf1) in our apt repo.
-    #
-    # This can be removed after we upgrade to stretch.
     'steam':
       responsefile => '/var/cache/debconf/steam.preseed',
       require      => File['/var/cache/debconf/steam.preseed'];
