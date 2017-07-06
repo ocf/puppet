93b280674f91de868ae5b3522409afd21ac071c4
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-07-06 00:55:46 -0700

    Remove old settings since everything is Puppet 4+
    It's happening!

--- modules/ocf/manifests/puppet.pp
+++ modules/ocf/manifests/puppet.pp
@@ -15,6 +15,8 @@ class ocf::puppet($stage = 'first') {
         'set agent/usecacheonfailure false',
 
         # Remove a bunch of old settings that are no longer needed
+        # TODO: Remove ones from here that have already been removed in the
+        # section below for non-puppet-agent hosts
         'rm main/logdir',
         'rm main/vardir',
         'rm main/ssldir',
@@ -35,47 +37,28 @@ class ocf::puppet($stage = 'first') {
     $puppet_pkg = 'puppet'
     package { [$puppet_pkg, 'facter', 'augeas-tools', 'ruby-augeas']: }
 
-    if $::lsbdistcodename == 'jessie' {
-      # configure puppet agent
-      # set environment to match server and disable cached catalog on failure
-      augeas { '/etc/puppet/puppet.conf':
-        context => '/files/etc/puppet/puppet.conf',
-        changes => [
-          "set agent/environment ${::environment}",
-          'set agent/usecacheonfailure false',
-          'set main/pluginsync true',
-          'set main/stringify_facts false',
-          'set main/rundir /run/puppet',
+    # Configure puppet agent
+    # Set environment to match server and disable cached catalog on failure
+    augeas { '/etc/puppet/puppet.conf':
+      context => '/files/etc/puppet/puppet.conf',
+      changes => [
+        "set agent/environment ${::environment}",
+        'set agent/usecacheonfailure false',
 
-          # future parser breaks too many 3rd-party modules
-          'rm main/parser',
-
-          # templatedir is deprecated in 3.8+ and we don't use it
-          'rm main/templatedir',
-        ],
-        require => Package[$puppet_pkg, 'augeas-tools', 'ruby-augeas'],
-        notify  => Service['puppet'],
-      }
-    } else {
-      augeas { '/etc/puppet/puppet.conf':
-        context => '/files/etc/puppet/puppet.conf',
-        changes => [
-          "set agent/environment ${::environment}",
-          'set agent/usecacheonfailure false',
-
-          # Remove a bunch of old settings that are no longer needed
-          'rm main/templatedir',
-          'rm main/factpath',
-          'rm main/pluginsync',
-          'rm main/stringify_facts',
-          'rm main/prerun_command',
-          'rm main/postrun_command',
-          'rm agent/certname',
-          'rm master/ssl_client_header',
-          'rm master/ssl_client_verify_header',
-        ],
-        require => Package[$puppet_pkg, 'augeas-tools'],
-      }
+        # Remove a bunch of old settings that are no longer needed
+        # TODO: Remove this once all hosts have run puppet once to delete the
+        # unnecessary settings
+        'rm main/templatedir',
+        'rm main/factpath',
+        'rm main/pluginsync',
+        'rm main/stringify_facts',
+        'rm main/prerun_command',
+        'rm main/postrun_command',
+        'rm agent/certname',
+        'rm master/ssl_client_header',
+        'rm master/ssl_client_verify_header',
+      ],
+      require => Package[$puppet_pkg, 'augeas-tools'],
     }
   }
 
