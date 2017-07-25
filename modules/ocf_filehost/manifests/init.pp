170e29e58f107a5f9d0f033ebb38ee940ef55cab
Chris Kuehl <ckuehl@ocf.berkeley.edu>
2017-07-24 20:58:06 -0700

    Increase NFS thread count to 32 (rt#4405)

--- modules/ocf_filehost/manifests/init.pp
+++ modules/ocf_filehost/manifests/init.pp
@@ -1,9 +1,15 @@
 class ocf_filehost {
-  package { 'nfs-kernel-server':; }
-  service { 'nfs-kernel-server':; }
-
+  package {
+    'nfs-kernel-server':
+  } ->
+  augeas { '/etc/default/nfs-kernel-server':
+    lens    => 'Shellvars.lns',
+    incl    => '/etc/default/nfs-kernel-server',
+    # Increase number of NFS threads.
+    changes =>  ['set RPCNFSDCOUNT 32'],
+  } ~>
   file { '/etc/exports':
     source => 'puppet:///modules/ocf_filehost/exports',
-    notify => Service['nfs-kernel-server'];
-  }
+  } ~>
+  service { 'nfs-kernel-server':; }
 }
