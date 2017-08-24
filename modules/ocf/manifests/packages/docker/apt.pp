981fdd2db81a3406f91f19fbb798119a1d130303
Matthew McAllister <matthew.mcallister@berkeley.edu>
2017-08-24 00:46:20 -0700

    Require HTTPS for Docker apt

--- modules/ocf/manifests/packages/docker/apt.pp
+++ modules/ocf/manifests/packages/docker/apt.pp
@@ -1,8 +1,9 @@
 # Include Docker apt repo.
 class ocf::packages::docker::apt {
   apt::key { 'docker':
-    id     => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
-    source => 'https://download.docker.com/linux/debian/gpg',
+    id      => '9DC858229FC7DD38854AE2D88D81803C0EBFCD88',
+    source  => 'https://download.docker.com/linux/debian/gpg',
+    require => Package['apt-transport-https'],
   }
 
   apt::source { 'docker':
@@ -12,6 +13,6 @@ class ocf::packages::docker::apt {
     include   => {
       src => false
     },
-    require     => Apt::Key['docker'],
+    require     => [Apt::Key['docker'], Package['apt-transport-https']],
   }
 }
