19bc0f50558f036180af23cdc5463b2fd97fea24
Matthew McAllister <matthew.mcallister.0@gmail.com>
2017-09-20 11:51:53 -0700

    Revert "ocf_www: Allow PUT, DELETE, and PATCH requests"
    This reverts commit 535968ea17f284c544d6a42388bb71b7f062156a.
    
    Revert "ocf_www: Disable Apache OPTIONS requests"
    
    This reverts commit da3724c7a5f74d3eee0e095e1d9f4cd4fc581000.

--- modules/ocf_www/manifests/mod/security.pp
+++
@@ -1,6 +0,0 @@
-class ocf_www::mod::security {
-  class { '::apache::mod::security':
-    # Disable OPTIONS requests, originally due to CVE-2017-9798
-    allowed_methods => 'GET HEAD POST PUT DELETE PATCH',
-  }
-}
