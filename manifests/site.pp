50f32ad172e9d51f80b7ed6bd1d2d40506c11222
kpengboy <kpengboy@users.noreply.github.com>
2017-09-23 23:41:41 -0700

    Clarify comment

--- manifests/site.pp
+++ manifests/site.pp
@@ -30,9 +30,10 @@ Nginx::Resource::Server {
   ipv6_listen_options => '',
 }
 
-# Cron type won't reset attributes if you don't specify them.
+# By default, the cron type won't reset attributes if you don't specify them.
 # (e.g. if you only set "minute => '0'", it won't reset hour to '*')
 # This is bad behavior because the resource is only partially managed by Puppet.
+# Change the defaults to make the cron type behave reasonably in our manifests.
 #
 # Also set the PATH environment variable to be the same as in /etc/crontab.
 # The default PATH is only /usr/bin:/bin, which lacks a lot of commands.
