80d99d263f7f946407ba9b1866f6cf3e674832a8
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-10-17 03:39:51 -0700

    ocf: simplify require expression
    Now that we have an ocf::firewall::post class which executes
    after all firewall rules

--- modules/ocf/manifests/firewall.pp
+++ modules/ocf/manifests/firewall.pp
@@ -9,7 +9,8 @@ class ocf::firewall {
   # Docker is designed to clear away or work with most existing iptables
   # rules. However, it doesn't clear POSTROUTING rules as we'd like it to,
   # so we have to manually ensure those don't get persisted.
-  Firewall <| |> -> exec { 'Delete Docker POSTROUTING rules in iptables persistent config':
+  exec { 'Delete Docker POSTROUTING rules in iptables persistent config':
+    require => Class['ocf::firewall::post'],
     command => "sed -i '/^-A POSTROUTING / d' /etc/iptables/rules.v4",
     onlyif  => "grep '^-A POSTROUTING ' /etc/iptables/rules.v4",
   }
