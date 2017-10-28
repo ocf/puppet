669bcd64216aa5068123652e144876cff989e896
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-10-28 05:37:07 -0700

    ocf: only let root/postfix send to port 25

--- modules/ocf/manifests/firewall/post.pp
+++ modules/ocf/manifests/firewall/post.pp
@@ -1,5 +1,33 @@
 class ocf::firewall::post {
+  # Only allow root and postfix to connect to anthrax port 25; everyone else
+  # must use the sendmail interface
+  ['root', 'postfix'].each |$username| {
+    ocf::firewall::firewall46 {
+      "996 allow ${username} to send on SMTP port":
+        opts => {
+          chain  => 'OUTPUT',
+          proto  => 'tcp',
+          dport  => 'smtp',
+          uid    => $username,
+          action => 'accept',
+        },
+        before => undef,
+    }
+  }
 
+  ocf::firewall::firewall46 {
+    '997 forbid other users from sending on SMTP port':
+      opts => {
+        chain  => 'OUTPUT',
+        proto  => 'tcp',
+        dport  => 'smtp',
+        action => 'drop',
+      },
+      before => undef,
+  }
+
+
+  # Special devices we want to protect from most hosts
   $devices = ['corruption-mgmt','hal-mgmt', 'jaws-mgmt', 'pagefault', 'pandemic-mgmt',
               'papercut', 'radiation', 'riptide-mgmt']
 
