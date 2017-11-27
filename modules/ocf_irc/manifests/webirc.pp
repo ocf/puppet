a3878ad2a036294de41ea722812b478b1035bd67
Jason Perrin <jvperrin@ocf.berkeley.edu>
2017-11-27 00:21:57 -0800

    Create ocf::nginx_proxy for common proxy settings

--- modules/ocf_irc/manifests/webirc.pp
+++ modules/ocf_irc/manifests/webirc.pp
@@ -12,43 +12,12 @@ class ocf_irc::webirc {
     server_purge => true,
   }
 
-  $ssl_options = {
-    ssl         => true,
-    ssl_cert    => "/etc/ssl/private/${::fqdn}.bundle",
-    ssl_key     => "/etc/ssl/private/${::fqdn}.key",
-    ssl_dhparam => '/etc/ssl/dhparam.pem',
-
-    add_header => {
-      'Strict-Transport-Security' => 'max-age=31536000',
-    },
-  }
-
-  nginx::resource::server {
-    $webirc_fqdn:
-      server_name      => [$webirc_fqdn],
-      proxy            => 'https://thelounge.ocf.berkeley.edu',
-      proxy_set_header => [
-        'X-Forwarded-For $remote_addr',
-        'Host thelounge.ocf.berkeley.edu',
-      ],
-
-      * => $ssl_options,
-
-      ssl_redirect => true;
-
-    "${webirc_fqdn}-redirect":
-      # Needs a www_root even though we just redirect
-      www_root => '/var/www',
-
-      server_name => [
-        $::hostname,
-        $::fqdn
-      ],
-
-      * => $ssl_options,
-
-      server_cfg_append => {
-        'return' => "301 https://${webirc_fqdn}\$request_uri"
-      };
+  ocf::nginx_proxy { $webirc_fqdn:
+    server_aliases => [
+      $::hostname,
+      $::fqdn,
+    ],
+    proxy => 'http://lb.ocf.berkeley.edu:10008',
+    ssl   => true,
   }
 }
