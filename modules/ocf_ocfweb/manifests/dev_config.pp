ced950762f6e67928cfcc88e893e7508b637b6ad
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2017-11-29 02:32:48 -0800

    dev_config: add ocfstats-dev

--- modules/ocf_ocfweb/manifests/dev_config.pp
+++ modules/ocf_ocfweb/manifests/dev_config.pp
@@ -3,9 +3,11 @@
 # staff VMs, etc.
 class ocf_ocfweb::dev_config($group = 'ocfstaff') {
   include ocf::firewall::output_printers
+
   # TODO: stop copy-pasting this password validation everywhere
   $redis_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('create::redis::password'))
   $ocfmail_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('ocfmail::mysql::dev_password'))
+  $ocfstats_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('ocfstats::mysql::dev_password'))
 
   $broker = "redis://:${redis_password}@admin.ocf.berkeley.edu:6378"
   $backend = $broker
