80d52a43f7672b8226dde322d350e23b3b79ebb0
Kevin Kuehler <kkuehler@berkeley.edu>
2017-12-03 04:09:04 -0800

    Allow HTTP(S) to ocf_puppet for puppetboard

--- modules/ocf_puppet/manifests/puppetboard.pp
+++ modules/ocf_puppet/manifests/puppetboard.pp
@@ -1,4 +1,6 @@
 class ocf_puppet::puppetboard {
+  include ocf::firewall::allow_http
+
   $puppet_fqdn = $::host_env ? {
     'dev'  => 'dev-puppet.ocf.berkeley.edu',
     'prod' => 'puppet.ocf.berkeley.edu',
