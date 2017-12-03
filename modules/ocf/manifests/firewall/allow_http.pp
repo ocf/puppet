53a17686169f122e951b01456f732c1df6b355c7
Kevin Kuehler <kkuehler@berkeley.edu>
2017-12-03 04:12:47 -0800

    Condense HTTP and HTTPS to one rule
    I think it's better style to have the two together, since every
    manifest that includes this class already expects both HTTP and HTTPS.

--- modules/ocf/manifests/firewall/allow_http.pp
+++ modules/ocf/manifests/firewall/allow_http.pp
@@ -1,19 +1,11 @@
 # firewall input rule to allow http and https
 class ocf::firewall::allow_http {
   ocf::firewall::firewall46 {
-    '100 allow https':
+    '100 allow http and https':
       opts => {
         chain  => 'PUPPET-INPUT',
         proto  => 'tcp',
-        dport  => 443,
-        action => 'accept',
-      };
-
-    '100 allow http':
-      opts => {
-        chain  => 'PUPPET-INPUT',
-        proto  => 'tcp',
-        dport  => 80,
+        dport  => [80, 443],
         action => 'accept',
       };
   }
