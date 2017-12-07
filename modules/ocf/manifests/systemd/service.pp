d07198866ccf8f21b044d4417d09f30cbaa1c714
Kevin Kuehler <kkuehler@berkeley.edu>
2017-12-07 03:58:36 -0800

    ocf_mirrors: add systemd timer resource
    The ocf::systemd::timer implementation creates a systemd
    timer and a corresponding service. The service will
    activate ones the timer has expired. Both the timer
    and service are implemented using templates.
    
    Furthermore, we create a more specific ocf_mirrors::timer
    that utilizes the OnCalendar option -- the systemd analogue
    to cron jobs.
    
    Lastly, we ensure that if any timer resource is created,
    a cron job under the same name is purged.

--- modules/ocf/manifests/systemd/service.pp
+++ modules/ocf/manifests/systemd/service.pp
@@ -4,18 +4,23 @@ define ocf::systemd::service(
   $ensure = 'running',
   $enable = true,
 ) {
+  if $content and $source {
+    fail('You may not supply both content and source parameters to ocf::systemd::service')
+  }
+  elsif $content == undef and $source == undef {
+    fail('You must supply either the content or source parameter to ocf::systemd::service')
+  }
+
   file { "/etc/systemd/system/${title}.service":
     source  => $source,
     content => $content,
     notify  => Exec['systemd-reload'],
     require => Package['systemd-sysv'],
-  }
-
+  } ~>
   service { $title:
     ensure    => $ensure,
     enable    => $enable,
     provider  => systemd,
-    subscribe => File["/etc/systemd/system/${title}.service"],
     require   => [
       Package['systemd-sysv'],
       File["/etc/systemd/system/${title}.service"],
