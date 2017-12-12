25307a25d82cbe2531ce4841a8ffa4602fa59590
Kevin Peng <kpengboy@ocf.berkeley.edu>
2017-12-11 23:27:50 -0800

    Only try to resolve IPv6 addresses when IPv6 active
    puppetlabs-firewall uses the ruby Resolv class to resolve hostnames.
    Resolv [only resolves IPv6 addresses when there is a public IPv6
    address][resolv]. So, only try to apply IPv6 firewall rules with
    host names when the local host has an IPv6 address configured.
    
    [resolv]: https://github.com/ruby/ruby/blob/a1a20cfaa2cf73ae8daaf2123d7c5b513427162e/lib/resolv.rb#L412

--- modules/ocf/manifests/firewall/firewall46.pp
+++ modules/ocf/manifests/firewall/firewall46.pp
@@ -7,12 +7,17 @@ define ocf::firewall::firewall46($opts,) {
     *         => $opts,
   }
 
-  firewall { "${title} (IPv6)":
-    provider  => 'ip6tables',
-    require   => $require,
-    subscribe => $subscribe,
-    before    => $before,
-    notify    => $notify,
-    *         => $opts,
+  # Ruby's Resolv class doesn't think it should resolve IPv6 addresses if the
+  # local host doesn't have a public IPv6 address. Thus we only try to apply
+  # IPv6 firewall rules here if the host already has an IPv6 address.
+  if $::ipaddress6 {
+    firewall { "${title} (IPv6)":
+      provider  => 'ip6tables',
+      require   => $require,
+      subscribe => $subscribe,
+      before    => $before,
+      notify    => $notify,
+      *         => $opts,
+    }
   }
 }
