3481e1d89c475bb48c8bbea00ab8a03345b58a8f
asaiacai <asai@berkeley.edu>
2018-01-17 21:40:38 -0800

    added opstaff schedule

--- modules/ocf_www/manifests/site/shorturl.pp
+++ modules/ocf_www/manifests/site/shorturl.pp
@@ -71,6 +71,8 @@ class ocf_www::site::shorturl {
       {rewrite_rule => '^/mlk$ https://www.ocf.berkeley.edu/mlk [R]'},
       {rewrite_rule => '^/mon$ https://munin.ocf.berkeley.edu/ [R]'},
       {rewrite_rule => '^/mysql$ https://www.ocf.berkeley.edu/docs/services/mysql/ [R]'},
+      {rewrite_rule => '^/opstaff-schedule$ https://docs.google.com/spreadsheets/d/1LUyU2AFY6YV403UkKUf_DhYn_-iqJGIO91JRRchGM8A/edit?usp=sharing [R]'},
+      {rewrite_rule => '^/os$ https://docs.google.com/spreadsheets/d/1LUyU2AFY6YV403UkKUf_DhYn_-iqJGIO91JRRchGM8A/edit?usp=sharing [R]'},
       {rewrite_rule => '^/password$ https://www.ocf.berkeley.edu/account/password [R]'},
       {rewrite_rule => '^/printing$ https://www.ocf.berkeley.edu/announcements/2016-02-09/printing [R]'},
       {rewrite_rule => '^/rt$ https://rt.ocf.berkeley.edu/ [R]'},
