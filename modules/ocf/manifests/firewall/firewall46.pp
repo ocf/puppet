ff49c22740c61195ee4c4e358a3d258107bcd3e8
Kevin Peng <kpengboy@ocf.berkeley.edu>
2018-02-25 01:07:35 -0800

    ocf: don't resolve IPv6 addresses when only link-local address exists

--- modules/ocf/manifests/firewall/firewall46.pp
+++ modules/ocf/manifests/firewall/firewall46.pp
@@ -10,7 +10,7 @@ define ocf::firewall::firewall46($opts,) {
   # Ruby's Resolv class doesn't think it should resolve IPv6 addresses if the
   # local host doesn't have a public IPv6 address. Thus we only try to apply
   # IPv6 firewall rules here if the host already has an IPv6 address.
-  if $::ipaddress6 {
+  if $::ipaddress6 and $::ipaddress6 !~ /^fe80::/ {
     firewall { "${title} (IPv6)":
       provider  => 'ip6tables',
       require   => $require,
