e2aeaeca8bd721cc4d3c17fae9f64891d0a88b2c
Kevin Kuehler <kkuehler@berkeley.edu>
2018-03-11 21:27:19 -0700

    Separate centos and centos-altarch

---
+++ modules/ocf_mirrors/manifests/centos_altarch.pp
@@ -0,0 +1,26 @@
+class ocf_mirrors::centos_altarch {
+  file { '/opt/mirrors/project/centos-altarch':
+    ensure  => directory,
+    source  => 'puppet:///modules/ocf_mirrors/project/centos-altarch/',
+    owner   => mirrors,
+    group   => mirrors,
+    mode    => '0755',
+    recurse => true,
+  }
+
+  ocf_mirrors::monitoring {
+    'centos-altarch':
+      type          => 'unix_timestamp',
+      upstream_host => 'mirror.centos.org',
+      upstream_path => '/altarch',
+      ts_path       => 'TIME';
+  }
+
+  cron {
+    'centos-altarch':
+      command => '/opt/mirrors/project/centos-altarch/sync-altarch > /dev/null',
+      user    => 'mirrors',
+      hour    => '*/3',
+      minute  => '44';
+  }
+}
