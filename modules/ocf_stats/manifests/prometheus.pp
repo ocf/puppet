5788f012b6f290b3b0956979f0449e677913db16
Daniel Kessler <daniel@dkess.me>
2018-03-15 13:56:34 -0700

    fix prometheus http path

--- modules/ocf_stats/manifests/prometheus.pp
+++ modules/ocf_stats/manifests/prometheus.pp
@@ -2,7 +2,7 @@
 class ocf_stats::prometheus {
   class { '::prometheus':
     version        => '2.0.0',
-    extra_options  => '--web.listen-address="127.0.0.1:9090" --web.route-prefix=/prometheus',
+    extra_options  => '--web.listen-address="127.0.0.1:9090" --web.external-url=http://127.0.0.1/prom',
     alerts         => {},
     scrape_configs => [
       { 'job_name' => 'prometheus',
