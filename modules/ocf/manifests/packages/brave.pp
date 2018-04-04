5083c1d1d62cee4cc4d0d7ca94b279dec4bfbd54
Kevin Kuehler <kkuehler@berkeley.edu>
2018-04-03 17:45:35 -0700

    Enable user namespaces for Brave sandboxing

--- modules/ocf/manifests/packages/brave.pp
+++ modules/ocf/manifests/packages/brave.pp
@@ -4,4 +4,8 @@ class ocf::packages::brave {
   }
 
   package { 'brave':; }
+
+  # On Debian userns is compiled-in but disabled by deafult.
+  # See more here: https://chromium.googlesource.com/chromium/src/+/lkcr/docs/linux_sandboxing.md#User-namespaces-sandbox
+  sysctl { 'kernel.unprivileged_userns_clone': value =>  '1' }
 }
