e612e8d4b5d7c0863e0fb423f150fd5f2ebc8af4
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-04-18 12:32:34 -0700

    trigger cleanup on session logout

--- modules/ocf_desktop/files/stats/update-delay.sh
+++ modules/ocf_desktop/files/stats/update-delay.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # this script is usually called by lightdm during user login/logout;
 # example: sudo -u ocfstats "/opt/stats/update-delay.sh" &
-sleep 1
-cd /opt/stats && /opt/stats/update.sh
+sleep 1 && /opt/stats/update.sh "$1"
