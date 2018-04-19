e60cb9cec5648e1083182a11a35423550804e9a7
Kevin Kuehler <kkuehler@berkeley.edu>
2018-04-18 22:26:35 -0700

    Add broker server with redis using hitch as tls proxy

--- modules/ocf_broker/manifests/init.pp
+++ modules/ocf_broker/manifests/init.pp
@@ -1,5 +1,7 @@
 class ocf_broker {
-    package { ['redis-server']: }
+    include ocf_ssl::default_bundle
+
+    package { ['redis-server', 'hitch']: }
 
     service { 'redis-server':
       require => Package['redis-server'],
@@ -13,7 +15,7 @@ class ocf_broker {
       notify   => Service['redis-server'];
     }
 
-    $redis_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('ocfbroker::redis::password'))
+    $redis_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('broker::redis::password'))
 
     augeas { '/etc/redis/redis.conf':
         lens      => 'Spacevars.simple_lns',
@@ -22,11 +24,28 @@ class ocf_broker {
           'set port 6379',
           "set requirepass ${redis_password}",
           'set appendonly yes',
-          'set appendfsynch everysec',
           'rm save',
         ],
         show_diff => false,
         require   => File['/etc/redis/redis.conf'],
         notify    => Service['redis-server'];
+    }
+
+    # We already have an OCF member with the username "hitch", so dpkg
+    # chooses "_hitch" as a fallback username.
+    user { '_hitch':
+      groups  => 'ssl-cert',
+      notify  => Service['hitch'],
+      require => Package['hitch'],
+    }
+
+    file { '/etc/hitch/hitch.conf':
+      content => template('ocf_broker/hitch.conf.erb'),
+      notify  => Service['hitch'],
+      require => Package['hitch'],
+    }
+
+    service { 'hitch':
+      require => [ Package['hitch'], Service['redis-server']];
     }
 }
