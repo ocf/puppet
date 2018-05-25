606f678ca8110468986bf5458e64938487637bb5
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-05-25 16:49:48 -0700

    Add conditional for jessie vs stretch

--- modules/ocf_www/manifests/mod/php.pp
+++ modules/ocf_www/manifests/mod/php.pp
@@ -1,10 +1,19 @@
 class ocf_www::mod::php {
-  package { ['php-cgi', 'php-apcu']:; }
+  if $::lsbdistcodename == 'jessie' {
+    package { ['php5-cgi', 'php5-apcu']:; }
 
-  file {
-    '/etc/php/7.0/cgi/conf.d/99-ocf.ini':
+    file { '/etc/php5/cgi/conf.d/99-ocf.ini':
+      source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
+      require => Package['php5-cgi'],
+      notify  => Service['httpd'];
+    }
+  } else {
+    package { ['php-cgi', 'php-apcu']:; }
+
+    file { '/etc/php/7.0/cgi/conf.d/99-ocf.ini':
       source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
       require => Package['php-cgi'],
       notify  => Service['httpd'];
+    }
   }
 }
