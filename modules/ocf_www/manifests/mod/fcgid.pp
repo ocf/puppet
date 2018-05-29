5ad94f16b5fa7e0787d1ba9c67bc09dd70751909
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-05-28 22:08:37 -0700

    Use php-fcgi-wrapper from puppet instead of NFS

--- modules/ocf_www/manifests/mod/fcgid.pp
+++ modules/ocf_www/manifests/mod/fcgid.pp
@@ -2,10 +2,23 @@ class ocf_www::mod::fcgid {
   include apache::mod::fcgid
   include apache::mod::suexec
 
+  file {
+    '/usr/local/bin/suexec':
+      ensure => directory;
+
+    # This wrapper script is in a separate subdirectory of /usr/local/bin so
+    # that none of the other scripts/files in /usr/local/bin are allowed by our
+    # suexec patch except the ones explicitly put in the suexec subdirectory
+    '/usr/local/bin/suexec/php-fcgi-wrapper':
+      source  => 'puppet:///modules/ocf_www/php-fcgi-wrapper',
+      mode    => '0755',
+      require => File['/usr/local/bin/suexec'];
+  }
+
   apache::custom_config { 'fastcgi_options':
     content => "
       FcgidCmdOptions /usr/lib/apache2/suexec ConnectTimeout 15 MaxProcesses 1000 MinProcesses 0
-      FcgidWrapper /services/http/suexec/php-fcgi-wrapper
+      FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper
     ",
   }
 }
