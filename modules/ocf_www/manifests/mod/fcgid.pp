a26949c4a85e336ef0a660c5d6fcf026162f06fc
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-05-31 13:28:34 -0700

    Add systemd service for restarting fcgi processes
    This is definitely a bit janky, but I still think it's the best solution
    I've come across and seems like the safest option, since it isn't
    in the critical path of any requests. It's also faster and less
    disruptive than just setting the Fcgid settings to have a low lifetime
    for any running processes, because that would kill off any started PHP
    processes too, which is undesirable.

--- modules/ocf_www/manifests/mod/fcgid.pp
+++ modules/ocf_www/manifests/mod/fcgid.pp
@@ -2,6 +2,8 @@ class ocf_www::mod::fcgid {
   include apache::mod::fcgid
   include apache::mod::suexec
 
+  package { 'python3-psutil':; }
+
   file {
     '/usr/local/bin/suexec':
       ensure => directory;
@@ -13,21 +15,41 @@ class ocf_www::mod::fcgid {
       source  => 'puppet:///modules/ocf_www/php-fcgi-wrapper',
       mode    => '0755',
       require => File['/usr/local/bin/suexec'];
+
+    '/usr/local/bin/fcgi-restarter':
+      source  => 'puppet:///modules/ocf_www/fcgi-restarter',
+      mode    => '0754',
+      notify  => Service['fcgi-restarter'],
+      require => Package['python3-psutil'];
+  }
+
+  ocf::systemd::service { 'fcgi-restarter':
+    source  => 'puppet:///modules/ocf_www/fcgi-restarter.service',
+    require => File['/usr/local/bin/fcgi-restarter'],
   }
 
   apache::custom_config { 'fcgid_options':
-    content => "
-      FcgidCmdOptions /usr/lib/apache2/suexec ConnectTimeout 15 MaxProcesses 1000 MinProcesses 0
+    content => '
+      # A process can live for at max an hour, whether it is idle or not
       FcgidIdleTimeout 3600
       FcgidProcessLifeTime 3600
+
+      # After 200 requests, a process will be killed off (and another spawned
+      # on the next request), to protect from memory leaks
       FcgidMaxRequestsPerProcess 200
+
+      # The default for this is 3, not 0, meaning that processes will linger
+      # forever when started, but with many users this does not work
       FcgidMinProcessesPerClass 0
+
+      # These are specified individually because .fcgi files do not use this
+      # wrapper and there is no regex option for it
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .php
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .php3
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .php4
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .php5
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .php7
       FcgidWrapper /usr/local/bin/suexec/php-fcgi-wrapper .phtml
-    ",
+    ',
   }
 }
