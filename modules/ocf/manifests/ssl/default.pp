7ea3dbd5d70bdff10f0bbc0c1cd31e81c5dbc7f3
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-07-09 02:41:38 -0700

    Special case certs for the DNS @ record

--- modules/ocf/manifests/ssl/default.pp
+++ modules/ocf/manifests/ssl/default.pp
@@ -1,14 +1,22 @@
 # Provides the key, certificate, and Let's Encrypt CA certificate bundle at
 # /etc/ssl/private/$cert_name.{key,crt,bundle,intermediate}
 class ocf::ssl::default {
-  # Attempt to collect all domains for a host to include in a SSL certificate
-  $domains = suffix(delete(concat(
+  # Attempt to collect all domains for a host to include in a SSL certificate.
+  # The '@' record needs to be handled in a special case, since
+  # "@.ocf.berkeley.edu" and "@.ocf.io" are both not valid domains
+  if '@' in $::dnsA {
+    $extra_domains = ['ocf.berkeley.edu', 'ocf.io']
+  } else {
+    $extra_domains = []
+  }
+
+  $domains = $extra_domains + suffix(delete(concat(
     [$::hostname],
-    $::dnsA,
+    delete($::dnsA, '@'),
     $::dnsCname,
   ), ''), '.ocf.berkeley.edu') + suffix(delete(concat(
     [$::hostname],
-    $::dnsA,
+    delete($::dnsA, '@'),
     $::dnsCname,
   ), ''), '.ocf.io')
 
