1df79144f02503d3bc3106be4a3c6a020d01d33b
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2018-07-14 17:04:28 -0700

    update create credentials

--- modules/ocf_admin/manifests/create/app.pp
+++ modules/ocf_admin/manifests/create/app.pp
@@ -3,36 +3,15 @@ class ocf_admin::create::app {
 
   package { 'ocf-approve':; }
 
-  # these get shoved into URIs, and we can't deal with escaping
-  $redis_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('create::redis::password'))
-  $mysql_password = assert_type(Pattern[/^[a-zA-Z0-9]*$/], hiera('create::mysql::password'))
-
-  $broker = "redis://:${redis_password}@admin.ocf.berkeley.edu:6378"
-  $backend = $broker
-
-  $redis_uri = "rediss://:${redis_password}@admin.ocf.berkeley.edu:6378"
-
-  # TODO: provide this entire file (rt#5887)
-  augeas { '/etc/ocf-create/ocf-create.conf':
-    lens      => 'Puppet.lns',
-    incl      => '/etc/ocf-create/ocf-create.conf',
-    changes   =>  [
-      "set mysql/uri mysql+pymysql://ocfcreate:${mysql_password}@mysql/ocfcreate",
-      "set celery/broker ${broker}",
-      "set celery/backend ${backend}",
-      "set redis/uri ${redis_uri}",
-    ],
-    show_diff => false,
-  }
-
   file {
     '/etc/ocf-create':
       ensure => directory;
 
     # TODO: ideally this file wouldn't be directly readable by staff
     '/etc/ocf-create/ocf-create.conf':
-      group => ocfstaff,
-      mode  => '0440';
+      group  => ocfstaff,
+      source => 'puppet:///private/create.conf',
+      mode   => '0440';
 
     '/etc/ocf-create/create.keytab':
       mode   => '0400',
