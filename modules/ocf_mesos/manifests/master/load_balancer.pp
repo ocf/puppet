051461f3e1fc5bac50042261826001b625d0157f
Daniel Kessler <daniel@dkess.me>
2018-08-24 15:00:02 -0700

    Add grafana to load balancer

--- modules/ocf_mesos/manifests/master/load_balancer.pp
+++ modules/ocf_mesos/manifests/master/load_balancer.pp
@@ -106,6 +106,12 @@ class ocf_mesos::master::load_balancer($marathon_http_password) {
     service_port   => 10007,
   }
 
+  ocf_mesos::master::load_balancer::http_vhost { 'grafana':
+    server_name    => 'grafana.ocf.berkeley.edu',
+    server_aliases => ['grafana'],
+    service_port   => 10011,
+  }
+
   # Port 10008 is used by thelounge, it is proxied to by ocf_irc
   # Port 10009 is used by puppetboard, it is proxied to by ocf_puppet
   # Port 10010 cannot be used due to https://github.com/moby/moby/issues/37507
