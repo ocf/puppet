e8b158469b39cb9b321e762e28101d9b62533e51
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-10-05 13:38:46 -0700

    Prometheus printer metrics

--- modules/ocf_prometheus/manifests/init.pp
+++ modules/ocf_prometheus/manifests/init.pp
@@ -23,9 +23,34 @@ class ocf_prometheus {
     creates => '/var/local/prometheus-nodes.json',
   }
 
+  file {
+    '/usr/local/bin/gen-prometheus-printers':
+      source => 'puppet:///modules/ocf_prometheus/gen-prometheus-printers',
+      mode   => '0755';
+  }
+
+  cron { 'gen-prometheus-printers':
+    command => '/usr/local/bin/gen-prometheus-printers /var/local/prometheus-printers.json',
+    minute  => '0',
+    require => File['/usr/local/bin/gen-prometheus-printers'];
+  }
+
+  exec { 'gen-promethues-printers-initial':
+    command => '/usr/local/bin/gen-prometheus-printers /var/local/prometheus-printers.json',
+    creates => '/var/local/prometheus-printers.json',
+  }
+
+  file {
+    '/etc/prometheus/rules.d':
+      ensure  => 'directory',
+      source  => 'puppet:///modules/ocf_prometheus/rules.d',
+      recurse => true;
+  }
+
   class { '::prometheus::server':
     version        => '2.4.2',
     extra_options  => '--web.listen-address="127.0.0.1:9090"',
+    rule_files     => [ '/etc/prometheus/rules.d/*' ],
     alerts         => {},
     scrape_configs => [
       {
@@ -50,6 +75,35 @@ class ocf_prometheus {
             'refresh_interval' => '1h',
           },
         ],
+      },
+      {
+        'job_name'        => 'printer',
+        'scrape_interval' => '30s',
+        'scrape_timeout'  => '5s',
+
+        'file_sd_configs' => [
+          {
+            'files'            => [ '/var/local/prometheus-printers.json' ],
+            'refresh_interval' => '1h',
+          },
+        ],
+
+        'metrics_path'    => '/snmp',
+        'params'          => { 'module' => [ 'printer' ]},
+
+        # Relabel trick to make sure we scrape from the exporter and not from
+        # the printers themselves, see
+        # https://github.com/prometheus/snmp_exporter#prometheus-configuration
+        'relabel_configs' => [
+          {
+            'source_labels' => [ '__address__' ],
+            'target_label'  => '__param_target',
+          },
+          {
+            'target_label' => '__address__',
+            'replacement'  => 'lb.ocf.berkeley.edu:10020',
+          },
+        ]
       }
     ]
   }
