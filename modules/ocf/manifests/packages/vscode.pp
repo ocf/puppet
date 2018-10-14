888d69504828ffee43d04baf818d50f6264dd7b9
Christopher Cooper <cooperc@ocf.berkeley.edu>
2018-10-13 20:47:21 -0700

    do not open browser windows on VS Code launch

--- modules/ocf/manifests/packages/vscode.pp
+++ modules/ocf/manifests/packages/vscode.pp
@@ -4,4 +4,15 @@ class ocf::packages::vscode {
   }
 
   package { 'code':; }
+
+  augeas { 'code.desktop Exec':
+    lens    => 'Desktop.lns',
+    incl    => '/usr/share/applications/code.desktop',
+    changes => [
+      'set "Desktop Entry/Exec" "/usr/share/code/code --skip-getting-started --unity-launch %F"'
+    ],
+    onlyif  => [
+      'match */Exec not_include --skip-getting-started'
+    ]
+  }
 }
