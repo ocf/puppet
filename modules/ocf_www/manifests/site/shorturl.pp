3c9616a3402cf66676141c9b3c4cc05ed4af6e84
Mathew Cha <mdcha@berkeley.edu>
2018-10-24 18:43:51 -0700

    Updating the survey shorturl

--- modules/ocf_www/manifests/site/shorturl.pp
+++ modules/ocf_www/manifests/site/shorturl.pp
@@ -99,7 +99,7 @@ class ocf_www::site::shorturl {
       {rewrite_rule => '^/status$ https://status.ocf.berkeley.edu/ [R]'},
       {rewrite_rule => '^/stretch$ https://www.ocf.berkeley.edu/docs/staff/backend/stretch/ [R]'},
       {rewrite_rule => '^/stf-cost-breakdown$ https://docs.google.com/spreadsheets/d/1U3YfU5S1hyi4c9u1vME84lz5hQPz23LKWCaD8mq4JHI/edit [R]'},
-      {rewrite_rule => '^/survey$ https://goo.gl/forms/nP3ijHvqn5SE4bGh1 [R]'},
+      {rewrite_rule => '^/survey$ https://goo.gl/forms/sk1s0dnuxDY71vBR2 [R]'},
       {rewrite_rule => '^/tw(/.*)?$ https://ocf.io/twitter$1 [R]'},
       {rewrite_rule => '^/twitter(/.*)?$ https://twitter.com/ucbocf$1 [R]'},
       {rewrite_rule => '^/tv(/.*)?$ https://www.ocf.berkeley.edu/tv$1 [R]'},
