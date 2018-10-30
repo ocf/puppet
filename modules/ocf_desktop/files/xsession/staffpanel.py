4c0703ff9f86ac05f3a4d4a45c81a1cab318855f
NotRyan <ryanchan989@gmail.com>
2018-10-30 01:01:41 -0700

    Script to remove visibility toggle from non-staff

---
+++ modules/ocf_desktop/files/xsession/staffpanel.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import os
+from ocflib.account.utils import is_staff
+from ocflib.misc.whoami import current_user
+
+LAUNCHER_SETTINGS_PATH = os.path.expanduser('~/.config/xfce4/panel/launcher-21/switch.desktop')
+
+def main(argv=None):
+    #Removes the visibility toggle button from the panel if the user is not staff
+    if not is_staff(current_user()):
+        os.remove(LAUNCHER_SETTINGS_PATH)
+
+if __name__ == '__main__':
+    sys.exit(main())
