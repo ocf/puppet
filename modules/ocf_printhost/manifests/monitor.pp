0ea4aa871b7703f2e65db3a57f4937e4e76cdd65
Daniel Kessler <dkessler@ocf.berkeley.edu>
2018-11-08 19:33:22 -0800

    Add CUPS monitoring and alerting

---
+++ modules/ocf_printhost/manifests/monitor.pp
@@ -0,0 +1,25 @@
+class ocf_printhost::monitor {
+  package { ['libcups2-dev', 'python3-cups', 'python3-prometheus-client']: }
+
+  file {
+    '/var/local/prometheus':
+      ensure => directory;
+  } ->
+  file {
+    '/usr/local/bin/monitor-cups':
+      source => 'puppet:///modules/ocf_printhost/monitor-cups',
+      mode   => '0755';
+  } ->
+  exec { 'monitor-cups-initial':
+    command => '/usr/local/bin/monitor-cups /var/local/prometheus/cups.prom',
+    creates => '/var/local/prometheus/cups.prom',
+  } ->
+  cron { 'monitor-cups':
+    command => '/usr/local/bin/monitor-cups /var/local/prometheus/cups.prom',
+    # Run every minute
+  }
+
+  class { 'prometheus::node_exporter':
+    extra_options => '--collector.textfile.directory /var/local/prometheus',
+  }
+}
