c78e8972c584c73e686d514aa356723307e51fe8
Kevin Kuehler <keur@ocf.berkeley.edu>
2018-11-15 02:06:38 -0800

    kubernetes: pin docker version
    Kubernetes is very specific about which versions of docker it will
    play nice with. For that reason, Kubernetes nodes will need an
    older version of docker installed. To keep apt-dater from updating
    docker to a newer version, we pin the desired version in
    /etc/apt/preferences.d/docker-ce.pref.

--- modules/ocf/manifests/packages/docker_kube.pp
+++ modules/ocf/manifests/packages/docker_kube.pp
@@ -3,8 +3,18 @@ class ocf::packages::docker_kube {
     stage => first,
   }
 
+  $docker_kube_version = lookup('kubernetes::docker_version')
+
+  # Kubernetes is specific about which versions of docker it will
+  # work reliably with.
   package {
     'docker-ce':
-      ensure => lookup('kubernetes::docker_version');
+      ensure => $docker_kube_version;
+  } ->
+  apt::pin { 'docker-ce':
+    ensure   => present,
+    packages => ['docker-ce'],
+    priority => 1001,
+    version  => $docker_kube_version;
   }
 }
