bc41a4417186515c2432c27814eae20c9792bd98
Jason Perrin <jvperrin@ocf.berkeley.edu>
2018-11-26 01:54:32 -0800

    Clean up php module for stretch

--- modules/ocf_www/manifests/mod/php.pp
+++ modules/ocf_www/manifests/mod/php.pp
@@ -1,19 +1,9 @@
 class ocf_www::mod::php {
-  if $::lsbdistcodename == 'jessie' {
-    package { ['php5-cgi', 'php5-apcu']:; }
+  package { ['php-cgi', 'php-apcu']:; }
 
-    file { '/etc/php5/cgi/conf.d/99-ocf.ini':
-      source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
-      require => Package['php5-cgi'],
-      notify  => Service['httpd'];
-    }
-  } else {
-    package { ['php-cgi', 'php-apcu']:; }
-
-    file { '/etc/php/7.0/cgi/conf.d/99-ocf.ini':
-      source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
-      require => Package['php-cgi'],
-      notify  => Service['httpd'];
-    }
+  file { '/etc/php/7.0/cgi/conf.d/99-ocf.ini':
+    source  => 'puppet:///modules/ocf_www/apache/mods/php/99-ocf.ini',
+    require => Package['php-cgi'],
+    notify  => Service['httpd'];
   }
 }
