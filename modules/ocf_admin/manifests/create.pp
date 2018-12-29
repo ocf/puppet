5e9b6902eaafb59380da1065b013809ece2c3dc4
Daniel Kessler <daniel@dkess.me>
2018-12-28 17:09:09 -0800

    Remove redis on supernova (#460)
    * Remove redis on supernova
    
    * Actually remove redis from supernova
    
    * Remove unused create.sudoers file

--- modules/ocf_admin/manifests/create.pp
+++ modules/ocf_admin/manifests/create.pp
@@ -1,4 +1,39 @@
 class ocf_admin::create {
-  include ocf_admin::create::app
-  include ocf_admin::create::redis
+  package { 'ocf-approve':; }
+
+  file {
+    '/etc/ocf-create':
+      ensure => directory;
+
+    # TODO: ideally this file wouldn't be directly readable by staff
+    '/etc/ocf-create/ocf-create.conf':
+      group  => ocfstaff,
+      source => 'puppet:///private/create.conf',
+      mode   => '0440';
+
+    '/etc/ocf-create/create.keytab':
+      mode   => '0400',
+      source => 'puppet:///private/create.keytab';
+
+    '/etc/ocf-create/create.key':
+      mode   => '0400',
+      source => 'puppet:///private/create.key';
+
+    '/etc/ocf-create/create.pub':
+      mode   => '0444',
+      source => 'puppet:///private/create.pub';
+  }
+
+  # temporary, for removing the old redis
+  package { 'redis-server':
+    ensure => 'absent',
+  }
+
+  package { 'hitch':
+    ensure => 'absent',
+  }
+
+  user { '_hitch':
+    ensure => 'absent',
+  }
 }
