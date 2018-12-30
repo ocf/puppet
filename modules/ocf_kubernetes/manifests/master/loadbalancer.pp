f1fa71d59f4a7578f12577463726a0a71dd27f90
Kevin Kuehler <keur@ocf.berkeley.edu>
2018-12-30 05:15:14 -0800

    kubernetes: add keepalived to masters
    VIP will be assigned to a random kubernetes master. The chosen master
    will route all traffic into the kubernetes cluster.

--- modules/ocf_kubernetes/manifests/master/loadbalancer.pp
+++ modules/ocf_kubernetes/manifests/master/loadbalancer.pp
@@ -1,10 +1,35 @@
 class ocf_kubernetes::master::loadbalancer {
-  include ocf::ssl::default
   include ::haproxy
 
   $kubernetes_worker_nodes = lookup('kubernetes::worker_nodes')
   $kubernetes_workers_ipv4 = $kubernetes_worker_nodes.map |$worker| { ldap_attr($worker, 'ipHostNumber') }
   $haproxy_ssl = "/etc/ssl/private/${::fqdn}.pem"
+
+  # At any given time, only one kubernetes master will hold
+  # the first IP. The master holding the IP will handle all
+  # HAProxy requests and send them into the cluster.
+  #
+  # TODO: If we expose TCP services, we may need to add more.
+  #
+  # IPv6 addresses have to be specified separately as they cannot be in the vrrp
+  # packet together (keepalived 1.2.20+) so they need to be in a
+  # virtual_ipaddress_excluded block instead.
+  $virtual_addresses = [
+    # Primary load balancer IP (v4)
+    '169.229.226.79',
+  ]
+  $virtual_addresses_v6 = [
+    # Primary load balancer IP (v6)
+    '2607:f140:8801::1:79',
+  ]
+  $keepalived_secret = lookup('kubernetes::keepalived::secret')
+
+  package { 'keepalived':; } ->
+  file { '/etc/keepalived/keepalived.conf':
+    content => template('ocf_kubernetes/master/loadbalancer/keepalived.conf.erb'),
+    mode    => '0400',
+  } ~>
+  service { 'keepalived': }
 
   haproxy::frontend { 'kubernetes-frontend':
     mode    => 'http',
