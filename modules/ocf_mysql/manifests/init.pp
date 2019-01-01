0e8a84987c2a7afd56dc71e2705d679ab1808df6
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2019-01-01 10:37:13 -0800

    mysql: add ssl service notify

--- modules/ocf_mysql/manifests/init.pp
+++ modules/ocf_mysql/manifests/init.pp
@@ -20,10 +20,12 @@ class ocf_mysql {
   file { '/etc/mysql/mariadb.conf.d/99-ocf.cnf':
     content => template('ocf_mysql/99-ocf.cnf'),
     require => Class['ocf::packages::mysql_server'],
-    notify  => Service['mysql'],
+    notify  => Service['mariadb'],
   }
 
-  service { 'mysql': }
+  service { 'mariadb':
+    subscribe => Class['ocf::ssl::default'];
+  }
 
   # allow mysql (3306 udp/tcp)
   ocf::firewall::firewall46 {
