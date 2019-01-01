9cc0305a07dfa7c9bc2fc0506996496a8e55e656
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2019-01-01 13:00:46 -0800

    keyserver: open firewall, recon on 0.0.0.0, open domain...
    also add subscribes and other such things

--- modules/ocf_keyserver/manifests/init.pp
+++ modules/ocf_keyserver/manifests/init.pp
@@ -35,6 +35,27 @@ class ocf_keyserver {
   # how to recreate this config in 'native' puppet-nginx
   file {
     '/etc/nginx/sites-enabled/sks-web-proxy.conf':
-      content => template('ocf_keyserver/sks-web-nginx.erb');
+      content => template('ocf_keyserver/sks-web-nginx.erb'),
+      notify  => Class['Nginx::Service'];
+  }
+
+  ocf::firewall::firewall46 {
+    '101 allow sks-recon':
+      opts => {
+        chain  => 'PUPPET-INPUT',
+        proto  => ['tcp'],
+        dport  => 11370,
+        action => 'accept',
+      };
+  }
+
+  ocf::firewall::firewall46 {
+    '101 allow sks-hkp':
+      opts => {
+        chain  => 'PUPPET-INPUT',
+        proto  => ['tcp'],
+        dport  => 11371,
+        action => 'accept',
+      };
   }
 }
