9b11e5371f9c2ddaa2b81ba5cce4747fe42bbaaa
Abizer Lokhandwala <abizer@ocf.berkeley.edu>
2019-01-02 00:34:53 -0800

    mysql: use chaining arrows to improve dependencies

--- modules/ocf_mysql/manifests/init.pp
+++ modules/ocf_mysql/manifests/init.pp
@@ -1,31 +1,22 @@
 class ocf_mysql {
+  require ocf::ssl::default;
 
-  include ocf::ssl::default;
-
+  class { 'ocf::packages::mysql_server':
+    manage_service => false,
+  } ->
   user { 'mysql':
     groups => ['ssl-cert'];
-  }
-
+  } ->
   file {
+    '/etc/mysql/mariadb.conf.d/99-ocf.cnf':
+      content => template('ocf_mysql/99-ocf.cnf');
+
     '/root/.my.cnf':
       mode      => '0600',
       source    => 'puppet:///private/root-my.cnf',
       show_diff => false;
-  }
-
-  class { 'ocf::packages::mysql_server':
-    manage_service => false,
-  }
-
-  file { '/etc/mysql/mariadb.conf.d/99-ocf.cnf':
-    content => template('ocf_mysql/99-ocf.cnf'),
-    require => Class['ocf::packages::mysql_server'],
-    notify  => Service['mariadb'],
-  }
-
-  service { 'mariadb':
-    subscribe => Class['ocf::ssl::default'];
-  }
+  } ~>
+  service { 'mariadb': }
 
   # allow mysql (3306 udp/tcp)
   ocf::firewall::firewall46 {
