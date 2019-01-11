8d236b31410b5f6ebf61587af5f5539d738bee7c
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-01-10 18:25:02 -0800

    reword and split up comment explaining dehydrated runs

--- modules/ocf/manifests/ssl/lets_encrypt/dns.pp
+++ modules/ocf/manifests/ssl/lets_encrypt/dns.pp
@@ -10,18 +10,16 @@ define ocf::ssl::lets_encrypt::dns(
     content => "${join($domains, ' ')} > ${title}",
   }
 
-  # Only run the dehydrated command to renew the cert if it is old enough, or
-  # missing some domains. dehydrated does the former check itself, but this
-  # should help with puppet error spam a bit (for instance if Let's Encrypt is
-  # down for any reason) and mean that services can subscribe to this class to
-  # reload after new certs are obtained since this won't refresh unless
-  # dehydrated is re-run for some reason.
-
   $parsed_cert_info = parsejson($::le_cert_info)
 
   $have_cert_info = $title in $parsed_cert_info
   if $have_cert_info {
+
+    # if we have info about the current cert, we need to check to make sure that
+    # the cert includes all domains, and that it is not close to expiring
+
     $cert_expires_soon = $parsed_cert_info[$title]['days_to_expiration'] < 30
+
     # we subtract out any domains that are in the cert, and see if any are left
     $cert_has_all_domains = ($domains - $parsed_cert_info[$title]['cert_names']) =~ Array[String, 0, 0]
   }
@@ -32,8 +30,19 @@ define ocf::ssl::lets_encrypt::dns(
     command     => '/usr/bin/dehydrated --cron --privkey /etc/ssl/lets-encrypt/le-account.key',
     user        => $owner,
     require     => Package['dehydrated-hook-ddns-tsig'],
-    # if the cert's chillin, only exec on notification
+
+    # Only run the dehydrated command to renew the cert if it is old enough, or
+    # missing some domains. dehydrated does the former check itself, but this
+    # should help with puppet error spam a bit (for instance if Let's Encrypt is
+    # down for any reason) and mean that services can subscribe to this class to
+    # reload after new certs are obtained since this won't refresh unless
+    # dehydrated is re-run for some reason.
+
     refreshonly => $have_cert_info and !$cert_expires_soon and $cert_has_all_domains,
+
+    # If we have new domains, the dehydrated config changes, or other related
+    # files are updated, we need to re-run dehydrated anyway
+
     subscribe   => [
       Concat['/var/lib/lets-encrypt/domains.txt'],
       File['/etc/dehydrated/config'],
