eac2bcfe482ba1c6e7dddc4b6afef77a81d8c181
Benjamin Zhang <bzh@ocf.berkeley.edu>
2019-01-11 22:41:24 -0800

    Put GC script in /opt/share/puppet .

--- modules/ocf_docker/manifests/deckschrubber.pp
+++ modules/ocf_docker/manifests/deckschrubber.pp
@@ -1,20 +1,17 @@
 class ocf_docker::deckschrubber {
   package { 'deckschrubber': }
 
-  file { ['/opt/docker', '/opt/docker/registry']:
-    ensure => directory,
-    mode   => '0755',
-  } ->
-  file { '/opt/docker/registry/garbage-collect':
+  file { '/opt/share/puppet/registry-garbage-collect':
     source  => 'puppet:///modules/ocf_docker/garbage-collect',
     mode    => '0755',
     require => [
       Package['deckschrubber'],
       File['/var/lib/registry'],
+      File['/opt/share/puppet'],
     ],
   } ->
   cron { 'registry-gc':
-    command => '/opt/docker/registry/garbage-collect',
+    command => '/opt/share/puppet/registry-garbage-collect',
     weekday => 'Sunday',
     hour    => 7,
     minute  => 0,
