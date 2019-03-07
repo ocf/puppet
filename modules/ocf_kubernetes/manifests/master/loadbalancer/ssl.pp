ed9d441324fad3bbc0179d7ec6f162b3f690024f
Daniel Kessler <dkessler@ocf.berkeley.edu>
2019-03-07 10:10:21 -0800

    remove ratelimit comment

--- modules/ocf_kubernetes/manifests/master/loadbalancer/ssl.pp
+++ modules/ocf_kubernetes/manifests/master/loadbalancer/ssl.pp
@@ -10,12 +10,6 @@ class ocf_kubernetes::master::loadbalancer::ssl(
   # Get all the cnames from the VIP
   $cnames = ldap_attr($vip, 'dnsCname', true)
 
-  # TODO: get the domain name for the VIP and ensure that
-  # is the first thing we pass to bundle, as that will
-  # be the cn that the cert is issued for
-  #
-  # Do this after our rate-limit is lifted >.>
-
   $vfqdns = $cnames.map |$cname| {
     $domains.map |$domain| { "${cname}.${domain}" }
   }
