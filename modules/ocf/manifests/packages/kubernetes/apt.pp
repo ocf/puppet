a0c385a899ee47c7db9e0cb687ea6c4841aad3f6
Benjamin Zhang <bzh@ocf.berkeley.edu>
2019-04-01 22:21:26 -0700

    Pin kubernetes packages in apt.

--- modules/ocf/manifests/packages/kubernetes/apt.pp
+++ modules/ocf/manifests/packages/kubernetes/apt.pp
@@ -10,4 +10,12 @@ class ocf::packages::kubernetes::apt {
     # NOTE: we can't use kubernetes-stretch because kubelet isn't included.
     release  => 'kubernetes-xenial',
   }
+  # This comes from the local variable of the same name in
+  # github.com/puppetlabs/puppetlabs-kubernetes/blob/master/manifests/packages.pp,
+  # which unfortunately is not exposed.
+  $kube_packages = ['kubelet', 'kubectl', 'kubeadm']
+  file { '/etc/apt/preferences.d/kubernetes.pref':
+    ensure  => file,
+    content => template('ocf/apt/kubernetes-pin.pref.erb'),
+  }
 }
