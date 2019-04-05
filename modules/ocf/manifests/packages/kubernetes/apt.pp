a226768ae21a80af388dbe146ca1c2d6f60f5cfd
Jason Perrin <jvperrin@ocf.berkeley.edu>
2019-04-04 18:43:51 -0700

    Use the vendored apt module for kube package pinning

--- modules/ocf/manifests/packages/kubernetes/apt.pp
+++ modules/ocf/manifests/packages/kubernetes/apt.pp
@@ -16,10 +16,13 @@ class ocf::packages::kubernetes::apt {
   # which unfortunately is not exposed.
   $kube_packages = ['kubelet', 'kubectl', 'kubeadm']
   $kube_package_version = lookup('kubernetes::kubernetes_package_version')
-  # Pins each package in $kube_packages to the hiera-specified version,
-  # so it won't get upgraded automatically by apt.
-  file { '/etc/apt/preferences.d/kubernetes.pref':
-    ensure  => file,
-    content => template('ocf/apt/kubernetes-pin.pref.erb'),
+
+  # Pin each package in $kube_packages to the hiera-specified version,
+  # so they won't get upgraded automatically by apt-dater.
+  apt::pin { 'kubernetes':
+    packages    => $kube_packages,
+    version     => $kube_package_version,
+    priority    => 1001,
+    explanation => "Pin kubernetes packages to the hiera-specified version so they won't get upgraded automatically by apt-dater",
   }
 }
