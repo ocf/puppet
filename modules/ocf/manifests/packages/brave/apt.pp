11d77b8476dfbc0117d0fb760b96b839c300f860
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-04-18 14:10:39 -0700

    update brave apt key when it expires
    Brave's APT signing key has a short expiration date: they updated the key on 2019-04-10
    before the key was set to expire on 2019-04-13. This will update the key so that we
    receive the new expiration dates and we can continue to pull from brave's repos.

--- modules/ocf/manifests/packages/brave/apt.pp
+++ modules/ocf/manifests/packages/brave/apt.pp
@@ -1,6 +1,7 @@
 # Include Brave apt repo.
 class ocf::packages::brave::apt {
   apt::key { 'brave':
+    ensure => refreshed,
     id     => 'D8BAD4DE7EE17AF52A834B2D0BB75829C2D4E821',
     server => 'pgp.ocf.berkeley.edu',
   }
