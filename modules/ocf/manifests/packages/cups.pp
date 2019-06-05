cabb0ac357d4a1b8a94ff18488b77e074e6ffdcf
John Xinyu Lin <johnxinyu@ocf.berkeley.edu>
2019-06-04 23:04:18 -0700

    New CUPS settings, to have PDFs rasterized before printing
    This changes CUPS settings, so print jobs first get processed locally, and then
    get passed onto printhost. A Tea4CUPS filter rasterizes all PDF files.
    
    This allows us to remove the PDF rasterization that occurs when opening a PDF
    on the desktops, which is inconvenient for those who just want to read a PDF
    and not actually print it out.

--- modules/ocf/manifests/packages/cups.pp
+++ modules/ocf/manifests/packages/cups.pp
@@ -1,12 +1,12 @@
 class ocf::packages::cups {
 
   # install cups
-  package { [ 'cups', 'cups-bsd' ]: }
+  package { [ 'cups', 'cups-bsd', 'cups-tea4cups' ]: }
 
   file {
     # set print server destination
     '/etc/cups/client.conf':
-      content => "ServerName printhost\nEncryption Always\n",
+      content => "ServerName localhost\nEncryption Always\n",
       require => Package[ 'cups', 'cups-bsd' ];
     # set default printer double
     '/etc/cups/lpoptions':
@@ -14,7 +14,20 @@ class ocf::packages::cups {
       require => Package[ 'cups', 'cups-bsd' ];
     # set default paper size
     '/etc/papersize':
-      content => 'letter'
+      content => 'letter';
+    # set printer configurations
+    '/etc/cups/printers.conf':
+      source => 'puppet:///modules/ocf/packages/cups/printers.conf',
+      group  => 'lp',
+      mode   => '0600';
+    # set rasterizing filter
+    '/usr/lib/cups/filter/raster-filter':
+      source => 'puppet:///modules/ocf/packages/cups/raster-filter',
+      mode   => '0755';
+    # set tea4cups configurations
+    '/etc/cups/tea4cups.conf':
+      source  => 'puppet:///modules/ocf/packages/cups/tea4cups.conf',
+      require => Package['cups-tea4cups'];
   }
 
   service { 'cups':
