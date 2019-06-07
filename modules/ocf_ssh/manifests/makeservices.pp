87f49e7b7bb71f800b7e7d68fcd047bca0a634ee
Daniel Kessler <dkessler@ocf.berkeley.edu>
2019-06-06 17:36:18 -0700

    ssh: add sudo rules and files for makexmpp

--- modules/ocf_ssh/manifests/makeservices.pp
+++ modules/ocf_ssh/manifests/makeservices.pp
@@ -1,7 +1,19 @@
 class ocf_ssh::makeservices {
+  user { 'ocfmakexmpp':
+    comment => 'OCF user to create XMPP accounts',
+    groups  => ['sys'],
+    shell   => '/bin/false',
+    system  => true,
+  }
+
   # enable regular users to run makemysql.py as mysql
   file { '/etc/sudoers.d/makeservices':
     content => "ALL ALL=(mysql) NOPASSWD: /opt/share/utils/makeservices/makemysql-real\n",
+  }
+
+  # enable regular users to run makexmpp.py as ocfmakexmpp
+  file { '/etc/sudoers.d/makexmpp':
+    content => "ALL ALL=(ocfmakexmpp) NOPASSWD: /opt/share/utils/makeservices/makexmpp-real\n",
   }
 
   file {
@@ -12,6 +24,15 @@ class ocf_ssh::makeservices {
 
     '/opt/share/makeservices/makemysql.conf':
       source    => 'puppet:///private/makeservices/makemysql.conf',
+      show_diff => false;
+
+    '/opt/share/makexmpp':
+      ensure => directory,
+      mode   => '0700',
+      owner  => 'ocfmakexmpp';
+
+    '/opt/share/makexmpp/makexmpp.conf':
+      source    => 'puppet:///private/makeservices/makexmpp.conf',
       show_diff => false;
   }
 }
