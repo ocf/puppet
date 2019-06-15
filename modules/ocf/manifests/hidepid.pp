c802c92b404e658c94850a7d86729be90ffed618
Kevin Kuehler <keur@ocf.berkeley.edu>
2019-06-15 16:33:22 -0700

    ocf/modules/hidepid: Work with policykit 0.115
    Since polkitd already runs as root there is no danger in adding this
    now. We get the added benefit of not having to worry about making this
    change when policykit upgrades to 0.115 down the line.

--- modules/ocf/manifests/hidepid.pp
+++ modules/ocf/manifests/hidepid.pp
@@ -7,15 +7,26 @@ class ocf::hidepid {
     name   => $group_cap_ptrace,
     system => true,
     before => Mount['/proc'];
-  } ->
-
-  ocf::systemd::override { 'hidepid':
-    unit    => 'systemd-logind.service',
-    content => "[Service]\nSupplementaryGroups=${group_cap_ptrace}\n",
   }
 
-  # NOTE: When policykit is eventually upgraded to version 0.115 we will need
-  # to make a similar change to that unit file.
+  # systemd-logind and policykit need to access info about all processes.
+  # see here: https://wiki.debian.org/Hardening#Mounting_.2Fproc_with_hidepid
+  ocf::systemd::override { 'hidepid-logind':
+    unit    => 'systemd-logind.service',
+    content => "[Service]\nSupplementaryGroups=${group_cap_ptrace}\n",
+    require => Group[$group_cap_ptrace];
+  }
+
+  if str2bool($::polkit_priv_drop) {
+    # Futureproof for policykit for version 0.115. policykit will create this
+    # user, we just need to add it to capptrace
+    user { 'polkitd':
+      name    => 'polkitd',
+      groups  => $group_cap_ptrace,
+      system  => true,
+      require => Group[$group_cap_ptrace];
+    }
+  }
 
   mount { '/proc':
     # Remounts proc with hidepid=2. This prevents users from seeing
@@ -25,6 +36,7 @@ class ocf::hidepid {
     remounts => true,
     device   => '/proc',
     fstype   => 'procfs',
-    options  => "rw,hidepid=2,gid=${group_cap_ptrace}";
+    options  => "rw,hidepid=2,gid=${group_cap_ptrace}",
+    require  => Group[$group_cap_ptrace];
   }
 }
