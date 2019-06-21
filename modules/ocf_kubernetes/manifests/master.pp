4be7c66095f087e12a669a890583610dc044d761
Frank Dai <fydai@ocf.berkeley.edu>
2019-06-20 23:44:07 -0700

    Move kubernetes webui to a separate repo

--- modules/ocf_kubernetes/manifests/master.pp
+++ modules/ocf_kubernetes/manifests/master.pp
@@ -15,7 +15,6 @@ class ocf_kubernetes::master {
   include ocf::packages::docker_kubernetes
   include ocf::packages::kubernetes
   include ocf_kubernetes::master::loadbalancer
-  include ocf_kubernetes::master::webui
 
   $etcd_version = lookup('kubernetes::etcd_version')
   $etcd_archive = "etcd-v${etcd_version}-linux-amd64.tar.gz"
@@ -66,6 +65,21 @@ class ocf_kubernetes::master {
     'rbac':
       target    => '/etc/ocf-kubernetes/manifests/rbac.yaml',
       subscribe => File['/etc/ocf-kubernetes/manifests/rbac.yaml'];
+  }
+
+  # Kubernetes webui configuration
+  # The webui needs to live in the kube-system namespace so it cannot be
+  # declared in the kube-webui repository
+
+  file {
+    '/etc/ocf-kubernetes/manifests/webui.yaml':
+      source => 'puppet:///modules/ocf_kubernetes/webui.yaml',
+  }
+
+  ocf_kubernetes::apply {
+    'webui':
+      target    => '/etc/ocf-kubernetes/manifests/webui.yaml',
+      subscribe => File['/etc/ocf-kubernetes/manifests/webui.yaml'],
   }
 
   # These are needed because puppetlabs-kubernetes sets the permissions to 600
