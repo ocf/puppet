a330af38f29c7fae52ce329024b34138702f4c45
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-06-22 23:52:56 -0700

    add yubikey-manager to desktops

--- modules/ocf_desktop/manifests/packages.pp
+++ modules/ocf_desktop/manifests/packages.pp
@@ -41,7 +41,7 @@ class ocf_desktop::packages {
     # notifications
     ['libnotify-bin', 'notification-daemon']:;
     # security tools
-    ['scdaemon']:;
+    ['scdaemon', 'yubikey-manager']:;
     # utilities
     ['wakeonlan']:;
     # Xorg
