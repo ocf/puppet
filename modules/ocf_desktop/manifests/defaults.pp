21b7a720db189f3ec90cf1307834b0df30d6aa82
John Xinyu Lin <johnxinyu@ocf.berkeley.edu>
2019-06-23 18:46:29 -0700

    Further removing old PDF rasterization setup
    To ensure that the old setup is gone, I am removing the files
    associated with the old raster-upon-open setup. IMPORTANT: this is
    meant to be TEMPORARY only; if my pull request gets merged, I will
    create a new pull request to remove this code once it has served its
    purpose.

--- modules/ocf_desktop/manifests/defaults.pp
+++ modules/ocf_desktop/manifests/defaults.pp
@@ -4,14 +4,12 @@ class ocf_desktop::defaults {
     source  => 'puppet:///modules/ocf_desktop/xsession/mimeapps.list';
   }
 
+  # temporary only, to get rid of files!
   file { '/usr/local/bin/pdf-open':
-      mode    => '0755',
-      source  => 'puppet:///modules/ocf_desktop/xsession/pdf-open',
-      require => Package['libimage-exiftool-perl'];
+    ensure => absent;
   }
 
   file { '/usr/share/applications/pdfopen.desktop':
-    source  => 'puppet:///modules/ocf_desktop/xsession/pdfopen.desktop',
-    require => File['/usr/local/bin/pdf-open'];
+    ensure => absent;
   }
 }
