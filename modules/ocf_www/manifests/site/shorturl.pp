b5229b720d8d16e554b4fce6304c9766079ebbd9
Christopher Cooper <cooperc@ocf.berkeley.edu>
2019-09-16 11:47:11 -0700

    add internals shorturl for second meeting

--- modules/ocf_www/manifests/site/shorturl.pp
+++ modules/ocf_www/manifests/site/shorturl.pp
@@ -64,6 +64,7 @@ class ocf_www::site::shorturl {
       {rewrite_rule => '^/hpc$ https://www.ocf.berkeley.edu/docs/services/hpc/ [R]'},
       {rewrite_rule => '^/https$ https://status.ocf.berkeley.edu/2014/10/moving-wwwocfberkeleyedu-to-https-only.html [R]'},
       {rewrite_rule => '^/infra$ https://docs.google.com/presentation/d/1tc41z4nJ-UfCSLhg1k_cgaTn0pYrzCiOd3cfoJv9j8Y/edit [R]'},
+      {rewrite_rule => '^/internals?$ https://docs.google.com/presentation/d/1jjuDgt2eML_LIyaK8tQt_ZJL3s1GaGIbbsfZA68sgDY/edit [R]'},
       {rewrite_rule => '^/irc$ https://www.ocf.berkeley.edu/docs/contact/irc/ [R]'},
       {rewrite_rule => '^/job$ https://docs.google.com/document/d/1oS3ma415LbtuyeEuuoucWKYLcWOJaWmzhv2nIs5f718/edit [R]'},
       {rewrite_rule => '^/join$ https://www.ocf.berkeley.edu/account/register/ [R]'},
