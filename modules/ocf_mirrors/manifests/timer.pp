b5b8938c35856cabe0bd262e488d607c1043c95a
Jason Perrin <jvperrin@ocf.berkeley.edu>
2020-01-27 20:25:01 -0800

    Add a noop mirrors sync mode and key for dev-fallingrocks

--- modules/ocf_mirrors/manifests/timer.pp
+++ modules/ocf_mirrors/manifests/timer.pp
@@ -12,6 +12,15 @@ define ocf_mirrors::timer(
   # Any meaningful second precision requires AccuracySec to be set
   $on_calendar = "${year}-${month}-${day} ${hour}:${minute}:00"
 
+  if $::host_env == 'dev' {
+    # Don't actually run the syncing commands when in dev since they will fill
+    # up the entire disk, so just print out what script would have been run to
+    # syslog
+    $exec = "logger -t ${title}_systemd_service: Would have run '${exec_start}' if in prod"
+  } else {
+    $exec = $exec_start
+  }
+
   ocf::systemd::timer { $title:
     service_content => template('ocf_mirrors/systemd/sync.service.erb'),
     timer_content   => template('ocf_mirrors/systemd/sync.timer.erb'),
