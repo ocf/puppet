c776b814c4fd8e0b92855a7147ea506d03236a9f
Christopher Cooper <cooperc@ocf.berkeley.edu>
2020-01-27 19:02:05 -0800

    update joinstaff link (#840)

--- modules/ocf_www/manifests/site/shorturl.pp
+++ modules/ocf_www/manifests/site/shorturl.pp
@@ -68,7 +68,7 @@ class ocf_www::site::shorturl {
       {rewrite_rule => '^/irc$ https://www.ocf.berkeley.edu/docs/contact/irc/ [R]'},
       {rewrite_rule => '^/job$ https://docs.google.com/document/d/1oS3ma415LbtuyeEuuoucWKYLcWOJaWmzhv2nIs5f718/edit [R]'},
       {rewrite_rule => '^/join$ https://www.ocf.berkeley.edu/account/register/ [R]'},
-      {rewrite_rule => '^/joinstaff$ https://docs.google.com/presentation/d/1S8-95Lz6o8AFog7ok6DiYgGn9KYr2wy025Pd15WNsTI/edit [R]'},
+      {rewrite_rule => '^/joinstaff$ https://docs.google.com/presentation/d/1anXV0Nmq8BpUNPt6THkAcPD4sgHRsOrybZ0KlFxKuag/edit [R]'},
       {rewrite_rule => '^/k$ https://kanboard.ocf.berkeley.edu/ [R]'},
       {rewrite_rule => '^/k/a$ https://kanboard.ocf.berkeley.edu/?controller=BoardViewController&action=show&project_id=2 [R]'},
       {rewrite_rule => '^/k/([0-9]+)$ https://kanboard.ocf.berkeley.edu/?controller=TaskViewController&action=show&task_id=$1 [R]'},
