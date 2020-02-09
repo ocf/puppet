d3d410587381fb1d63df6a24a39ac73053931c90
jaw <jaw@ocf.berkeley.edu>
2020-02-08 21:01:26 -0800

    Remove firewall rule

--- modules/ocf_prometheus/manifests/pushgateway.pp
+++ modules/ocf_prometheus/manifests/pushgateway.pp
@@ -1,15 +1,5 @@
 class ocf_prometheus::pushgateway {
 
-  ocf::firewall::firewall46 {
-    '995 allow pushgateway to accept metrics':
-      opts => {
-        chain  => 'PUPPET-INPUT',
-        proto  => 'tcp',
-        dport  => 9091,
-        action => 'accept',
-      };
-  }
-
   class { '::prometheus::pushgateway':
     version       => '1.0.0',
     extra_options => "--web.route-prefix=/ --web.external-url=\"https://prometheus.ocf.berkeley.edu/pushgateway\"",
