64fa9e1139143b27aa1cdc0b1814f8be8df8ee8a
kmo <encadyma@gmail.com>
2020-02-24 22:13:52 -0800

    Fix Puppet indentation for matrix/Riot

--- modules/ocf/manifests/packages/riot/apt.pp
+++ modules/ocf/manifests/packages/riot/apt.pp
@@ -2,7 +2,7 @@
 class ocf::packages::riot::apt {
   apt::key { 'riot':
     ensure => refreshed,
-    id     =>  '12D4CD600C2240A9F4A82071D7B0B66941D01538',
+    id     => '12D4CD600C2240A9F4A82071D7B0B66941D01538',
     source => 'https://packages.riot.im/debian/riot-im-archive-keyring.asc',
   }
 
