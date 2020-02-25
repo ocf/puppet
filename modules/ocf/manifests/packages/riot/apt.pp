3f6c0cbbe62e2618478fae6b16153f4015676385
Kevin Mo <kmo@ocf.berkeley.edu>
2020-02-24 22:18:45 -0800

    Add Riot client (Matrix) to OCF desktops (#879)
    * Add Riot client (Matrix) to OCF desktops
    * Fix Puppet indentation for matrix/Riot

---
+++ modules/ocf/manifests/packages/riot/apt.pp
@@ -0,0 +1,16 @@
+# Include Riot apt repo.
+class ocf::packages::riot::apt {
+  apt::key { 'riot':
+    ensure => refreshed,
+    id     => '12D4CD600C2240A9F4A82071D7B0B66941D01538',
+    source => 'https://packages.riot.im/debian/riot-im-archive-keyring.asc',
+  }
+
+  apt::source { 'riot':
+    architecture => 'amd64',
+    location     => 'https://packages.riot.im/debian',
+    release      => $::lsbdistcodename,
+    repos        => 'main',
+    require      => Apt::Key['riot'],
+  }
+}
