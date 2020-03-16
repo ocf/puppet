c837f81cc7eb36f5065115e679c9ba2d6af56e7d
Ja (Thanakul) Wattanawong <jaw@ocf.berkeley.edu>
2020-03-16 12:43:23 -0700

    Add passkey and user to F@H (#917)

--- modules/ocf/manifests/packages/fahclient.pp
+++ modules/ocf/manifests/packages/fahclient.pp
@@ -2,13 +2,17 @@ class ocf::packages::fahclient {
   # Folding @ Home
   package {'fahclient':;}
 
+  $fah_user = 'Open_Computing_Facility'
+  $fah_passkey = lookup('ocf_desktop::fah_passkey')
+
   file {
     '/etc/fahclient/config.xml':
-      source  => 'puppet:///modules/ocf/packages/fahclient/config.xml',
-      require => Package['fahclient'],
-      owner   => 'fahclient',
-      mode    => '0664',
-      notify  => Service['FAHClient'];
+      content   => template('ocf/packages/fahclient/config.xml.erb'),
+      show_diff => false,
+      require   => Package['fahclient'],
+      owner     => 'fahclient',
+      mode      => '0660',
+      notify    => Service['FAHClient'];
   }
 
   service {
