e5bae16ae659dacd00e29a9f0fed35026afc4eb3
Bernard Zhao <bernardzhao@ocf.berkeley.edu>
2020-04-03 20:34:42 -0700

    Added logging to kubernetes (#928)
    * Added logging to kubernetes
    
    * Fixed apply order issue?
    
    * Fix file
    
    * Requested changes

--- modules/ocf_kubernetes/manifests/master.pp
+++ modules/ocf_kubernetes/manifests/master.pp
@@ -14,6 +14,7 @@
 class ocf_kubernetes::master {
   include ocf::packages::kubernetes
   include ocf_kubernetes::master::loadbalancer
+  include ocf_kubernetes::logging
 
   class { 'ocf::packages::docker':
     kubernetes => true;
