43cb65667c252f04ceed03e125685a631ac58a3d
Mandy Zhao <mandi.zhao@berkeley.edu>
2020-04-15 17:43:41 -0700

    adding two files

---
+++ modules/ocf_mirrors/manifests/projects/alpine.pp
@@ -0,0 +1,24 @@
+class ocf_mirrors::projects::alpine {
+  file {
+    '/opt/mirrors/project/alpine':
+      ensure  => directory,
+      source  => 'puppet:///modules/ocf_mirrors/project/alpine',
+      owner   => mirrors,
+      group   => mirrors,
+      mode    => '0755',
+      recurse => true;
+  }
+
+  ocf_mirrors::monitoring { 'alpine':
+    type          => 'unix_timestamp',
+    upstream_host => 'mirrors.alpinelinux.org',
+    ts_path       => 'TIME',
+  }
+
+  ocf_mirrors::timer {
+    'alpine':
+      exec_start => 'opt/mirrors/project/alpine/sync-archive',
+      hour       => '0/3',
+      minute     => '22';
+  }
+}
