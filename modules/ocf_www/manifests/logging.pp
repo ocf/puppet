6cd346a05ba3386cd623d4bc83e68d354198013c
jaw <jaw@ocf.berkeley.edu>
2020-05-17 15:31:12 -0700

    modified rules slightly

--- modules/ocf_www/manifests/logging.pp
+++ modules/ocf_www/manifests/logging.pp
@@ -14,6 +14,7 @@ class ocf_www::logging {
     default:
         chain      => 'PUPPET-OUTPUT',
         outiface   => '! lo',
+        state      => ['NEW'],
         jump       => 'LOG',
         log_prefix => '[iptables-outbound] ',
         log_level  => 7,
