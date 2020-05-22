13ad4ca6d8ab8e06a6fbea8d13be256163950305
Ja (Thanakul) Wattanawong <jaw@ocf.berkeley.edu>
2020-05-22 01:18:39 -0700

    Separate monitor and sync service files, adjust retry for monitor (#973)
    The monitor service file should retry 5 times before giving up,
    after which the service will go into reset-failed state.

--- modules/ocf_mirrors/manifests/timer.pp
+++ modules/ocf_mirrors/manifests/timer.pp
@@ -12,9 +12,19 @@ define ocf_mirrors::timer(
   # Any meaningful second precision requires AccuracySec to be set
   $on_calendar = "${year}-${month}-${day} ${hour}:${minute}:00"
 
+  if !($type in ['sync', 'monitor']) {
+    fail('Type parameter of ocf_mirrors:timer must be either sync or monitor')
+  }
+
+  if $type == 'sync' {
+    $service_content = template('ocf_mirrors/systemd/sync.service.erb')
+  } else {
+    $service_content = template('ocf_mirrors/systemd/monitor.service.erb')
+  }
+
   ocf::systemd::timer { $title:
-    service_content => template('ocf_mirrors/systemd/sync.service.erb'),
-    timer_content   => template('ocf_mirrors/systemd/sync.timer.erb'),
+    service_content => $service_content,
+    timer_content   => template('ocf_mirrors/systemd/all.timer.erb'),
   }
 
   # Ensure there is no duplicate cron job
