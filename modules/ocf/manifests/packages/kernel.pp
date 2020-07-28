83c9ee0f684d29345791251ea59f76248ea138ab
Dara Adib <daradib@ocf.berkeley.edu>
2020-07-28 15:53:50 -0400

    Replace lockdown with hardening-runtime

--- modules/ocf/manifests/packages/kernel.pp
+++ modules/ocf/manifests/packages/kernel.pp
@@ -1,10 +1,26 @@
 class ocf::packages::kernel {
   if $::lsbdistcodename != 'stretch' {
-    # Disable some kernel features: module loading after boot, kexec,
-    # Berkeley Packet Filter (BPF). Not to be confused with the lockdown
-    # security module introduced with Linux 5.4, which imposes similar
-    # restrictions.
-    package { 'lockdown': }
+    # Harden kernel using kernel command line options and sysctl settings
+    # recommended by the Kernel Self Protection Project:
+    # https://kernsec.org/wiki/index.php/Kernel_Self_Protection_Project/Recommended_Settings
+    # Tails makes similar changes:
+    # https://tails.boum.org/contribute/design/kernel_hardening/
+    # kernel command line changes - potential performance impact:
+    #  * always enable kernel address space layout randomization (KASLR)
+    #  * always enable kernel page-table isolation (PTI, formerly KAISER)
+    #  * wipe slab and page allocations and enable sanity checks
+    #  * disable simultaneous multithreading (SMT) aka hyperthreading (HT)
+    # sysctl changes:
+    #  * disable kexec
+    #  * restrict ptrace access to parent processes
+    #  * disable user namespaces
+    #    currently breaks systemd services specifying PrivateUsers=yes,
+    #    such as upower on bullseye, see
+    #    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=959884
+    #  * disable unprivileged Berkeley Packet Filter (BPF) access
+    # For bullseye, also consider enabling the lockdown security module
+    # introduced with Linux 5.4.
+    package { 'hardening-runtime': }
 
     if $::is_virtual {
       # Install cloud kernel image which removes some hardware support.
