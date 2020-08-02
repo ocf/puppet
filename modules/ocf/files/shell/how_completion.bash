b3b0b226b64d7dea8c38835a1fb9c645c903f091
Jason Perrin <jvperrin@ocf.berkeley.edu>
2020-08-02 04:04:08 -0700

    Upgrade pre-commit hooks and add shellcheck

--- modules/ocf/files/shell/how_completion.bash
+++ modules/ocf/files/shell/how_completion.bash
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 _how () {
     COMPREPLY=()
 
--- modules/ocf_irc/files/reload-ssl.sh
+++ modules/ocf_irc/files/reload-ssl.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 # Send a signal to the server to make it reload certs
 
-pid=`pgrep inspircd`
+pid=$(pgrep inspircd)
 echo "Reloading certs for inspircd pid: $pid"
-kill -SIGUSR1 $pid
+kill -SIGUSR1 "$pid"
