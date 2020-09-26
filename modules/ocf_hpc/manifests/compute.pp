03d4b89b97bb619a7479e50325e5222ab4f38174
Long(Tony) Lian <1040424979@qq.com>
2020-09-26 21:39:49 +0800

    Update compute.pp

--- modules/ocf_hpc/manifests/compute.pp
+++ modules/ocf_hpc/manifests/compute.pp
@@ -7,7 +7,7 @@ class ocf_hpc::compute {
   include ocf_hpc::singularity
 
   # install proprietary nvidia drivers and CUDA.
-  ocf::repackage { ['nvidia-driver', 'nvidia-settings', 'nvidia-cuda-toolkit', 'nvidia-persistenced']:
+  ocf::repackage { ['nvidia-driver', 'nvidia-settings', 'nvidia-cuda-toolkit', 'nvidia-persistenced', 'libosmesa6-dev', 'xvfb']:
       backport_on => buster;
   } -> file { '/etc/modules-load.d/nvidia-uvm.conf':
     # The nvidia-uvm kernel module, which is needed for CUDA apps, can't be loaded as needed from within Singularity.
