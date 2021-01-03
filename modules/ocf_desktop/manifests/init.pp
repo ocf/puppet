935eec97b9b0273e02d4de35016093ca6e685a18
Jason Perrin <jvperrin@ocf.berkeley.edu>
2021-01-03 09:52:32 -0700

    Fix puppet on desktops, riot-web -> element-desktop (#1060)

--- modules/ocf_desktop/manifests/init.pp
+++ modules/ocf_desktop/manifests/init.pp
@@ -3,9 +3,9 @@ class ocf_desktop {
   include ocf::packages::brave
   include ocf::packages::chrome
   include ocf::packages::cups
+  include ocf::packages::element
   include ocf::packages::firefox
   include ocf::packages::pulse
-  include ocf::packages::riot
   include ocf::packages::vscode
   include ocf_desktop::crondeny
   include ocf_desktop::defaults
