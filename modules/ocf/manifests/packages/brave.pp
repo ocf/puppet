8ae8806537f8cf8e54d75f0d5bd46277cf2567b2
Nikhil Jha <nikhiljha@users.noreply.github.com>
2021-08-18 14:58:22 -0700

    remove from existing desktops

---
+++ modules/ocf/manifests/packages/brave.pp
@@ -0,0 +1,11 @@
+class ocf::packages::brave {
+  # Remove the old version of brave
+  package { 'brave':
+    ensure => purged
+  }
+  
+  # Remove the new version of brave
+  package { 'brave-browser':
+    ensure => purged
+  }
+}
