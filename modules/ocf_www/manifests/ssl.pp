dfb7581b17eff9f5b2c8cca0c1e901cd7f137e83
Jonathan Zhang <jonathan@bbs4.us>
2021-10-09 16:25:57 -0700

    Update ssl.pp
    add http_protocols variable

--- modules/ocf_www/manifests/ssl.pp
+++ modules/ocf_www/manifests/ssl.pp
@@ -1,9 +1,19 @@
 # Common SSL settings for all sites.
 class ocf_www::ssl {
-  apache::custom_config { 'ocf-ssl':
-    content => "
-      SSLProtocol ${::ssl_protocols}
-      SSLCipherSuite ${::ssl_ciphersuite}
-    ",
+  if $::http_protocols {
+    apache::custom_config { 'ocf-ssl':
+      content => "
+        SSLProtocol ${::ssl_protocols}
+        SSLCipherSuite ${::ssl_ciphersuite}
+        Protocols ${::http_protocols}
+      ",
+    }
+  } else {
+    apache::custom_config { 'ocf-ssl':
+      content => "
+        SSLProtocol ${::ssl_protocols}
+        SSLCipherSuite ${::ssl_ciphersuite}
+      ",
+    }
   }
 }
