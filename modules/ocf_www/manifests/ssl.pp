98753356aa340a475e0a789f18189401695f13f1
Jonathan Zhang <jonathan@bbs4.us>
2021-10-09 17:00:12 -0700

    Update ssl.pp

--- modules/ocf_www/manifests/ssl.pp
+++ modules/ocf_www/manifests/ssl.pp
@@ -1,19 +1,9 @@
 # Common SSL settings for all sites.
 class ocf_www::ssl {
-  if defined('$::http_protocols') {
-    apache::custom_config { 'ocf-ssl':
-      content => "
-        SSLProtocol ${::ssl_protocols}
-        SSLCipherSuite ${::ssl_ciphersuite}
-        Protocols ${::http_protocols}
-      ",
-    }
-  } else {
-    apache::custom_config { 'ocf-ssl':
-      content => "
-        SSLProtocol ${::ssl_protocols}
-        SSLCipherSuite ${::ssl_ciphersuite}
-      ",
-    }
+  apache::custom_config { 'ocf-ssl':
+    content => "
+      SSLProtocol ${::ssl_protocols}
+      SSLCipherSuite ${::ssl_ciphersuite}
+    ",
   }
 }
