59ae9243b623991a6508cb6a094c206fa684db5e
Jonathan Zhang <jonathan@bbs4.us>
2021-10-14 18:25:59 -0700

    Revert "test q-f-m"
    This reverts commit a6d84a0a790629b6a6acc1787f1b17d73fa0f3ae.

--- modules/ocf_mirrors/manifests/projects/epel.pp
+++
@@ -1,8 +0,0 @@
-class ocf_mirrors::projects::fedora {
-  ocf_mirrors::q-f-m {
-    'epel':
-      remote_host  => 'rsync://dl.fedoraproject.org',
-      cron_hour    => '0/6',
-      cron_minute  => '10';
-  }
-}
