6fc5fe665ca444a64d12f3e88a2592e4caa3a5b1
Jonathan Zhang <jonathan@bbs4.us>
2021-10-14 23:59:57 -0700

    Update debian.pp

--- modules/ocf_mirrors/manifests/projects/debian.pp
+++ modules/ocf_mirrors/manifests/projects/debian.pp
@@ -2,7 +2,7 @@ class ocf_mirrors::projects::debian {
   ocf_mirrors::ftpsync {
     'debian':
       rsync_host  => 'mirrors.wikimedia.org',
-      cron_hour   => '0/6',
+      cron_hour   => '2/8',
       cron_minute => '10';
 
     'debian-security':
