7b7cb4737d56c83fc4ed80ed72b65d4a059ee90e
Jonathan Zhang <jonathan@bbs4.us>
2021-11-05 18:29:15 -0700

    Update init.pp
    nuke apache, add nginx config entries

--- modules/ocf_mirrors/manifests/init.pp
+++ modules/ocf_mirrors/manifests/init.pp
@@ -5,7 +5,7 @@ class ocf_mirrors {
   include ocf_mirrors::ftp
   include ocf_mirrors::rsync
   include ocf_mirrors::firewall_input
-
+  include nginx
   # projects
   include ocf_mirrors::projects::apache
   include ocf_mirrors::projects::alpine
@@ -35,20 +35,6 @@ class ocf_mirrors {
   include ocf_mirrors::projects::tails
   include ocf_mirrors::projects::trisquel
   include ocf_mirrors::projects::ubuntu
-  package {
-      [
-        'prometheus-apache-exporter',
-      ]:;
-    }
-  # Prometheus user needed for the prometheus-apache-exporter daemon,
-  # which runs as user "prometheus"
-  user {
-    'prometheus':
-      comment  => 'prometheus user for running exporters',
-
-      # Set to have no password, only allow key-based login
-      password => '*',
-  }
   user { 'mirrors':
     comment  => 'OCF Mirroring',
     home     => '/opt/mirrors',
@@ -59,7 +45,7 @@ class ocf_mirrors {
   }
 
   $ocfstats_password = lookup('ocfstats::mysql::password')
-
+ 
   file {
     ['/opt/mirrors', '/opt/mirrors/ftp', '/opt/mirrors/project', '/opt/mirrors/bin']:
       ensure  => directory,
@@ -81,126 +67,40 @@ class ocf_mirrors {
     '/var/log/rsync':
       ensure => directory;
   }
-
-  class {
-    '::apache':
-      keepalive   => 'on',
-      log_formats => {
-        # A custom log format that counts bytes transferred by accesses (mod_logio)
-        io_count => '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %I %O',
-      },
-      # "false" lets us define the class below with custom args
-      mpm_module  => false;
-
-    '::apache::mod::worker':
-      startservers    => 8,
-      # maxclients should be set to a max of serverlimit * threadsperchild
-      maxclients      => 5000,
-      threadsperchild => 50,
-      serverlimit     => 100;
+  nginx::resource::server { "mirrors.ocf.berkeley.edu":
+    ensure                => present,
+    listen_port           => 443,
+    listen_options        => 'default_server',
+    www_root              => '/opt/mirrors/ftp',
+    autoindex             => on,
+    ssl                   => true,
+    http2                 => on,
+    ssl_cert              => "/etc/ssl/private/${::fqdn}.bundle",
+    ssl_key               => "/etc/ssl/private/${::fqdn}.key",
+    ipv6_enable           => true,
+    ipv6_listen_port      => 443,
   }
-  include apache::mod::headers
-  include apache::mod::status
-  include apache::mod::rewrite
-
-  # Restart apache if any cert changes occur
-  Class['ocf::ssl::default'] ~> Class['Apache::Service']
-
-  # The Apache project requires very particular configuration:
-  # https://www.apache.org/info/how-to-mirror.html#Configuration
-  $apache_project_directory_options = {
-    path          => '/opt/mirrors/ftp/apache',
-    options       => ['Indexes', 'SymLinksIfOwnerMatch', 'FollowSymLinks'],
-    index_options => [
-      'FancyIndexing',
-      'NameWidth=*',
-      'FoldersFirst',
-      'ScanHTMLTitles',
-      'DescriptionWidth=*'
-    ],
-    allow_override => ['FileInfo', 'Indexes'],
-    error_documents => [
-      {
-        error_code => '404',
-        document => 'default',
-      }
-    ],
-    custom_fragment => "
-      HeaderName HEADER.html
-      ReadmeName README.html
-    ",
+  nginx::resource::server { "mirrors.ocf.berkeley.edu":
+    ensure                => present,
+    listen_port           => 80,
+    listen_options        => 'default_server',
+    www_root              => '/opt/mirrors/ftp',
+    autoindex             => on,
+    http2                 => on,
+    ipv6_enable           => true,
+    ipv6_listen_port      => 80,
+  }
+  nginx::resource::server { "mirrors.berkeley.edu":
+    ensure              => present,
+    listen_port         => 80,
+    ipv6_enable         => true,
+    ipv6_listen_port    => 80,
+    www_root            => '/var/www',
+    location_cfg_append => {
+      'rewrite' => '^ http://mirrors.ocf.berkeley.edu permanent'
+    }
   }
 
-  apache::vhost { 'mirrors.ocf.berkeley.edu':
-    serveraliases     => ['mirrors', 'dl.amnesia.boum.org', '*.dl.amnesia.boum.org'],
-    port              => 80,
-    default_vhost     => true,
-    docroot           => '/opt/mirrors/ftp',
-
-    directories       => [
-      {
-        path            => '/opt/mirrors/ftp',
-        options         => ['+Indexes', '+SymlinksIfOwnerMatch'],
-        custom_fragment => '
-          RewriteEngine On
-          RewriteCond "%{HTTP_USER_AGENT}" "MSIE 7\.0|Chrome\/49\.0|Chrome\/67\.0|Edg\/85\.0\.537\.0"
-          RewriteRule ^ - [F]
-        ',
-        index_options   => ['NameWidth=*', '+SuppressDescription']
-      },
-      $apache_project_directory_options,
-    ],
-
-    access_log_format => 'io_count',
-    custom_fragment   => "
-      Protocols h2c http/1.1
-      HeaderName README.html
-      ReadmeName FOOTER.html
-    ",
-  }
-
-  apache::vhost { 'mirrors.berkeley.edu':
-    port            => 80,
-
-    # we have to specify docroot even though we always redirect
-    docroot         => '/var/www',
-    custom_fragment => 'Protocols h2c http/1.1',
-    redirect_source => '/',
-    redirect_dest   => 'http://mirrors.ocf.berkeley.edu/',
-    redirect_status => '301',
-  }
-
-  apache::vhost { 'mirrors.ocf.berkeley.edu-ssl':
-    servername        => 'mirrors.ocf.berkeley.edu',
-    port              => 443,
-    docroot           => '/opt/mirrors/ftp',
-
-    directories       => [
-      {
-        path            => '/opt/mirrors/ftp',
-        options         => ['+Indexes', '+SymlinksIfOwnerMatch'],
-        custom_fragment => '
-          RewriteEngine On
-          RewriteCond "%{HTTP_USER_AGENT}" "MSIE 7\.0|Chrome\/49\.0|Chrome\/67\.0|Edg\/85\.0\.537\.0"
-          RewriteRule ^ - [F]
-        ',
-        index_options   => ['NameWidth=*', '+SuppressDescription']
-      },
-      $apache_project_directory_options,
-    ],
-
-    access_log_format => 'io_count',
-    custom_fragment   => "
-      Protocols h2 http/1.1
-      HeaderName README.html
-      ReadmeName FOOTER.html
-    ",
-
-    ssl               => true,
-    ssl_key           => "/etc/ssl/private/${::fqdn}.key",
-    ssl_cert          => "/etc/ssl/private/${::fqdn}.crt",
-    ssl_chain         => "/etc/ssl/private/${::fqdn}.intermediate",
-  }
 
   file { '/opt/mirrors/bin/report-sizes':
     source => 'puppet:///modules/ocf_mirrors/report-sizes',
