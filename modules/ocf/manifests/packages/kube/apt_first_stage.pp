7cca38fb486b8a385ea8b66ad6b526a31e54d6fd
Nikhil Jha <nikhiljha@users.noreply.github.com>
2021-11-12 21:43:33 -0800

    switch to cri-o for kubernetes (#1199)

--- modules/ocf/manifests/packages/kube/apt_first_stage.pp
+++ modules/ocf/manifests/packages/kube/apt_first_stage.pp
@@ -17,4 +17,30 @@ class ocf::packages::kube::apt_first_stage {
       # pull.
       release  => 'kubernetes-xenial',
   }
+
+  apt::key { 'crio repo key':
+    id     => '2472D6D0D2F66AF87ABA8DA34D64390375060AA4',
+    # the key is the same for both
+    source => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_${::operatingsystemmajrelease}/Release.key";
+  }
+
+  $is_dev = $::hostname in lookup('kube_dev::controller_nodes')
+  $kube_prefix = if $is_dev { 'kube_dev' } else { 'kube' }
+  $kube_version = lookup("${kube_prefix}::kubernetes_version")
+  $split_version = split($kube_version, '.')
+  $crio_version = "${split_version[0]}.${split_version[1]}"
+
+  # for packages: cri-o cri-o-runc
+  apt::source { 'crio':
+      architecture => 'amd64',
+      location     => "http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/${crio_version}/Debian_${::operatingsystemmajrelease}/",
+      repos        => '/',
+      require      => Apt::Key['crio repo key'],
+  }
+  apt::source { 'libcontainers':
+      architecture => 'amd64',
+      location     => "https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/Debian_${::operatingsystemmajrelease}/",
+      repos        => '/',
+      require      => Apt::Key['crio repo key'],
+  }
 }
