c6c2a5aa7124abdc6b4e3c243cab7408a490e523
Ethan Wu <ethanwu10@gmail.com>
2021-12-15 16:16:18 -0800

    wip: ssh and processors

--- modules/ocf/manifests/packages/microcode.pp
+++ modules/ocf/manifests/packages/microcode.pp
@@ -1,11 +1,11 @@
 class ocf::packages::microcode {
-  if $::processor0 {
-    if $::processor0 =~ /\bIntel\b/ {
+  if $::processors['models'][0] {
+    if $::processors['models'][0] =~ /\bIntel\b/ {
       package { 'intel-microcode':; }
-    } elsif $::processor0 =~ /\bAMD\b/ {
+    } elsif $::processors['models'][0] =~ /\bAMD\b/ {
       package { 'amd64-microcode':; }
     } else {
-      fail("Don't know how to interpret processor0: ${::processor0}")
+      fail("Don't know how to interpret processor model: ${::processors['models'][0]}")
     }
   }
 }
