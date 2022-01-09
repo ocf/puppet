02294151dcf34bb4fd9dc0faa77a643484d0df5a
Jonathan Zhang <jonathan@bbs4.us>
2022-01-08 20:16:15 -0800

    Update pulse.pp

--- modules/ocf_tv/manifests/pulse.pp
+++ modules/ocf_tv/manifests/pulse.pp
@@ -21,15 +21,7 @@ class ocf_tv::pulse {
     notify  => Service['pulseaudio'],
   }
 
-  # the timer-based scheduler performs worse for some reason for remote audio
-  # on our network, so we disable it in favor of the interrupt-based one.
-  # "load-module module-udev-detect" is generally line 47 in default.pa
-  exec { 'change-pulse-scheduler':
-    command => "sed -i 's/module-udev-detect$/module-udev-detect tsched=0/' /etc/pulse/default.pa",
-    unless  => "grep -q 'tsched=0' /etc/pulse/default.pa",
-    require => Package['pulseaudio'],
-    notify  => Service['pulseaudio'],
-  }
+
 
   # configure the default server manually so we don't have to run
   # pax11publish -e -S 127.0.0.1 every time Xorg restarts
