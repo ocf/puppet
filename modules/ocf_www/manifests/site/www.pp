4b9cde9124934e2e6717421c802fec9892a6850a
Jonathan Zhang <jonathan@bbs4.us>
2022-02-12 12:20:35 -0800

    fix: add custom_fragment

--- modules/ocf_www/manifests/site/www.pp
+++ modules/ocf_www/manifests/site/www.pp
@@ -85,9 +85,18 @@ class ocf_www::site::www {
 
     directories         => [
       {
-        path     => '/.well-known/matrix/server',
-        provider => 'location',
-        header   => 'set Access-Control-Allow-Origin \"*\"',
+        path            => '/.well-known/matrix/server',
+        provider        => 'location',
+        custom_fragment => '
+            Header set Access-Control-Allow-Origin "*"
+        ',
+      },
+      {
+        path            => '/.well-known/matrix/client',
+        provider        => 'location',
+        custom_fragment => '
+            Header set Access-Control-Allow-Origin "*"
+        ',
       },
       {
         path           => '/services/http/users',
