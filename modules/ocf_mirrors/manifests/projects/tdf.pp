d7de502244a39dd59dd130b343879a6ed16c76da
Jonathan Zhang <jonathan@bbs4.us>
2022-02-21 12:35:15 -0800

    feat: add tdf (#1260)

---
+++ modules/ocf_mirrors/manifests/projects/tdf.pp
@@ -0,0 +1,25 @@
+class ocf_mirrors::projects::tdf {
+  file {
+    '/opt/mirrors/project/tdf':
+      ensure  => directory,
+      source  => 'puppet:///modules/ocf_mirrors/project/tdf',
+      owner   => mirrors,
+      group   => mirrors,
+      mode    => '0755',
+      recurse => true;
+  }
+
+  ocf_mirrors::monitoring { 'tdf':
+    type          => 'unix_timestamp',
+    upstream_host => 'download.documentfoundation.org',
+    ts_path       => 'TIMESTAMP',
+  }
+
+  ocf_mirrors::timer {
+    'tdf':
+      exec_start => '/opt/mirrors/project/tdf/sync-archive',
+      hour       => '0/12',
+      minute     => '20',
+      require    => File['/opt/mirrors/project/tdf'];
+  }
+}
