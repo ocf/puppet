e0a2bc9cd2988601ce7e41835e07b64b6c3b7999
Ethan Wu <ethanwu10@gmail.com>
2022-03-19 14:27:14 -0700

    fix: revamp code for detecting active session user
    KDE creates multiple wtmp entries per logged-in user, which confuses the
    old `who`-based parsing for getting the current active user session.
    
    Rewrite it to use loginctl sessions, and search for the currently active
    session on seat0 (the physical display), ignoring display managers. This
    should make the logic robust to anything a desktop environment can throw
    at us, so long as it uses systemd sessions properly.

--- modules/ocf_desktop/files/stats/update.sh
+++ modules/ocf_desktop/files/stats/update.sh
@@ -8,7 +8,21 @@
 # "cleanup" triggers closing of the session
 # other files to look at: session-cleanup and session-setup in /etc/lightdm
 
-CUR_USER=$(who | awk '$NF == "(:0)" { print $1 }')
+# This abomination of bash scripting:
+# - lists all the sessions
+# - filters by seat == "seat0"
+# - filters out any session whose session ID is not a number
+#   (this is because display managers like lightdm create sessions like
+#   c### and we don't want to report the DM as being logged in)
+# - outputs the session IDs, as newline-delimited raw strings
+# - runs a shell script on each session id which:
+#   - gets session data
+#   - checks if the session is active
+#   - if so, outputs the user associated with the session
+# - gets the last output user (just in case something unexpected happened
+#   and we end up with more than one)
+# shellcheck disable=SC2016
+CUR_USER=$(loginctl list-sessions -o json | jq 'map(select(.seat == "seat0" and (try (.session | tonumber) | true)))|.[]|.session' -r | xargs -d '\n' -n 1 bash -c 'data=$(loginctl show-session "$1"); grep "^Active=yes$" >/dev/null <<<"$data" && grep -Po "(?<=^Id=).*$" <<<"$data"' -- | tail -n1)
 
 # if a user is logged in, the state is active, else, take it from argv[1]
 # (used when calling update-delay.sh from the lightdm session-cleanup script)
