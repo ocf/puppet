6afb477a014d913ca2aba8ded4a88c70193c0470
Jonathan Zhang <jonathan@bbs4.us>
2022-04-30 21:31:25 -0700

    fix: update

--- modules/ocf_mirrors/manifests/projects/raspbian.pp
+++ modules/ocf_mirrors/manifests/projects/raspbian.pp
@@ -1,13 +1,13 @@
 class ocf_mirrors::projects::raspbian {
   ocf_mirrors::ftpsync { 'raspbian':
-    rsync_host  => 'archive.raspbian.org',
-    rsync_path  => 'archive',
+    rsync_host  => 'ftp.halifax.rwth-aachen.de',
+    rsync_path  => 'raspbian',
     cron_minute => '45',
   }
 
   ocf_mirrors::monitoring { 'raspbian':
     type          => 'debian',
-    dist_to_check => 'stretch',
+    dist_to_check => 'bullseye',
     local_path    => '/raspbian/raspbian',
     upstream_host => 'archive.raspbian.org';
   }
