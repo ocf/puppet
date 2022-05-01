4864352450d08d4bd078d6e14f8e52c680a18295
Jonathan Zhang <jonathan@bbs4.us>
2022-04-30 22:46:03 -0700

    feat: http header health check

--- modules/ocf_mirrors/manifests/projects/fedora.pp
+++ modules/ocf_mirrors/manifests/projects/fedora.pp
@@ -5,10 +5,27 @@ class ocf_mirrors::projects::fedora {
       cron_hour   => '0/6',
       cron_minute => '10';
   }
+
+  ocf_mirrors::monitoring { 'epel':
+    type          => 'http_last_modified',
+    upstream_host => 'dl.fedoraproject.org',
+    ts_path       => 'fullfiletimelist-epel',
+    upstream_path => '/pub/epel',
+    local_path    => '/fedora/epel';
+  }
+
   ocf_mirrors::qfm {
     'enchilada':
       remote_host => 'rsync://mirrors.kernel.org',
       cron_hour   => '2/6',
       cron_minute => '20';
+  }
+
+  ocf_mirrors::monitoring { 'enchilada':
+    type          => 'http_last_modified',
+    upstream_host => 'dl.fedoraproject.org',
+    ts_path       => 'fullfiletimelist-fedora',
+    upstream_path => '/pub/fedora',
+    local_path    => '/fedora/fedora';
   }
 }
