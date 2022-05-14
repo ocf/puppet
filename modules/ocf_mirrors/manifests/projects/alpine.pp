842953d96001f41cc716f06c86cd331826e5a079
Jonathan Zhang <jonathan@bbs4.us>
2022-05-13 23:59:41 -0700

    fix: fix alpine, osdn

--- modules/ocf_mirrors/manifests/projects/alpine.pp
+++ modules/ocf_mirrors/manifests/projects/alpine.pp
@@ -1,19 +1,25 @@
-class ocf_mirrors::projects::osdn {
+class ocf_mirrors::projects::alpine {
   file {
-    '/opt/mirrors/project/osdn':
+    '/opt/mirrors/project/alpine':
       ensure  => directory,
-      source  => 'puppet:///modules/ocf_mirrors/project/osdn',
+      source  => 'puppet:///modules/ocf_mirrors/project/alpine',
       owner   => mirrors,
       group   => mirrors,
       mode    => '0755',
       recurse => true;
   }
 
+  ocf_mirrors::monitoring { 'alpine':
+    type          => 'unix_timestamp',
+    upstream_host => 'dl-cdn.alpinelinux.org',
+    ts_path       => 'last-updated',
+  }
+
   ocf_mirrors::timer {
-    'osdn':
-      exec_start => '/opt/mirrors/project/osdn/sync-archive',
+    'alpine':
+      exec_start => '/opt/mirrors/project/alpine/sync-archive',
       hour       => '*',
-      minute     => '32',
-      require    => File['/opt/mirrors/project/osdn'];
+      minute     => '42',
+      require    => File['/opt/mirrors/project/alpine'];
   }
 }
