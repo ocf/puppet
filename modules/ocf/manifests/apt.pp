173a781330cf870a3a9949ed5299e540a0b57562
Jonathan Zhang <jonathan@bbs4.us>
2022-08-06 16:35:22 -0700

    style: lint

--- modules/ocf/manifests/apt.pp
+++ modules/ocf/manifests/apt.pp
@@ -12,183 +12,182 @@ class ocf::apt($stage = 'first') {
   $repos = 'main contrib non-free'
 
   if $::lsbdistid == 'Debian' {
-      if $::operatingsystemmajrelease == '9' {
-        apt::key { 'freexian':
-            id     => 'AB597C4F6F3380BD4B2BEBC2A07310D369055D5A',
-            source => 'https://deb.freexian.com/extended-lts/archive-key.gpg';
-            }
+    if $::operatingsystemmajrelease == '9' {
+      apt::key { 'freexian':
+          id     => 'AB597C4F6F3380BD4B2BEBC2A07310D369055D5A',
+          source => 'https://deb.freexian.com/extended-lts/archive-key.gpg';
+          }
 
-        apt::source {
-        'debian':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => $::lsbdistcodename,
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      apt::source {
+      'debian':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => $::lsbdistcodename,
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'debian-updates':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => "${::lsbdistcodename}-updates",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      'debian-updates':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => "${::lsbdistcodename}-updates",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'debian-security':
-            location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
-            release  => "${::lsbdistcodename}/updates",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      'debian-security':
+          location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
+          release  => "${::lsbdistcodename}/updates",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'extended-lts':
-            location => 'http://deb.freexian.com/extended-lts',
-            release  => $::lsbdistcodename,
-            repos    => $repos;
+      'extended-lts':
+          location => 'http://deb.freexian.com/extended-lts',
+          release  => $::lsbdistcodename,
+          repos    => $repos;
 
-        'ocf':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => $::lsbdistcodename,
-            repos    => 'main',
-            include  => {
-            src => true
-            };
+      'ocf':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => $::lsbdistcodename,
+          repos    => 'main',
+          include  => {
+          src => true
+          };
 
-        'ocf-backports':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => "${::lsbdistcodename}-backports",
-            repos    => 'main',
-            include  => {
-            src => true
-            };
-        }
+      'ocf-backports':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => "${::lsbdistcodename}-backports",
+          repos    => 'main',
+          include  => {
+          src => true
+          };
+      }
 
-        # Pin anything coming from *-backports to be lower than normal priority
-        apt::pin { 'ocf-backports':
-        priority => 200,
-        codename => "${::lsbdistcodename}-backports",
-        }
+      # Pin anything coming from *-backports to be lower than normal priority
+      apt::pin { 'ocf-backports':
+      priority => 200,
+      codename => "${::lsbdistcodename}-backports",
+      }
 
-        # TODO: Submit patch to puppetlabs-apt to enable having includes for
-        # apt::backports (so that we can include the source too)
-        class { 'apt::backports':
-        location => 'https://mirrors.ocf.berkeley.edu/debian/';
-        }
-
+      # TODO: Submit patch to puppetlabs-apt to enable having includes for
+      # apt::backports (so that we can include the source too)
+      class { 'apt::backports':
+      location => 'https://mirrors.ocf.berkeley.edu/debian/';
+      }
     }
-elsif $::operatingsystemmajrelease == '10' {
-       apt::source {
-        'debian':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => $::lsbdistcodename,
-            repos    => $repos,
-            include  => {
-            src => true
-            };
-
-        'debian-updates':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => "${::lsbdistcodename}-updates",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
-
-        'debian-security':
-            location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
-            release  => "${::lsbdistcodename}/updates",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
-
-        'ocf':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => $::lsbdistcodename,
-            repos    => 'main',
-            include  => {
-            src => true
-            };
-
-        'ocf-backports':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => "${::lsbdistcodename}-backports",
-            repos    => 'main',
-            include  => {
-            src => true
-            };
-        }
-
-        # Pin anything coming from *-backports to be lower than normal priority
-        apt::pin { 'ocf-backports':
-        priority => 200,
-        codename => "${::lsbdistcodename}-backports",
-        }
-
-        # TODO: Submit patch to puppetlabs-apt to enable having includes for
-        # apt::backports (so that we can include the source too)
-        class { 'apt::backports':
-        location => 'https://mirrors.ocf.berkeley.edu/debian/';
-        }
-
-    }
-else {
+  elsif $::operatingsystemmajrelease == '10' {
     apt::source {
-        'debian':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => $::lsbdistcodename,
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      'debian':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => $::lsbdistcodename,
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'debian-updates':
-            location => 'https://mirrors.ocf.berkeley.edu/debian/',
-            release  => "${::lsbdistcodename}-updates",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      'debian-updates':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => "${::lsbdistcodename}-updates",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'debian-security':
-            location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
-            release  => "${::lsbdistcodename}-security",
-            repos    => $repos,
-            include  => {
-            src => true
-            };
+      'debian-security':
+          location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
+          release  => "${::lsbdistcodename}/updates",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
 
-        'ocf':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => $::lsbdistcodename,
-            repos    => 'main',
-            include  => {
-            src => true
-            };
+      'ocf':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => $::lsbdistcodename,
+          repos    => 'main',
+          include  => {
+          src => true
+          };
 
-        'ocf-backports':
-            location => 'https://apt.ocf.berkeley.edu/',
-            release  => "${::lsbdistcodename}-backports",
-            repos    => 'main',
-            include  => {
-            src => true
-            };
-        }
+      'ocf-backports':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => "${::lsbdistcodename}-backports",
+          repos    => 'main',
+          include  => {
+          src => true
+          };
+      }
 
-        # Pin anything coming from *-backports to be lower than normal priority
-        apt::pin { 'ocf-backports':
-        priority => 200,
-        codename => "${::lsbdistcodename}-backports",
-        }
+      # Pin anything coming from *-backports to be lower than normal priority
+      apt::pin { 'ocf-backports':
+      priority => 200,
+      codename => "${::lsbdistcodename}-backports",
+      }
 
-        # TODO: Submit patch to puppetlabs-apt to enable having includes for
-        # apt::backports (so that we can include the source too)
-        class { 'apt::backports':
-        location => 'https://mirrors.ocf.berkeley.edu/debian/';
-        }
-}
+      # TODO: Submit patch to puppetlabs-apt to enable having includes for
+      # apt::backports (so that we can include the source too)
+      class { 'apt::backports':
+      location => 'https://mirrors.ocf.berkeley.edu/debian/';
+      }
+
+    }
+  else {
+    apt::source {
+      'debian':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => $::lsbdistcodename,
+          repos    => $repos,
+          include  => {
+          src => true
+          };
+
+      'debian-updates':
+          location => 'https://mirrors.ocf.berkeley.edu/debian/',
+          release  => "${::lsbdistcodename}-updates",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
+
+      'debian-security':
+          location => 'https://mirrors.ocf.berkeley.edu/debian-security/',
+          release  => "${::lsbdistcodename}-security",
+          repos    => $repos,
+          include  => {
+          src => true
+          };
+
+      'ocf':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => $::lsbdistcodename,
+          repos    => 'main',
+          include  => {
+          src => true
+          };
+
+      'ocf-backports':
+          location => 'https://apt.ocf.berkeley.edu/',
+          release  => "${::lsbdistcodename}-backports",
+          repos    => 'main',
+          include  => {
+          src => true
+          };
+      }
+
+      # Pin anything coming from *-backports to be lower than normal priority
+      apt::pin { 'ocf-backports':
+      priority => 200,
+      codename => "${::lsbdistcodename}-backports",
+      }
+
+      # TODO: Submit patch to puppetlabs-apt to enable having includes for
+      # apt::backports (so that we can include the source too)
+      class { 'apt::backports':
+      location => 'https://mirrors.ocf.berkeley.edu/debian/';
+      }
+  }
 
   } elsif $::lsbdistid == 'Raspbian' {
     apt::source {
