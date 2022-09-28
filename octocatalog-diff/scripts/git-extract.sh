6b078900f756aaf3f4f7a566b52f92dcb13c7542
Ethan Wu <ethanwu10@gmail.com>
2022-09-28 13:23:07 -0700

    fix: make script reentrant

--- octocatalog-diff/scripts/git-extract.sh
+++ octocatalog-diff/scripts/git-extract.sh
@@ -1,22 +1,2 @@
 #!/usr/bin/env bash
-
-# Based on github/octocatalog-diff:examples/scritp-overrides/git-extract-submodules/git-extract.sh
-# (b2834b58bfd0c2f22797daccff53bcdf8cda915b)
-
-# This script is called from lib/octocatalog-diff/catalog-util/git.rb and is used to
-# archive and extract a certain branch of a git repository into a target directory.
-
-if [ -z "$OCD_GIT_EXTRACT_BRANCH" ]; then
-  echo "Error: Must declare OCD_GIT_EXTRACT_BRANCH"
-  exit 255
-fi
-
-if [ -z "$OCD_GIT_EXTRACT_TARGET" ]; then
-  echo "Error: Must declare OCD_GIT_EXTRACT_TARGET"
-  exit 255
-fi
-
-set -euf -o pipefail
-git worktree remove -f "$OCD_GIT_EXTRACT_TARGET" || true
-git worktree add "$OCD_GIT_EXTRACT_TARGET" "$OCD_GIT_EXTRACT_BRANCH" --detach
-( cd "$OCD_GIT_EXTRACT_TARGET" && git submodule sync --recursive && git submodule update --init --recursive )
+python3 "$(dirname "$(realpath "$0")")/git-extract.py"
