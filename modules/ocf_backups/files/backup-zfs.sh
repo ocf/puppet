2466a3f39e2559c682ee4432f90dc386a8ca3066
Jonathan Zhang <jonathan@bbs4.us>
2022-10-19 02:31:59 -0700

    feat: zfs-based rsnapshot backup

---
+++ modules/ocf_backups/files/backup-zfs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CURRENT_SNAPSHOT_FILE=/opt/share/backups/current-zfs-snapshot
+CURRENT_SNAPSHOT=$(cat $CURRENT_SNAPSHOT_FILE)
+OFFSITE_HOST=$(cat /opt/share/backups/offsite-host)
+echo $CURRENT_SNAPSHOT
+
+rsnapshot -V -c /opt/share/backups/rsnapshot-zfs.conf sync
+rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-mysql.conf sync
+rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-git.conf sync
+rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-pgsql.conf sync
+
+zfs-auto-snapshot --syslog --label=after-backup --keep=10 // | awk -F"," '{print $1}' | cut -c2- > $CURRENT_SNAPSHOT_FILE
+NEW_SNAPSHOT=$(cat $CURRENT_SNAPSHOT_FILE)
+
+if [ -t 0 ]; then
+        echo $CURRENT_SNAPSHOT
+        echo $NEW_SNAPSHOT
+        zfs send -cRwi backup/encrypted/rsnapshot@$CURRENT_SNAPSHOT backup/encrypted/rsnapshot@$NEW_SNAPSHOT | pv | ssh $OFFSITE_HOST "zfs recv -d data1/ocfbackup"
+else
+        echo $CURRENT_SNAPSHOT
+        echo $NEW_SNAPSHOT
+        zfs send -cRwi backup/encrypted/rsnapshot@$CURRENT_SNAPSHOT backup/encrypted/rsnapshot@$NEW_SNAPSHOT | ssh $OFFSITE_HOST "zfs recv -d data1/ocfbackup"
+fi
