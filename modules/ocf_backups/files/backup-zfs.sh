515e96f2fdcf5166a2396d81360b9a42eee5db80
Jonathan Zhang <jonathan@bbs4.us>
2022-11-21 01:51:11 -0800

    feat: reduce verbosity

--- modules/ocf_backups/files/backup-zfs.sh
+++ modules/ocf_backups/files/backup-zfs.sh
@@ -5,10 +5,10 @@ CURRENT_SNAPSHOT=$(cat $CURRENT_SNAPSHOT_FILE)
 OFFSITE_HOST=$(cat /opt/share/backups/offsite-host)
 echo "$CURRENT_SNAPSHOT"
 
-rsnapshot -V -c /opt/share/backups/rsnapshot-zfs.conf sync
-rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-mysql.conf sync
-rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-git.conf sync
-rsnapshot -V -c /opt/share/backups/rsnapshot-zfs-pgsql.conf sync
+rsnapshot -c /opt/share/backups/rsnapshot-zfs.conf sync
+rsnapshot -c /opt/share/backups/rsnapshot-zfs-mysql.conf sync
+rsnapshot -c /opt/share/backups/rsnapshot-zfs-git.conf sync
+rsnapshot -c /opt/share/backups/rsnapshot-zfs-pgsql.conf sync
 
 zfs-auto-snapshot --syslog --label=after-backup --keep=10 // | awk -F"," '{print $1}' | cut -c2- > $CURRENT_SNAPSHOT_FILE
 NEW_SNAPSHOT=$(cat $CURRENT_SNAPSHOT_FILE)
