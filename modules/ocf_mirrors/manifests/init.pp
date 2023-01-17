eff29740e0c8faaca853eabc79c72e020d55174c
Ethan Wu <ethanwu10@gmail.com>
2023-01-16 21:07:22 -0800

    refactor(mirrors): move+use variable for ocfstats password
    Some history: the variable was introduced in #169 when stat tracking was
    added, but #925 changed the implementation and didn't use the variable
    any more (probably because it was so far away from where it was used).
    This commit moves it next to where it is used now.

--- modules/ocf_mirrors/manifests/init.pp
+++ modules/ocf_mirrors/manifests/init.pp
@@ -97,7 +97,6 @@ class ocf_mirrors {
 
       END
   }
-  $ocfstats_password = lookup('ocfstats::mysql::password')
 
   # Restart nginx if any cert changes occur
   Class['ocf::ssl::default'] ~> Class['Nginx::Service']
@@ -218,9 +217,10 @@ class ocf_mirrors {
     mode   => '0755',
   }
 
+  $ocfstats_password = lookup('ocfstats::mysql::password')
   file {
     '/opt/ocfstats-password':
-      content   => lookup('ocfstats::mysql::password'),
+      content   => $ocfstats_password,
       mode      => '0600',
       owner     => 'root',
       group     => 'root',
