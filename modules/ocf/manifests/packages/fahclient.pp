a0de619fb87e3cd41fa965820b03df8cc2dc6270
Jonathan Zhang <jonathan@bbs4.us>
2023-05-06 11:33:37 -0700

    chore: remove fahclient (#1366)

--- modules/ocf/manifests/packages/fahclient.pp
+++ modules/ocf/manifests/packages/fahclient.pp
@@ -1,24 +1,7 @@
 class ocf::packages::fahclient {
   # Folding @ Home
-  package {'fahclient':;}
-
-  $fah_user = 'Open_Computing_Facility'
-  $fah_passkey = lookup('ocf_desktop::fah_passkey')
-
-  file {
-    '/etc/fahclient/config.xml':
-      content   => template('ocf/packages/fahclient/config.xml.erb'),
-      show_diff => false,
-      require   => Package['fahclient'],
-      owner     => 'fahclient',
-      mode      => '0660',
-      notify    => Service['FAHClient'];
-  }
-
-  service {
-    'FAHClient':
-      ensure  => 'stopped',
-      enable  => false,
-      require => Package['fahclient'];
+  package {
+    'fahclient':
+      ensure => purged;
   }
 }
