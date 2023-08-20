0964e705f5ef8edac04c0d6e966300e34abf88ca
Jonathan Zhang <jonathan@bbs4.us>
2023-08-20 01:00:21 -0700

    fix: update element-desktop signing key

--- modules/ocf/manifests/packages/element/apt.pp
+++ modules/ocf/manifests/packages/element/apt.pp
@@ -3,13 +3,13 @@ class ocf::packages::element::apt {
   apt::key { 'element':
     ensure => refreshed,
     id     => '12D4CD600C2240A9F4A82071D7B0B66941D01538',
-    source => 'https://packages.riot.im/debian/riot-im-archive-keyring.asc',
+    source => 'https://packages.element.io/debian/element-io-archive-keyring.asc',
   }
 
   apt::source { 'element':
     architecture => 'amd64',
-    location     => 'https://packages.riot.im/debian',
-    release      => $facts['os']['distro']['codename'],
+    location     => 'https://packages.element.io/debian/',
+    release      => 'default',
     repos        => 'main',
     require      => Apt::Key['element'],
   }
