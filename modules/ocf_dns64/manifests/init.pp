a9bd56d310e86df4a568d52ed6d4362640576fee
Kian <kian@ocf.berkeley.edu>
2023-12-29 19:28:44 -0800

    Probably should've tested

--- modules/ocf_dns64/manifests/init.pp
+++ modules/ocf_dns64/manifests/init.pp
@@ -1,10 +1,12 @@
 class ocf_dns64 {
+  require ocf::networking
+
   package { 'bind9':; }
   service { 'bind9':
     require => Package['bind9'];
   }
 
-  $upstream_nameservers = ocf::networking::nameservers
+  $upstream_nameservers = $ocf::networking::nameservers
   $ocf_ipv6_mask = lookup('ocf_ipv6_mask')
   $decalvm_src_range_4 = lookup('decalvm_src_range_4')
   $decalvm_src_range_6 = lookup('decalvm_src_range_6')
@@ -20,22 +22,18 @@ class ocf_dns64 {
 
   firewall_multi {
     '101 allow dns (IPv4)':
-      opts => {
-        chain     => 'PUPPET-INPUT',
-        src_range => $decalvm_src_range_4,
-        proto     => ['tcp', 'udp'],
-        dport     => 53,
-        action    => 'accept',
-      };
+      chain     => 'PUPPET-INPUT',
+      src_range => $decalvm_src_range_4,
+      proto     => ['tcp', 'udp'],
+      dport     => 53,
+      action    => 'accept';
 
     '101 allow dns (IPv6)':
-      opts => {
-        provider  => 'ip6tables',
-        chain     => 'PUPPET-INPUT',
-        src_range => $decalvm_src_range_6,
-        proto     => ['tcp', 'udp'],
-        dport     => 53,
-        action    => 'accept',
-      };
+      provider  => 'ip6tables',
+      chain     => 'PUPPET-INPUT',
+      src_range => $decalvm_src_range_6,
+      proto     => ['tcp', 'udp'],
+      dport     => 53,
+      action    => 'accept';
   }
 }
