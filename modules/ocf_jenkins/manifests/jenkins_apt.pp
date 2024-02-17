548eda4253ddb95ac97cd08aa312361fabfc1272
Oliver Ni <oliver.ni@gmail.com>
2024-02-16 17:18:43 -0800

    Update jenkins apt key

--- modules/ocf_jenkins/manifests/jenkins_apt.pp
+++ modules/ocf_jenkins/manifests/jenkins_apt.pp
@@ -1,7 +1,7 @@
 class ocf_jenkins::jenkins_apt {
   apt::key { 'jenkins':
     id     => '63667EE74BBA1F0A08A698725BA31D57EF5975CA',
-    source => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
+    source => 'https://pkg.jenkins.io/debian/jenkins.io-2023.key';
   }
 
   apt::source { 'jenkins':
