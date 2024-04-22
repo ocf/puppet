843cc0a488ebdad75fad99859e61f0e66cc8f802
Jonathan Zhang <jonathan@bbs4.us>
2024-04-21 20:10:38 -0700

    oof

--- modules/ocf/manifests/packages/kubernetes/apt.pp
+++ modules/ocf/manifests/packages/kubernetes/apt.pp
@@ -1,15 +1,4 @@
 class ocf::packages::kubernetes::apt {
-  apt::key { 'kubernetes':
-    id      => '35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3',
-    source  => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
-  }
-  apt::source { 'kubernetes':
-    location => 'https://apt.kubernetes.io',
-    repos    => 'main',
-    # NOTE: we can't use kubernetes-stretch because kubelet isn't included.
-    release  => 'kubernetes-xenial',
-  }
-
   # This comes from the local variable of the same name in
   # github.com/puppetlabs/puppetlabs-kubernetes/blob/master/manifests/packages.pp,
   # which unfortunately is not exposed.
