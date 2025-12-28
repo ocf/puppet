30d9238aeaec9f46834800763c839d6128027532
Jonathan Zhang <jonathan@bbs4.us>
2025-12-28 00:47:12 -0800

    feat: healthcheck for termux

--- modules/ocf_mirrors/manifests/projects/termux.pp
+++ modules/ocf_mirrors/manifests/projects/termux.pp
@@ -8,6 +8,15 @@ class ocf_mirrors::projects::termux {
       mode    => '0755',
       recurse => true;
   }
+
+  ocf_mirrors::monitoring { 'termux':
+    type          => 'unix_timestamp',
+    upstream_host => 'packages.termux.dev',
+    ts_path       => 'termux-main/lastupdate',
+    upstream_path => '/apt',
+    local_path    => '/termux';
+  }
+
   ocf_mirrors::timer {
     'termux':
       exec_start => '/opt/mirrors/project/termux/sync-archive',
