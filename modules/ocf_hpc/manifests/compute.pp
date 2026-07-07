5355247ca5dbe264fee3a5cac15715eac9d542d0
sophie <sophiebsw@gmail.com>
2026-07-06 22:53:33 -0700

    disable cgroups v1

--- modules/ocf_hpc/manifests/compute.pp
+++ modules/ocf_hpc/manifests/compute.pp
@@ -42,7 +42,7 @@ class ocf_hpc::compute {
     incl    => '/etc/default/grub',
     lens    => 'ShellVars.lns',
     changes => [
-      "set GRUB_CMDLINE_LINUX '\"cgroup_enable=memory swapaccount=1 systemd.unified_cgroup_hierarchy=0\"'",
+      "set GRUB_CMDLINE_LINUX '\"swapaccount=1 systemd.unified_cgroup_hierarchy=1\"'",
     ],
   } ~> exec { 'update-grub':
     user        => 'root',
